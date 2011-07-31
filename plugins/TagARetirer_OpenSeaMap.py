125d08f60f4920bc96871a4b408c013e27127918
BlackMyst <black.myst@free.fr>
2011-07-31 18:38:58 +0200

    Nouveau plugin pour les imports OpenSeaMap
    Les objets marqué par le tag "seamark:fixme" doivent être mieux
    géolocalisé (voir osm-fr 26/07/2011)

---
+++ plugins/TagARetirer_OpenSeaMap.py
@@ -0,0 +1,30 @@
+#-*- coding: utf-8 -*-
+
+###########################################################################
+##                                                                       ##
+## Copyrights Etienne Chové <chove@crans.org> 2009                       ##
+##                                                                       ##
+## This program is free software: you can redistribute it and/or modify  ##
+## it under the terms of the GNU General Public License as published by  ##
+## the Free Software Foundation, either version 3 of the License, or     ##
+## (at your option) any later version.                                   ##
+##                                                                       ##
+## This program is distributed in the hope that it will be useful,       ##
+## but WITHOUT ANY WARRANTY; without even the implied warranty of        ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         ##
+## GNU General Public License for more details.                          ##
+##                                                                       ##
+## You should have received a copy of the GNU General Public License     ##
+## along with this program.  If not, see <http://www.gnu.org/licenses/>. ##
+##                                                                       ##
+###########################################################################
+
+class plugin:
+
+    err_4060    = 4060
+    err_4060_fr = u"Import OpenSeaMap, la position de l'objet est très approximative."
+    err_4060_en = u"OpenSeaMap import, very approximative position."
+
+    def node(self, data, tags):
+        if "seamark:fixme" in tags:
+            return [(4060, 0, {})]
