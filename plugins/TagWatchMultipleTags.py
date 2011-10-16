e08eb88f42af8cdb433c87188d1902c5dc805a05
Fred <fred.rodrigo@gmail.com>
2011-10-16 10:04:57 +0200

    fix messages in TagWatchMultipleTags

--- plugins/TagWatchMultipleTags.py
+++ plugins/TagWatchMultipleTags.py
@@ -46,11 +46,11 @@ class TagWatchMultipleTags(Plugin):
         if "amenity" in tags:
             if tags["amenity"] == "place_of_worship":
                 if self.Eglise.match(tags["name"]) and not self.EgliseNot1.match(tags["name"]) and not self.EgliseNot2.match(tags["name"]):
-                    err.append((3032, 1, {"fr": u"name=%s est la localisation mais pas le nom" % (tags["name"])}))
+                    err.append((3032, 1, {"fr": u"\"name=%s\" est la localisation mais pas le nom" % (tags["name"])}))
         if "historic" in tags:
             if tags["historic"] == "monument":
                 if self.MonumentAuxMorts.match(tags["name"]):
-                    err.append((3032, 2, {"fr": u"Un monuments aux Morts est un historic=memorial"}))
+                    err.append((3032, 2, {"fr": u"Un monument aux Morts est un historic=memorial"}))
 
         if (not "highway" in tags) and (self.SalleDesFetes.match(tags["name"]) or self.MaisonDeQuartier.match(tags["name"])) and not ("amenity" in tags and tags["amenity"] == "community_centre"):
             err.append((3032, 3, {"fr": u"Le tag pour une salle des fêtes ou une maison de quartier est amenity=community_centre"}))
