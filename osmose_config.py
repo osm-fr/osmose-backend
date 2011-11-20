8d2805ca4abd5d4a9f32a03f156b18d6e3ec8e2e
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2011-11-20 21:27:28 +0100

    Add analyser_osmosis_broken_highway_level_continuity to config

--- osmose_config.py
+++ osmose_config.py
@@ -154,6 +154,7 @@ for region in "alsace aquitaine auvergne basse-normandie bourgogne bretagne cent
   config[country].analyser["osmosis_double_tagging"] = "xxx"
   config[country].analyser["osmosis_associatedStreet"] = "xxx"
   config[country].analyser["osmosis_highway_link"] = "xxx"
+  config[country].analyser["osmosis_broken_highway_level_continuity"] = "xxx"
   config[country].analyser["stats"] = "xxx"
 
   config[country].analyser_options = { "sax": { "plugin_filter": ["fr", "FR"] },
@@ -224,6 +225,7 @@ for country in "belgique luxembourg".split():
   config[country].analyser["osmosis_double_tagging"] = "xxx"
   config[country].analyser["osmosis_associatedStreet"] = "xxx"
   config[country].analyser["osmosis_highway_link"] = "xxx"
+  config[country].analyser["osmosis_broken_highway_level_continuity"] = "xxx"
   config[country].analyser["stats"] = "xxx"
 
 country = "belgique"
