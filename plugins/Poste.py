21ed72ff33d9253ff8d593510c19e0110912239a
Fred <fred.rodrigo@gmail.com>
2012-04-30 16:59:30 +0200

    Add Poste import plugin

---
+++ plugins/Poste.py
@@ -0,0 +1,44 @@
+#-*- coding: utf-8 -*-
+
+###########################################################################
+##                                                                       ##
+## Copyrights Frédéric Rodrigo 2012                                      ##
+##                                                                       ##
+## This program is free software: you can redistribute it and/or modify  ##
+## it under the terms of the GNU General Public License as published by  ##
+## the Free Software Foundation, either version 3 of the License, or     ##
+## (at your option) any later version.                                   ##
+##                                                                       ##
+## This program is distributed in the hope that it will be useful,       ##
+## but WITHOUT ANY WARRANTY; without even the implied warranty of        ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         ##
+## GNU General Public License for more details.                          ##
+##                                                                       ##
+## You should have received a copy of the GNU General Public License     ##
+## along with this program.  If not, see <http://www.gnu.org/licenses/>. ##
+##                                                                       ##
+###########################################################################
+
+from plugins.Plugin import Plugin
+import re
+
+class Poste(Plugin):
+
+    only_for = ["FR"]
+
+    def init(self, logger):
+        Plugin.init(self, logger)
+        self.errors[7050] = { "item": 7050, "desc": {"fr": u"Poste à importer"} }
+
+    def node(self, data, tags):
+        if not "amenity" in tags or not tags["amenity"] = "post_office":
+            return
+
+        if not "ref:FR:LaPoste" in tags:
+            return [(7050, 0, {})]
+
+    def way(self, data, tags, nds):
+        return self.node(data, tags)
+
+    def relation(self, data, tags, members):
+        return self.node(data, tags)
