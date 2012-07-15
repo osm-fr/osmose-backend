4fc68f82b23c1cabb41b0883508a76f89da9ed52
Fred <fred.rodrigo@gmail.com>
2012-07-15 14:21:11 +0200

    Add backlist key into osmosis_tag_typo

--- analyser_osmosis_tag_typo.py
+++ analyser_osmosis_tag_typo.py
@@ -47,7 +47,7 @@ FROM
             key
         ) AS keys
     WHERE
-        key NOT IN ('tower:type', 'power:type', 'food', 'foot')
+        key NOT IN ('tower', 'power', 'tower:type', 'power:type', 'food', 'foot', 'line', 'lines')
     ) AS keys
 GROUP BY
     key
