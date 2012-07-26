3cab52995d2ce29daf58c8f0467e6039df57081b
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2012-07-26 19:50:50 +0200

    Add new analyse osmosis_railway_approximate to all countries

--- osmose_config.py
+++ osmose_config.py
@@ -171,6 +171,7 @@ for region in "alsace aquitaine auvergne basse-normandie bourgogne bretagne cent
   config[country].analyser["osmosis_node_like_way"] = "xxx"
   config[country].analyser["osmosis_boundary_administrative"] = "xxx"
   config[country].analyser["osmosis_tag_typo"] = "xxx"
+  config[country].analyser["osmosis_railway_approximate"] = "xxx"
 #  config[country].analyser["stats"] = "xxx"
 
   config[country].analyser_options = { "sax": { "plugin_filter": ["fr", "FR", "FR_%s" % region]
@@ -239,6 +240,7 @@ for country in "belgium luxembourg madagascar switzerland".split():
   config[country].analyser["osmosis_node_like_way"] = "xxx"
   config[country].analyser["osmosis_boundary_administrative"] = "xxx"
   config[country].analyser["osmosis_tag_typo"] = "xxx"
+  config[country].analyser["osmosis_railway_approximate"] = "xxx"
 #  config[country].analyser["stats"] = "xxx"
 
 country = "belgium"
