ab9bdbb42f6cd04cd193aaf86f49eaf8f3d4c86d
Fred <fred.rodrigo@gmail.com>
2012-08-02 10:40:50 +0200

    Fix item into merge_school_fr

--- analyser_merge_school_fr.py
+++ analyser_merge_school_fr.py
@@ -31,7 +31,7 @@ class Analyser_Merge_School_Fr(Analyser_Merge):
         Analyser_Merge.__init__(self, config, logger)
         self.classs[1] = {"item":"8030", "level": 3, "tag": ["merge"], "desc":{"fr":"École non intégrée"} }
         self.classs[2] = {"item":"7070", "level": 3, "tag": ["merge"], "desc":{"fr":"École sans ref:UAI ou invalide"} }
-        self.classs[3] = {"item":"8030", "level": 3, "tag": ["merge"], "desc":{"fr":"École, proposition d'intégration"} }
+        self.classs[3] = {"item":"8031", "level": 3, "tag": ["merge"], "desc":{"fr":"École, proposition d'intégration"} }
         self.officialURL = "http://www.data.gouv.fr/donnees/view/G%C3%A9olocalisation-des-%C3%A9tablissements-d%27enseignement-du-premier-degr%C3%A9-et-du-second-degr%C3%A9-du-minist%C3%A8re-d-30378093"
         self.officialName = "établissements d'enseignement du premier degré et du second degré"
         self.osmTags = {
