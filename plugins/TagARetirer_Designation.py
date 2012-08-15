d8ee66265b77c18768c8f009f4aeeaeaf1dcbf24
Fred <fred.rodrigo@gmail.com>
2012-08-15 14:28:56 +0200

    Add plugin misused designation tag

---
+++ plugins/TagARetirer_Designation.py
@@ -0,0 +1,42 @@
+#-*- coding: utf-8 -*-
+
+###########################################################################
+##                                                                       ##
+## Copyrights Frederic Rodrigo 2012                                      ##
+##                                                                       ##
+## This program is free software: you can redistribute it and/or modify  ##
+## it under the terms of the GNU General Public License as published by  ##
+## the Free Software Foundation, either version 3 of the License, or     ##
+## (at your option) any later version.                                   ##
+##                                                                       ##
+## This program is distributed in the hope that it will be useful,       ##
+## but WITHOUT ANY WARRANTY; without even the implied warranty of        ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         ##
+## GNU General Public License for more details.                          ##
+##                                                                       ##
+## You should have received a copy of the GNU General Public License     ##
+## along with this program.  If not, see <http://www.gnu.org/licenses/>. ##
+##                                                                       ##
+###########################################################################
+
+from plugins.Plugin import Plugin
+import re
+
+
+class TagARetirer_Designation(Plugin):
+
+    only_for = ["FR"]
+
+    def init(self, logger):
+        Plugin.init(self, logger)
+        self.errors[41001] = { "item": 4100, "level": 1, "tag": ["tag"], "desc": {"en": u"Misused tag \"designation\"", "fr": u"Mauvais usage du tag \"designation\""} }
+
+    def node(self, data, tags):
+        if "designation" in tags:
+            return ((41001, 1, {}))
+
+    def way(self, data, tags, nds):
+        return self.node(data, tags)
+
+    def relation(self, data, tags, members):
+        return self.node(data, tags)
