30cbd40606d51cb217d7997976b48ea074987f29
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2012-09-09 23:26:36 +0200

    Add support for PBF througr library imposm.parser

---
+++ modules/OsmPbf.py
@@ -0,0 +1,99 @@
+#-*- coding: utf-8 -*-
+
+###########################################################################
+##                                                                       ##
+## Copyrights Jocelyn Jaubert 2012                                       ##
+##                                                                       ##
+## This program is free software: you can redistribute it and/or modify  ##
+## it under the terms of the GNU General Public License as published by  ##
+## the Free Software Foundation, either version 3 of the License, or     ##
+## (at your option) any later version.                                   ##
+##                                                                       ##
+## This program is distributed in the hope that it will be useful,       ##
+## but WITHOUT ANY WARRANTY; without even the implied warranty of        ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         ##
+## GNU General Public License for more details.                          ##
+##                                                                       ##
+## You should have received a copy of the GNU General Public License     ##
+## along with this program.  If not, see <http://www.gnu.org/licenses/>. ##
+##                                                                       ##
+###########################################################################
+
+import os, signal
+from imposm.parser.simple import OSMParser
+
+###########################################################################
+
+class dummylog:
+    def log(self, text):
+        return
+
+###########################################################################
+
+class OsmPbfReader:
+
+    def log(self, txt):
+        self._logger.log(txt)
+    
+    def __init__(self, pbf_file, logger = dummylog()):
+        self._pbf_file = pbf_file
+        self._logger   = logger
+
+
+    def CopyTo(self, output):
+        self._debug_in_way      = False
+        self._debug_in_relation = False
+        self._output = output
+        self.parser = OSMParser(concurrency=2,
+                                nodes_callback=self.NodeParse,
+                                ways_callback=self.WayParse,
+                                relations_callback=self.RelationParse)
+        self.parser.parse(self._pbf_file)
+
+       
+    def NodeParse(self, nodes):
+        for node in nodes:
+            data = {}
+            data["id"] = node[0]
+            data["lon"] = node[2][0]
+            data["lat"] = node[2][1]
+            data["tag"] = node[1]
+            try:
+                self._output.NodeCreate(data)
+            except:
+                print node, data
+                raise
+
+    def WayParse(self, ways):
+        for way in ways:
+            data = {}
+            data["id"] = way[0]
+            data["tag"] = way[1]
+            data["nd"] = way[2]
+            try:
+                self._output.WayCreate(data)
+            except:
+                print way, data
+                pass
+
+    def RelationParse(self, relations):
+        for relation in relations:
+            data = {}
+            data["id"] = relation[0]
+            data["tag"] = relation[1]
+            data["member"] = []
+            for (ref, type, role) in relation[2]:
+                attrs = { "ref": int(ref),
+                          "role": role,
+                          "type": type,
+                        }
+
+                data["member"].append(attrs)
+
+            try:
+                self._output.RelationCreate(data)
+            except:
+                print data
+                pass
+
+ 
