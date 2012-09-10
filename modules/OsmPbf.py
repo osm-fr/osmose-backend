99104def26df7e8315f3d27c56c540c11c1d2d0d
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2012-09-10 19:58:23 +0200

    Add fields version,timestamp,uid in OsmPbf
    This will need a specially compiled python-imposm-parser package,
    using source from https://github.com/mvexel/imposm.parser/

--- modules/OsmPbf.py
+++ modules/OsmPbf.py
@@ -19,6 +19,7 @@
 ##                                                                       ##
 ###########################################################################
 
+import time
 import traceback
 from imposm.parser.simple import OSMParser
 
@@ -60,9 +61,12 @@ class OsmPbfReader:
         for node in nodes:
             data = {}
             data["id"] = node[0]
+            data["tag"] = node[1]
             data["lon"] = node[2][0]
             data["lat"] = node[2][1]
-            data["tag"] = node[1]
+            data["version"] = node[3]
+            data["timestamp"] = time.strftime("%Y-%m-%dT%H:%M:%SZ",time.gmtime(node[4]))
+            data["uid"] = node[5]
             try:
                 self._output.NodeCreate(data)
             except:
@@ -78,6 +82,9 @@ class OsmPbfReader:
             data["id"] = way[0]
             data["tag"] = way[1]
             data["nd"] = way[2]
+            data["version"] = way[3]
+            data["timestamp"] = time.strftime("%Y-%m-%dT%H:%M:%SZ",time.gmtime(way[4]))
+            data["uid"] = way[5]
             try:
                 self._output.WayCreate(data)
             except:
@@ -92,6 +99,9 @@ class OsmPbfReader:
             data = {}
             data["id"] = relation[0]
             data["tag"] = relation[1]
+            data["version"] = relation[3]
+            data["timestamp"] = time.strftime("%Y-%m-%dT%H:%M:%SZ",time.gmtime(relation[4]))
+            data["uid"] = relation[5]
             data["member"] = []
             for (ref, type, role) in relation[2]:
                 attrs = { "ref": int(ref),
