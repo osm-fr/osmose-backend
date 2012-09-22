24d4510a0d4cec46523fd9eeee7cc2a2a706c7ea
Fred <fred.rodrigo@gmail.com>
2012-09-22 14:02:22 +0200

    Fix norm/diff mode

--- analyser_osmosis_useless.py
+++ analyser_osmosis_useless.py
@@ -63,6 +63,6 @@ class Analyser_Osmosis_Useless(Analyser_Osmosis):
         self.callback10 = lambda res: {"class":1, "data":[self.node_full, self.relation_full, self.positionAsText]}
         self.callback20 = lambda res: {"class":2, "data":[self.way_full, self.relation_full if res[1] else None, self.positionAsText]}
 
-    def analyser_osmosis_all(self):
+    def analyser_osmosis(self):
         self.run(sql10, self.callback10)
         self.run(sql20, self.callback20)
