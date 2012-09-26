93aefc1273bfd65619f5bd6d821002a7602fa510
Fred <fred.rodrigo@gmail.com>
2012-09-26 19:46:27 +0200

    Add multipolygon analyser

--- analyser_osmosis_multipolygon.py
+++ analyser_osmosis_multipolygon.py
@@ -47,21 +47,158 @@ FROM
 WHERE
     relations.tags?'type' AND
     relations.tags->'type' = 'multipolygon'
-;
 """
 
-class Analyser_Osmosis_Polygon(Analyser_Osmosis):
+sql20 = """
+SELECT
+    relations.id,
+    ways.id,
+    ST_AsText(way_locate(ways.linestring)),
+    relations.tags->'landuse' rl,
+    ways.tags->'landuse' wl,
+    relations.tags->'natural' rn,
+    ways.tags->'natural' wn,
+    relations.tags->'waterway' rw,
+    ways.tags->'waterway' ww,
+    relations.tags->'building' rb,
+    ways.tags->'building' wb,
+    COALESCE(relations.tags->'landuse', relations.tags->'natural', relations.tags->'waterway', relations.tags->'building')
+FROM
+    {0}relations AS relations
+    JOIN relation_members ON
+        relation_members.relation_id = relations.id AND
+        relation_members.member_type = 'W' AND
+        relation_members.member_role IN ('', 'outer')
+    JOIN {1}ways AS ways ON
+        ways.id = relation_members.member_id
+WHERE
+    relations.tags?'type' AND
+    relations.tags->'type' = 'multipolygon' AND
+    (
+        relations.tags?'landuse' AND
+        ways.tags?'landuse' AND
+        ways.tags->'landuse' != (relations.tags->'landuse')
+    ) OR (
+        relations.tags?'natural' AND
+        relations.tags->'natural' IN ('bay', 'beach', 'fell', 'grassland', 'glacier', 'heath', 'mud', 'sand', 'scree', 'scrub', 'sinkhole', 'water', 'wetland', 'wood') AND
+        ways.tags?'natural' AND
+        ways.tags->'natural' IN ('bay', 'beach', 'fell', 'grassland', 'glacier', 'heath', 'mud', 'sand', 'scree', 'scrub', 'sinkhole', 'water', 'wetland', 'wood') AND
+        ways.tags->'natural' != (relations.tags->'natural')
+    ) OR (
+        relations.tags?'waterway' AND
+        relations.tags->'waterway' IN ('boatyard', 'dock', 'riverbank') AND
+        ways.tags?'waterway' AND
+        ways.tags->'waterway' IN ('boatyard', 'dock', 'riverbank') AND
+        ways.tags->'waterway' != (relations.tags->'waterway')
+    ) OR (
+        relations.tags?'building' AND
+        ways.tags?'building' AND
+        ways.tags->'building' != (relations.tags->'building')
+    )
+"""
+
+sql30 = """
+SELECT
+    id,
+    ST_AsText(relation_locate(id)),
+    string_agg(landuse, ',') AS landuse,
+    string_agg("natural", ',') AS "natural",
+    string_agg(waterway, ',') AS waterway,
+    string_agg(building, ',') AS building
+FROM
+(
+    SELECT
+        relations.id,
+        ways.tags->'landuse' AS landuse,
+        ways.tags->'natural' AS "natural",
+        ways.tags->'waterway' AS waterway,
+        ways.tags->'building' AS building
+    FROM
+        {0}relations AS relations
+        JOIN relation_members ON
+            relation_members.relation_id = relations.id AND
+            relation_members.member_type = 'W' AND
+            relation_members.member_role IN ('', 'outer')
+        JOIN {1}ways AS ways ON
+            ways.id = relation_members.member_id
+    WHERE
+        relations.tags?'type' AND
+        relations.tags->'type' = 'multipolygon' AND
+        NOT relations.tags?'landuse' AND
+        NOT relations.tags?'natural' AND
+        NOT relations.tags?'waterway' AND
+        NOT relations.tags?'building' AND
+        ((ways.tags->'landuse') IS NOT NULL OR (ways.tags->'natural') IS NOT NULL OR (ways.tags->'waterway') IS NOT NULL OR (ways.tags->'building') IS NOT NULL)
+    GROUP BY
+        relations.id,
+        ways.tags->'landuse',
+        ways.tags->'natural',
+        ways.tags->'waterway',
+        ways.tags->'building'
+) AS t
+GROUP BY
+    id
+HAVING
+    COUNT(*) > 1
+"""
+
+sql40 = """
+SELECT
+    ways.id,
+    ST_AsText(way_locate(ways.linestring)),
+    ways.tags->'landuse',
+    ways.tags->'natural',
+    ways.tags->'waterway',
+    ways.tags->'building',
+    COALESCE(ways.tags->'landuse', ways.tags->'natural', ways.tags->'waterway', ways.tags->'building')
+FROM
+    {0}ways AS ways
+    LEFT JOIN relation_members ON
+        relation_members.member_id = ways.id AND
+        relation_members.member_type = 'W'
+WHERE
+    (
+        ways.tags?'landuse' OR
+        (ways.tags?'natural' AND ways.tags->'natural' in ('bay', 'beach', 'fell', 'grassland', 'glacier', 'heath', 'mud', 'sand', 'scree', 'scrub', 'sinkhole', 'water', 'wetland', 'wood')) OR
+        (ways.tags?'waterway' AND ways.tags->'waterway' in ('boatyard', 'dock', 'riverbank')) OR
+        ways.tags?'building'
+    ) AND
+    NOT ways.is_polygon AND
+    relation_members.member_id IS NULL
+"""
+
+class Analyser_Osmosis_Multipolygon(Analyser_Osmosis):
 
     def __init__(self, config, logger = None):
         Analyser_Osmosis.__init__(self, config, logger)
         self.classs_change[1] = {"item":"1170", "level": 3, "tag": ["relation", "geom"], "desc":{"en":"Double inner polygon"} }
+        self.classs_change[2] = {"item":"1170", "level": 2, "tag": ["relation", "multipolygon"], "desc":{"fr":"Nature du multipolygon inconsistant avec la nature de ces membres", "en":"Inconsistant multoplygon nature with nature members"} }
+        self.classs_change[3] = {"item":"1170", "level": 2, "tag": ["relation", "multipolygon"], "desc":{"fr":"Nature des membres du multipolygon inconsistant", "en":"Inconsistant multoplygon member nature"} }
+        self.classs_change[4] = {"item":"1170", "level": 1, "tag": ["relation", "geom"], "desc":{"fr":"Drevait être un polygone ou faire partie d'un multipolygon", "en":"Must be polygon or part of multipolygon"} }
         self.callback10 = lambda res: {"class":1, "data":[self.way_full, self.way_full, self.positionAsText]}
+        self.callback20 = lambda res: {"class":2, "subclass":abs(int(hash(res[11]))), "data":[self.relation_full, self.way_full, self.positionAsText],
+            "text": {"en":", ".join(map(lambda k: "%s=(%s,%s)"%k, filter(lambda k: k[1], (("landuse",res[3],res[4]), ("natural",res[5],res[6]), ("waterway",res[7],res[8]), ("building",res[9],res[10])))))}
+        }
+        self.callback30 = lambda res: {"class":3, "subclass":1, "data":[self.relation_full, self.positionAsText],
+            "text": {"en":", ".join(map(lambda k: "%s=(%s)"%k, filter(lambda k: k[1], (("landuse",res[2]), ("natural",res[3]), ("waterway",res[4]), ("building",res[5])))))}
+        }
+        self.callback40 = lambda res: {"class":4, "subclass":abs(int(hash(res[6]))), "data":[self.way_full, self.positionAsText],
+            "text": {"en":", ".join(map(lambda k: "%s=%s"%k, filter(lambda k: k[1], (("landuse",res[2]), ("natural",res[3]), ("waterway",res[4]), ("building",res[5])))))}
+        }
 
     def analyser_osmosis_all(self):
         self.run(sql10.format("", "", ""), self.callback10)
+        self.run(sql20.format("", ""), self.callback20)
+        self.run(sql30.format("", ""), self.callback30)
+        self.run(sql40.format(""), self.callback40)
 
     def analyser_osmosis_touched(self):
         dup = set()
         self.run(sql10.format("touched_", "", ""), lambda res: dup.add((res[0], res[1])) or self.callback10(res))
         self.run(sql10.format("", "touched_", ""), lambda res: (res[0], res[1]) in dup or dup.add((res[0], res[1])) or self.callback10(res))
         self.run(sql10.format("", "", "touched_"), lambda res: (res[0], res[1]) in dup or dup.add((res[0], res[1])) or self.callback10(res))
+        self.run(sql20.format("touched_", ""), self.callback20)
+        self.run(sql20.format("", "touched_"), self.callback20)
+        self.run(sql30.format("touched_", ""), self.callback30)
+        self.run(sql30.format("", "touched_"), self.callback30)
+        self.run(sql40.format("touched_"), self.callback40)
