ddf118666ba50536feb9b1e4d2def7fd6ae90805
Fred <fred.rodrigo@gmail.com>
2012-09-27 10:55:14 +0200

    Add large building intersection cluster to analyser building_overlaps

--- analyser_osmosis_building_overlaps.py
+++ analyser_osmosis_building_overlaps.py
@@ -4,6 +4,7 @@
 ###########################################################################
 ##                                                                       ##
 ## Copyrights Etienne Chové <chove@crans.org> 2009                       ##
+## Copyrights Frédéric Rodrigo 2011                                      ##
 ##                                                                       ##
 ## This program is free software: you can redistribute it and/or modify  ##
 ## it under the terms of the GNU General Public License as published by  ##
@@ -113,6 +114,31 @@ ORDER BY
     bnodes.geom
 """
 
+sql60 = """
+SELECT
+    ST_AsText(ST_Centroid(geom)),
+    ST_Area(geom)
+FROM
+    (
+    SELECT
+        (ST_Dump(poly)).geom AS geom
+    FROM
+        (
+        SELECT
+            ST_Union(ST_Buffer(ways.linestring,5e-3,'quad_segs=2')) AS poly
+        FROM
+            intersection_{0}_{1}
+            JOIN ways ON
+                ways.id = id1
+        WHERE
+            intersectionArea > threshold
+        ) AS building
+    ) AS buffer
+WHERE
+    ST_Area(geom) > 5e-4
+;
+"""
+
 class Analyser_Osmosis_Building_Overlaps(Analyser_Osmosis):
 
     def __init__(self, config, logger = None):
@@ -121,9 +147,11 @@ class Analyser_Osmosis_Building_Overlaps(Analyser_Osmosis):
         self.classs[2] = {"item":"0", "level": 2, "tag": ["building", "geom"], "desc":{"fr":"Grosses intersections de bâtiments", "en":"Large building intersection"} }
         self.classs[3] = {"item":"0", "level": 3, "tag": ["building", "geom"], "desc":{"fr":"Bâtiments trop petit", "en":"Too small building"} }
         self.classs[4] = {"item":"0", "level": 3, "tag": ["building", "geom"], "desc":{"fr":"Interstice entre les bâtiments", "en":"Gap between buildings"} }
+        self.classs[5] = {"item":"0", "level": 1, "tag": ["building"], "desc":{"fr":"Groupe de Grosses intersections de bâtiments", "en":"Large building intersection cluster"} }
         self.callback30 = lambda res: {"class":2 if res[3]>res[4] else 1, "data":[self.way, self.way, self.positionAsText]}
         self.callback40 = lambda res: {"class":3, "data":[self.way, self.positionAsText]}
         self.callback50 = lambda res: {"class":4, "data":[self.way, self.way, self.positionAsText]}
+        self.callback60 = lambda res: {"class":5, "data":[self.positionAsText]}
 
     def analyser_osmosis(self):
         self.run(sql10.format(""))
@@ -134,6 +162,7 @@ class Analyser_Osmosis_Building_Overlaps(Analyser_Osmosis):
         self.run(sql31.format("", ""), self.callback30)
         self.run(sql40.format(""), self.callback40)
         self.run(sql50.format("", ""), self.callback50)
+        self.run(sql60.format("", ""), self.callback60)
 
     def analyser_osmosis_touched(self):
         self.run(sql10.format(""))
@@ -156,3 +185,4 @@ class Analyser_Osmosis_Building_Overlaps(Analyser_Osmosis):
         self.run(sql50.format("touched_", ""), lambda res: dup.add(res[0]) or self.callback50(res))
         self.run(sql50.format("", "touched_"), lambda res: res[0] in dup or dup.add(res[0]) or self.callback50(res))
         self.run(sql50.format("touched_", "touched_"), lambda res: res[0] in dup or dup.add(res[0]) or self.callback50(res))
+        #self.run(sql60.format("", ""), self.callback60) Can be done in diff mode without runing a full sql30
