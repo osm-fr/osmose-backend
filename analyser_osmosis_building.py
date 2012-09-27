6f49107f34a829f43369740a64d65b68a301cbe5
Fred <fred.rodrigo@gmail.com>
2012-09-27 13:59:24 +0200

    Add analyse osmosis_building to detect building usage by shape

---
+++ analyser_osmosis_building.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+###########################################################################
+##                                                                       ##
+## Copyrights Etienne Chové <chove@crans.org> 2010                       ##
+##            Vincent Pottier <@.> 2010                                  ##
+##                                                                       ##
+## This program is free software: you can redistribute it and/or modify  ##
+## it under the terms of the GNU General Public License as published by  ##
+## the Free Software Foundation, either version 3 of the License, or     ##
+## (at your option) any later version.                                   ##
+##                                                                       ##
+## This program is distributed in the hope that it will be useful,       ##
+## but WITHOUT ANY WARRANTY; without even the implied warranty of        ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         ##
+## GNU General Public License for more details.                          ##
+##                                                                       ##
+## You should have received a copy of the GNU General Public License     ##
+## along with this program.  If not, see <http://www.gnu.org/licenses/>. ##
+##                                                                       ##
+###########################################################################
+
+from Analyser_Osmosis import Analyser_Osmosis
+
+sql10 = """
+SELECT
+    id,
+    ST_AsText(way_locate(linestring))
+FROM
+    ways
+WHERE
+    tags?'building' AND
+    tags->'building' = 'yes' AND
+    NOT tags?'wall' AND
+    array_length(nodes, 1) > 16 AND
+    is_polygon AND
+    ST_Area(ST_MakePolygon(ST_Transform(linestring,2154)))/ST_Area(ST_MinimumBoundingCircle(ST_Transform(linestring,2154))) > 0.95 AND
+    ST_MaxDistance(ST_Transform(linestring,2154), ST_Transform(linestring,2154)) > 5 AND
+    array_length(akeys(delete(delete(delete(delete(tags, 'created_by'), 'source'), 'name'), 'building')), 1) = 0 -- IS NULL
+"""
+
+sql20 = """
+SELECT
+    id,
+    ST_AsText(way_locate(linestring))
+FROM
+    ways
+WHERE
+    tags?'building' AND
+    tags->'building' = 'yes' AND
+    NOT tags?'wall' AND
+    is_polygon AND
+    ST_MaxDistance(ST_Transform(linestring,2154), ST_Transform(linestring,2154)) > 300 AND
+    array_length(akeys(delete(delete(delete(delete(tags, 'created_by'), 'source'), 'name'), 'building')), 1) = 0 -- IS NULL
+"""
+
+class Analyser_Osmosis_Building(Analyser_Osmosis):
+
+    def __init__(self, config, logger = None):
+        Analyser_Osmosis.__init__(self, config, logger)
+        self.classs_change[1] = {"item":"7011", "level": 3, "tag": ["building"], "desc":{"fr":"Batiment spécial (rond)", "en":"Special building (round)"} }
+        self.classs_change[2] = {"item":"7011", "level": 3, "tag": ["building"], "desc":{"fr":"Batiment spécial (grand)", "en":"Special building (large)"} }
+        self.callback10 = lambda res: {"class":1, "data":[self.way_full, self.positionAsText], "fix":[
+            {"+":{"man_made":"water_tower"}},
+            {"+":{"man_made":"reservoir_covered"}},
+            {"+":{"man_made":"wastewater_plant"}},
+            {"+":{"man_made":"storage_tank"}},
+            ]}
+        self.callback20 = lambda res: {"class":2, "data":[self.way_full, self.positionAsText], "fix":[
+            {"+":{"man_made":"works"}},
+            {"+":{"shop":"mall"}},
+            {"+":{"shop":"supermarket"}},
+            {"~":{"building":"warehouse"}},
+            ]}
+
+    def analyser_osmosis_all(self):
+        self.run(sql10.format(""), self.callback10)
+        self.run(sql20.format(""), self.callback20)
+
+    def analyser_osmosis_touched(self):
+        self.run(sql10.format("touched_"), self.callback10)
+        self.run(sql20.format("touched_"), self.callback20)
