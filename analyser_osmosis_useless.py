ecef6c8d322a79bdc86b47f4ec412563b680cb56
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2012-10-04 21:11:43 +0200

    Fix array_length call when array is empty for postgresql 9.1

--- analyser_osmosis_useless.py
+++ analyser_osmosis_useless.py
@@ -34,7 +34,7 @@ FROM
         member_type = 'N' AND
         member_role = ''
 WHERE
-    array_length(akeys(delete(delete(tags, 'created_by'), 'source')), 1) = 0
+    array_length(akeys(delete(delete(tags, 'created_by'), 'source')), 1) IS NULL
 ;
 """
 
@@ -50,7 +50,7 @@ FROM
         member_type = 'W'
 WHERE
     (member_role IS NULL OR member_role = '') AND
-    array_length(akeys(delete(delete(tags, 'created_by'), 'source')), 1) = 0
+    array_length(akeys(delete(delete(tags, 'created_by'), 'source')), 1) IS NULL
 ;
 """
 
