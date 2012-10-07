f75cb739bc112d5453b2057b454f9322f08cd4f3
Frédéric Rodrigo <frodrigo@openstreetmap.fr>
2012-10-07 17:35:25 +0200

    Add geodesie merge analyser

---
+++ analyser_merge_geodesie.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+###########################################################################
+##                                                                       ##
+## Copyrights Frédéric Rodrigo 2012                                      ##
+##                                                                       ##
+## This program is free software: you can redistribute it and/or modify  ##
+## it under the terms of the GNU General Public License as published by  ##
+## the Free Software Foundation, either version 3 of the License, or     ##
+## (at your option) any later version.                                   ##
+##                                                                       ##
+## This program is distributed in the hope that it will be useful,       ##
+## but WITHOUT ANY WARRANTY; without even the implied warranty of        ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         ##
+## GNU General Public License for more details.                          ##
+##                                                                       ##
+## You should have received a copy of the GNU General Public License     ##
+## along with this program.  If not, see <http://www.gnu.org/licenses/>. ##
+##                                                                       ##
+###########################################################################
+
+import re
+from Analyser_Merge import Analyser_Merge
+
+
+class Analyser_Merge_Geodesie(Analyser_Merge):
+
+    create_table = """
+        id VARCHAR(254) PRIMARY KEY,
+        lat VARCHAR(254),
+        lon VARCHAR(254),
+        description VARCHAR(4096),
+        ele VARCHAR(254),
+        ref VARCHAR(254)
+    """
+
+    def __init__(self, config, logger = None):
+        self.missing_official = {"item":"8070", "class": 1, "level": 3, "tag": ["merge", "building"], "desc":{"fr":"Repère géodésique manquant"} }
+        Analyser_Merge.__init__(self, config, logger)
+        self.officialURL = "http://geodesie.ign.fr"
+        self.officialName = "Fiches géodésiques"
+        self.csv_file = "merge_data/geodesie.csv"
+        self.csv_format = "WITH DELIMITER AS ',' NULL AS 'null' CSV"
+        self.osmTags = {
+            "man_made": "survey_point",
+        }
+        self.osmRef = "ref"
+        self.osmTypes = ["nodes"]
+        self.sourceTable = "geodesie"
+        self.sourceRef = "ref"
+        self.sourceX = "lon"
+        self.sourceY = "lat"
+        self.sourceSRID = "4326"
+        self.defaultTag = {
+            "man_made": "survey_point",
+            "note": "Ne pas déplacer ce point, cf. - Do not move this node, see - http://wiki.openstreetmap.org/wiki/WikiProject_France/Repères_Géodésiques#Permanence_des_rep.C3.A8res",
+            "source": "©IGN 2010 dans le cadre de la cartographie réglementaire",
+        }
+        self.defaultTagMapping = {
+            "ref": "ref",
+            "ele": "ele",
+            "description": "description",
+        }
+        self.text = lambda tags, fields: {"fr":"Repères géodésiques %s" % tags["ref"]}
