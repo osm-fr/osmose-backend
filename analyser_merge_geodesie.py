999495a2b35338940fb3b7bc0a54da2215f022b3
Frédéric Rodrigo <frodrigo@openstreetmap.fr>
2012-10-08 21:58:20 +0200

    Add geodesic site to merge analyser

--- analyser_merge_geodesie.py
+++ analyser_merge_geodesie.py
@@ -20,7 +20,6 @@
 ##                                                                       ##
 ###########################################################################
 
-import re
 from Analyser_Merge import Analyser_Merge
 
 
@@ -36,12 +35,12 @@ class Analyser_Merge_Geodesie(Analyser_Merge):
     """
 
     def __init__(self, config, logger = None):
-        self.missing_official = {"item":"8070", "class": 1, "level": 3, "tag": ["merge", "building"], "desc":{"fr":"Repère géodésique manquant"} }
+        self.missing_official = {"item":"8070", "class": 1, "level": 3, "tag": ["merge"], "desc":{"fr":"Repère géodésique manquant"} }
         Analyser_Merge.__init__(self, config, logger)
         self.officialURL = "http://geodesie.ign.fr"
         self.officialName = "Fiches géodésiques"
         self.csv_file = "merge_data/geodesie.csv"
-        self.csv_format = "WITH DELIMITER AS ',' NULL AS 'null' CSV"
+        self.csv_format = "WITH DELIMITER AS ',' NULL AS '' CSV"
         self.osmTags = {
             "man_made": "survey_point",
         }
@@ -63,3 +62,37 @@ class Analyser_Merge_Geodesie(Analyser_Merge):
             "description": "description",
         }
         self.text = lambda tags, fields: {"fr":"Repères géodésiques %s" % tags["ref"]}
+
+
+class Analyser_Merge_Geodesie_Site(Analyser_Merge):
+
+    create_table = """
+        id VARCHAR(254) PRIMARY KEY,
+        ref VARCHAR(254),
+        name VARCHAR(254),
+        note VARCHAR(254),
+        network VARCHAR(254),
+        source VARCHAR(254),
+        lat VARCHAR(254),
+        lon VARCHAR(254)
+    """
+
+    def __init__(self, config, logger = None):
+        self.missing_official = {"item":"8070", "class": 2, "level": 3, "tag": ["merge"], "desc":{"fr":"Site géodésique manquant"} }
+        Analyser_Merge.__init__(self, config, logger)
+        self.officialURL = "http://geodesie.ign.fr"
+        self.officialName = "Fiches géodésiques-site"
+        self.csv_file = "merge_data/geodesie_site.csv"
+        self.csv_format = "WITH DELIMITER AS ',' NULL AS '' CSV"
+        self.osmTags = {
+            "type": "site",
+            "site": "geodesic",
+        }
+        self.osmRef = "ref"
+        self.osmTypes = ["relations"]
+        self.sourceTable = "geodesie_site"
+        self.sourceRef = "ref"
+        self.sourceX = "lon"
+        self.sourceY = "lat"
+        self.sourceSRID = "4326"
+        self.text = lambda tags, fields: {"fr":"Site géodésiques %s - %s" % (fields["ref"], fields["name"])}
