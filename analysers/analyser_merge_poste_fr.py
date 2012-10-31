1325f8112637c59b52a9b7a7953a57301ba66f6a
Frédéric Rodrigo <frodrigo@openstreetmap.fr>
2012-10-31 16:09:41 +0100

    Update poste_fr

--- analysers/analyser_merge_poste_fr.py
+++ analysers/analyser_merge_poste_fr.py
@@ -32,21 +32,28 @@ class Analyser_Merge_Poste_Fr(Analyser_Merge):
         identifiant VARCHAR(254) PRIMARY KEY,
         libelle_site VARCHAR(254),
         caracteristique_site VARCHAR(254),
-        complement_adresse VARCHAR(254),
         adresse VARCHAR(254),
+        complement_adresse VARCHAR(254),
         lieu_dit VARCHAR(254),
         code_postal VARCHAR(254),
         localite VARCHAR(254),
         pays VARCHAR(254),
         latitude NUMERIC(10,7),
         longitude NUMERIC(10,7),
+        geocodage VARCHAR(254),
         telephone VARCHAR(254),
         changeur_monnaie VARCHAR(254),
-        photocopieur VARCHAR(254),
         dab VARCHAR(254),
+        dab_timbre VARCHAR(254),
+        photocopieur VARCHAR(254),
         affranchissement_libre_service VARCHAR(254),
-        recharge_moneo VARCHAR(254),
-        monnaie_paris VARCHAR(254)
+        pas_ressaut VARCHAR(254), -- Accessibilité - Absence de ressaut de plus de 2 cm de haut
+        affranchissement_libre_service_audio VARCHAR(254), -- Accessibilité - Automate d'affranchissement avec prise audio
+        boucle_magnetique VARCHAR(254), -- Accessibilité - Boucle magnétique en état de fonctionnement
+        dab_audio VARCHAR(254), -- Accessibilité - Distributeur de billets avec prise audio
+        autonome_fauteuil_roulant VARCHAR(254), -- Accessibilité - Entrée autonome en fauteuil roulant possible
+        vide VARCHAR(254), -- Accessibilité - Pas d'escalier ou bandes de vigilance présentes
+        prioritaire VARCHAR(254) -- Accessibilité - Présence d'un panneau prioritaire
     """
 
     def __init__(self, config, logger = None):
@@ -57,7 +64,7 @@ class Analyser_Merge_Poste_Fr(Analyser_Merge):
         self.officialURL = "http://www.data.gouv.fr/donnees/view/Liste-des-points-de-contact-du-r%C3%A9seau-postal-fran%C3%A7ais-551640"
         self.officialName = "points de contact du réseau postal français"
         self.csv_file = "merge_data/270949f7a9ff7dce81b45d8150279259.csv"
-        self.csv_format = "WITH DELIMITER AS ';' NULL AS 'null' CSV"
+        self.csv_format = "WITH DELIMITER AS ';' NULL AS '' CSV HEADER"
         self.csv_encoding = "ISO-8859-15"
         self.osmTags = {
             "amenity": "post_office",
@@ -72,13 +79,14 @@ class Analyser_Merge_Poste_Fr(Analyser_Merge):
         self.defaultTag = {
             "amenity": "post_office",
             "operator": "La Poste",
-            "source": "data.gouv.fr:LaPoste - 04/2012"
+            "source": "data.gouv.fr:LaPoste - 10/2012"
         }
         self.Annexe = re.compile(' A$')
         self.Principal = re.compile(' PAL$')
+        self.APBP = re.compile(' (AP|BP)$')
         self.defaultTagMapping = {
             "ref:FR:LaPoste": "identifiant",
-            "name": lambda res: re.sub(self.Principal, " Principal", re.sub(self.Annexe, " Annexe", res["libelle_site"])),
+            "name": lambda res: re.sub(self.Principal, " Principal", re.sub(self.Annexe, " Annexe", re.sub(self.APBP, "", res["libelle_site"]))),
             "post_office:type": lambda res: {
                 None: None,
                 "AGENCE POSTALE COMMUNALE": "post_annex",
@@ -92,10 +100,12 @@ class Analyser_Merge_Poste_Fr(Analyser_Merge):
             "copy_facility": lambda res: self.bool[res["photocopieur"]],
             "atm": lambda res: self.bool[res["dab"]],
             "stamping_machine": lambda res: self.bool[res["affranchissement_libre_service"]],
-            "moneo:loading": lambda res: self.bool[res["recharge_moneo"]],
-            # monnaie_paris
+            "wheelchair": lambda res:
+                "yes" if self.bool[res["pas_ressaut"]] and self.bool[res["autonome_fauteuil_roulant"]] else
+                "limited" if self.bool[res["pas_ressaut"]] or self.bool[res["autonome_fauteuil_roulant"]] else
+                "no"
         }
         self.conflationDistance = 1000
-        self.text = lambda tags, fields: {"fr":"Bureau de poste de %s" % ", ".join(filter(lambda x: x!=None, [fields["adresse"], fields["complement_adresse"], fields["lieu_dit"], fields["localite"]]))}
+        self.text = lambda tags, fields: {"fr":"Bureau de poste %s" % ", ".join(filter(lambda x: x!=None, [fields["geocodage"].lower(), fields["adresse"], fields["complement_adresse"], fields["lieu_dit"], fields["localite"]]))}
 
-    bool = {"N": None, "O": "yes"}
+    bool = {"Non": None, "Oui": "yes"}
