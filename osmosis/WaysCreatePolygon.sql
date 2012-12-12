2b4e3f28954e320ee3fc831dbd257e5f91412cd9
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2012-12-12 22:51:45 +0100

    Move to function ways_is_polygon()

--- osmosis/WaysCreatePolygon.sql
+++ osmosis/WaysCreatePolygon.sql
@@ -4,8 +4,20 @@ CREATE INDEX ways_tags ON ways USING gist(tags);
 -- crée des polygones là où il faut
 ALTER TABLE ways ADD COLUMN is_polygon boolean;
 
-UPDATE ways SET is_polygon = (ST_IsClosed(linestring) AND ST_NumPoints(linestring) > 3 AND
-                              NOT (tags ? 'attraction' AND tags->'attraction' = 'roller_coaster'));
+CREATE OR REPLACE FUNCTION ways_is_polygon(nodes bigint[], linestring geometry, tags hstore)
+RETURNS BOOLEAN
+AS $$
+BEGIN
+  RETURN (array_length(nodes,1) > 3 AND
+          nodes[array_lower(nodes,1)] = nodes[array_upper(nodes,1)] AND
+          ST_NumPoints(linestring) > 3 AND ST_IsClosed(linestring) AND
+          ST_IsValid(linestring) AND ST_IsSimple(linestring) AND
+          ST_IsValid(ST_MakePolygon(linestring)) AND
+          NOT (tags ? 'attraction' AND tags->'attraction' = 'roller_coaster'));
+END
+$$ LANGUAGE plpgsql;
+
+UPDATE ways SET is_polygon = ways_is_polygon(nodes, linestring, tags);
 
 CREATE INDEX idx_ways_is_polygon ON ways USING btree (is_polygon);
 
