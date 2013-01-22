339536c1f0ea239bf67de981a47f40baaaee2259
Frédéric Rodrigo <frodrigo@openstreetmap.fr>
2013-01-22 21:47:39 +0100

    Support diff mode into analyser_osmosis_useless

--- analysers/analyser_osmosis_useless.py
+++ analysers/analyser_osmosis_useless.py
@@ -24,33 +24,35 @@ from Analyser_Osmosis import Analyser_Osmosis
 
 sql10 = """
 SELECT
-    id,
+    nodes.id,
     relation_members.relation_id,
     ST_ASText(geom)
 FROM
-    nodes AS nodes
+    {0}nodes AS nodes
     JOIN relation_members ON
         member_id = id AND
         member_type = 'N' AND
         member_role = ''
+    {1}
 WHERE
-    array_length(akeys(delete(delete(delete(tags, 'created_by'), 'source'), 'note:qadastre')), 1) IS NULL
+    array_length(akeys(delete(delete(delete(nodes.tags, 'created_by'), 'source'), 'note:qadastre')), 1) IS NULL
 ;
 """
 
 sql20 = """
 SELECT
-    id,
+    ways.id,
     relation_members.relation_id,
     ST_ASText(way_locate(linestring))
 FROM
-    ways
+    {0}ways AS ways
     LEFT JOIN relation_members ON
         member_id = id AND
         member_type = 'W'
+    {1}
 WHERE
     (member_role IS NULL OR member_role = '') AND
-    array_length(akeys(delete(delete(delete(tags, 'created_by'), 'source'), 'note:qadastre')), 1) IS NULL
+    array_length(akeys(delete(delete(delete(ways.tags, 'created_by'), 'source'), 'note:qadastre')), 1) IS NULL
 ;
 """
 
@@ -58,11 +60,21 @@ class Analyser_Osmosis_Useless(Analyser_Osmosis):
 
     def __init__(self, config, logger = None):
         Analyser_Osmosis.__init__(self, config, logger)
-        self.classs[1] = {"item":"1140", "level": 3, "tag": [], "desc":{"fr":"Nœud sans tag ou rôle", "en":"Missing tag or role on node"} }
-        self.classs[2] = {"item":"1140", "level": 3, "tag": [], "desc":{"fr":"Way sans tag ou rôle", "en":"Missing tag or role on way"} }
+        self.classs_change[1] = {"item":"1140", "level": 3, "tag": [], "desc":{"fr":"Nœud sans tag ou rôle", "en":"Missing tag or role on node"} }
+        self.classs_change[2] = {"item":"1140", "level": 3, "tag": [], "desc":{"fr":"Way sans tag ou rôle", "en":"Missing tag or role on way"} }
         self.callback10 = lambda res: {"class":1, "data":[self.node_full, self.relation_full, self.positionAsText]}
         self.callback20 = lambda res: {"class":2, "data":[self.way_full, self.relation_full if res[1] else None, self.positionAsText]}
 
-    def analyser_osmosis(self):
-        self.run(sql10, self.callback10)
-        self.run(sql20, self.callback20)
+    def analyser_osmosis_all(self):
+        self.run(sql10.format("", ""), self.callback10)
+        self.run(sql20.format("", ""), self.callback20)
+
+    def analyser_osmosis_touched(self):
+        touched_relations = """
+    JOIN touched_relations AS relations ON
+        relation_members.relation_id = relations.id
+"""
+        self.run(sql10.format("touched_", ""), self.callback10)
+        self.run(sql10.format("", touched_relations), self.callback10)
+        self.run(sql20.format("touched_", ""), self.callback20)
+        self.run(sql20.format("", touched_relations), self.callback20)
