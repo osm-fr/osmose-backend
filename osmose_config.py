249fa77482a1cff82dc9d29e3529b2630f3c0eed
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2013-01-29 22:05:18 +0100

    Add Mali

--- osmose_config.py
+++ osmose_config.py
@@ -284,6 +284,7 @@ default_country_simple("africa", "burundi", 195269, {"country": "BI"}, download_
 default_country_simple("africa", "cameroon", 192830, {"country": "CM"}, download_repo=OSMFR)
 default_country_simple("africa", "central_african_republic", 192790, {"country": "CF"}, download_repo=OSMFR)
 default_country_simple("africa", "chad", 2361304, {"country": "TD"}, download_repo=OSMFR)
+default_country_simple("africa", "mali", 192785, {"country": "ML", "language": "fr"}, download_repo=OSMFR)
 
 default_country_simple("central-america", "haiti", 307829, {"country": "HT"},
                        download_repo=GEOFABRIK, download_country="haiti-and-domrep")
