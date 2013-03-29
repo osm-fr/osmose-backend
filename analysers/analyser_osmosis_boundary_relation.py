9dde33a0fedcf86add71afb7d919ba46c013cb4e
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2013-03-29 22:38:54 +0100

    Rename column admin_centre to has_admin_centre in analyser_osmosis_boundary_relation.py

--- analysers/analyser_osmosis_boundary_relation.py
+++ analysers/analyser_osmosis_boundary_relation.py
@@ -27,7 +27,7 @@ DROP TABLE IF EXISTS admin;
 CREATE TEMP TABLE admin AS
 SELECT
     relations.id,
-    relation_members.member_role IS NOT NULL AS admin_centre,
+    (relation_members.member_role IS NOT NULL) AS has_admin_centre,
     relations.tags AS rtags,
     nodes.tags AS ntags,
     ways.tags AS wtags
@@ -58,7 +58,7 @@ SELECT
 FROM
     admin
 WHERE
-    NOT admin_centre
+    NOT has_admin_centre
 """
 
 sql20 = """
