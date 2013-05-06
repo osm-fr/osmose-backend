703109de9694349fda1964893c426e20664dd239
Frédéric Rodrigo <frodrigo@openstreetmap.fr>
2013-05-06 11:02:52 +0200

    Generalize osmosis_deadend

--- analysers/analyser_osmosis_deadend.py
+++ analysers/analyser_osmosis_deadend.py
@@ -23,14 +23,14 @@
 from Analyser_Osmosis import Analyser_Osmosis
 
 sql10 = """
-CREATE TEMP TABLE cycleway_ends AS
+CREATE TEMP TABLE way_ends AS
 SELECT
     ends(nodes) AS id
 FROM
     {0}ways AS ways
 WHERE
     tags?'highway' AND
-    tags->'highway' = 'cycleway'
+    tags->'highway' IN ('cycleway', 'motorway', 'motorway_link', 'trunk', 'trunk_link', 'primary', 'primary_link', 'secondary', 'secondary_link', 'link_tertiary')
 """
 
 sql20 = """
@@ -38,9 +38,9 @@ SELECT
     way_nodes.node_id,
     ST_AsText(nodes.geom)
 FROM
-    cycleway_ends
+    way_ends
     JOIN way_nodes ON
-        cycleway_ends.id = way_nodes.node_id
+        way_ends.id = way_nodes.node_id
     JOIN nodes ON
         way_nodes.node_id = nodes.id
 GROUP BY
@@ -54,7 +54,7 @@ class Analyser_Osmosis_DeadEnd(Analyser_Osmosis):
 
     def __init__(self, config, logger = None):
         Analyser_Osmosis.__init__(self, config, logger)
-        self.classs_change[1] = {"item":"1210", "level": 1, "tag": ["highway", "cycleway"], "desc":{"fr": u"Voie cyclable non connectée", "en": u"Unconnected cycleway"} }
+        self.classs_change[1] = {"item":"1210", "level": 1, "tag": ["highway"], "desc":{"fr": u"Voie non connectée", "en": u"Unconnected way"} }
         self.callback20 = lambda res: {"class":1, "data":[self.node_full, self.positionAsText]}
 
     def analyser_osmosis_all(self):
