3821411d80018e82a57cd2308de248d15760dace
Frédéric Rodrigo <frodrigo@openstreetmap.fr>
2013-05-25 15:31:05 +0200

    Detect misc intersections (highway, water, tree)

--- analysers/analyser_osmosis_highway_vs_building.py
+++ analysers/analyser_osmosis_highway_vs_building.py
@@ -45,22 +45,113 @@ WHERE
     NOT highways.tags ? 'area' AND
     buildings.tags->'building' = 'yes' AND
     NOT buildings.tags ? 'wall' AND
-    ST_Intersects(buildings.linestring, highways.linestring) AND
-    NOT ST_Touches(buildings.linestring, highways.linestring)
-;
+    ST_NPoints(buildings.linestring) > 1 AND
+    ST_NPoints(highways.linestring) > 1 AND
+    ST_Crosses(buildings.linestring, highways.linestring)
+"""
+
+sql20 = """
+SELECT
+    tree.id,
+    building.id,
+    ST_AsText(tree.geom)
+FROM
+    nodes AS tree
+    JOIN ways AS building ON
+        tree.geom && building.linestring AND
+        ST_NPoints(highway.linestring) > 2 AND
+        ST_Intersects(tree.geom, ST_MakePolygon(building.linestring))
+    LEFT JOIN relation_members ON
+        relation_members.member_type = 'W' AND
+        relation_members.member_id = building.id
+WHERE
+    relation_members.member_id IS NULL AND
+    tree.tags?'natural' AND
+    tree.tags->'natural' = 'tree' AND
+    building.tags?'building' AND
+    building.tags->'building' != 'no' AND
+    NOT building.tags ? 'wall' AND
+    building.is_polygon
+"""
+
+sql30 = """
+SELECT
+    tree.id,
+    highway.id,
+    ST_AsText(tree.geom)
+FROM
+    nodes AS tree
+    JOIN ways AS highway ON
+        tree.geom && highway.linestring AND
+        ST_NPoints(highway.linestring) > 1 AND
+        ST_Intersects(ST_Buffer(tree.geom::geography, 0.25)::geometry, highway.linestring)
+WHERE
+    tree.tags?'natural' AND
+    tree.tags->'natural' = 'tree' AND
+    highway.tags?'highway' AND
+    highway.tags->'highway' IN (
+        'motorway', 'motorway_link',
+        'trunk', 'trunk_link',
+        'primary', 'primary_link',
+        'secondary', 'secondary_link',
+        'tertiary', 'tertiary_link',
+        'unclassified', 'residential', 'living_street') AND
+    NOT highway.tags?'layer'
+"""
+
+sql40 = """
+SELECT
+    highways.id,
+    water.id,
+    ST_AsText(ST_Centroid(ST_Intersection(highways.linestring, water.linestring)))
+FROM
+    ways AS highways
+    JOIN ways AS water ON
+        highways.linestring && water.linestring AND
+        ST_NPoints(highways.linestring) > 1 AND
+        ST_NPoints(water.linestring) > 1 AND
+        ST_Crosses(highways.linestring, water.linestring)
+WHERE
+    (highways.tags?'highway' OR highways.tags?'railway') AND
+    NOT highways.tags?'tunnel' AND
+    NOT highways.tags?'bridge' AND
+    (
+        (
+            water.tags?'waterway' AND
+            NOT water.tags?'tunnel' AND
+            NOT water.tags?'bridge'
+        ) OR (
+            water.tags?'natural' AND
+            water.tags->'natural' = 'water'
+        )
+    )
 """
 
 class Analyser_Osmosis_Highway_VS_Building(Analyser_Osmosis):
 
     def __init__(self, config, logger = None):
         Analyser_Osmosis.__init__(self, config, logger)
-        self.classs_change[1] = {"item":"1070", "level": 2, "tag": ["highway", "building", "geom"], "desc":{"fr": u"Intersection entre une voie et un bâtiment", "en": u"Way intersecting building"} }
+        self.classs_change[1] = {"item":"1070", "level": 2, "tag": ["highway", "building", "geom"], "desc":{"fr": u"Intersection entre une voie et un bâtiment", "en": u"Highway intersecting building"} }
+        self.classs_change[2] = {"item":"1070", "level": 2, "tag": ["tree", "building", "geom"], "desc":{"fr": u"Intersection entre un arbre et un bâtiment", "en": u"Tree intersecting building"} }
+        self.classs_change[3] = {"item":"1070", "level": 2, "tag": ["highway", "tree", "geom"], "desc":{"fr": u"Arbre très proche d'une voie", "en": u"Tree and highway too close"} }
+        self.classs_change[4] = {"item":"1070", "level": 2, "tag": ["highway", "waterway", "geom"], "desc":{"fr": u"Intersection entre une voie et de l'eau", "en": u"Highway intersecting water"} }
         self.callback10 = lambda res: {"class":1, "data":[self.way_full, self.way_full, self.positionAsText]}
+        self.callback20 = lambda res: {"class":1, "data":[self.node_full, self.way_full, self.positionAsText]}
+        self.callback30 = lambda res: {"class":1, "data":[self.node_full, self.way_full, self.positionAsText]}
+        self.callback40 = lambda res: {"class":1, "data":[self.way_full, self.way_full, self.positionAsText]}
 
     def analyser_osmosis_all(self):
         self.run(sql10.format("", ""), self.callback10)
+        self.run(sql20.format("", ""), self.callback20)
+        self.run(sql30.format("", ""), self.callback30)
+        self.run(sql40.format("", ""), self.callback40)
 
     def analyser_osmosis_touched(self):
-        dup = set()
-        self.run(sql10.format("touched_", ""), lambda res: dup.add(res[0]) or self.callback10(res))
-        self.run(sql10.format("", "touched_"), lambda res: res[0] in dup or dup.add(res[0]) or self.callback10(res))
+        self.run(sql10.format("touched_", ""), self.callback10)
+        self.run(sql10.format("", "touched_"), self.callback10)
+        self.run(sql20.format("touched_", ""), self.callback20)
+        self.run(sql20.format("", "touched_"), self.callback20)
+        self.run(sql30.format("touched_", ""), self.callback30)
+        self.run(sql30.format("", "touched_"), self.callback30)
+        self.run(sql40.format("touched_", ""), self.callback40)
+        self.run(sql40.format("", "touched_"), self.callback40)
