048a10c9ea4bddb55bd2db50913c9d202884785e
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2013-07-07 13:29:33 +0200

    Add plugin to detect way with only one node

---
+++ plugins/Structural_WayOneNode.py
@@ -0,0 +1,33 @@
+#-*- coding: utf-8 -*-
+
+###########################################################################
+##                                                                       ##
+## Copyrights Jocelyn Jaubert 2013                                       ##
+##                                                                       ##
+## This program is free software: you can redistribute it and/or modify  ##
+## it under the terms of the GNU General Public License as published by  ##
+## the Free Software Foundation, either version 3 of the License, or     ##
+## (at your option) any later version.                                   ##
+##                                                                       ##
+## This program is distributed in the hope that it will be useful,       ##
+## but WITHOUT ANY WARRANTY; without even the implied warranty of        ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         ##
+## GNU General Public License for more details.                          ##
+##                                                                       ##
+## You should have received a copy of the GNU General Public License     ##
+## along with this program.  If not, see <http://www.gnu.org/licenses/>. ##
+##                                                                       ##
+###########################################################################
+
+from plugins.Plugin import Plugin
+
+
+class Structural_WayOneNode(Plugin):
+
+    def init(self, logger):
+        Plugin.init(self, logger)
+        self.errors[12401] = { "item": 1240, "level": 2, "tag": ["geom", "fix:chair"], "desc": T_(u"Way with one node") }
+
+    def way(self, data, tags, nds):
+        if len(nds) == 1:
+            return [(12401, 0, {})]
