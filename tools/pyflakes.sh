6ee50320b449b8fc06e6a2a3cd5879ce00b24047
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2014-01-12 19:11:26 +0100

    Add pyflakes.sh

---
+++ tools/pyflakes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd $(dirname $0)
+cd ..
+
+# grep will return 0 only if it founds something, but our script
+# wants to return 0 when it founds nothing!
+pyflakes . | grep -v "\('T_'\|/disabled/\)" && exit 1 || exit 0
