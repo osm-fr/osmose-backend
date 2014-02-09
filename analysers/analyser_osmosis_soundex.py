4ad063c2f4c0e9a79675840c09965c3d3d82a318
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2014-02-09 13:01:01 +0100

    Fix soundex for countries without a configured language

--- analysers/analyser_osmosis_soundex.py
+++ analysers/analyser_osmosis_soundex.py
@@ -246,7 +246,7 @@ class Analyser_Osmosis_Soundex(Analyser_Osmosis):
         self.classs[1] = {"item":"5050", "level": 2, "tag": ["name", "fix:survey"], "desc": T_(u"Soundex test") } # FIXME "menu":"test soundex"
 
     def analyser_osmosis(self):
-        if self.config.options["language"] == "fr":
+        if "language" in self.config.options and self.config.options["language"] == "fr":
             self.run(sql01_fr)
             self.run(sql02_fr)
             self.run(sql03.format("fn_soundex2"))
