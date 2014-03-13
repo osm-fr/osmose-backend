c3346860bca572522d7957521b99618a7cd2a64c
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2014-03-13 17:42:13 +0100

    Add Gironde tourism camp

--- analysers/analyser_merge_tourism_FR_gironde.py
+++ analysers/analyser_merge_tourism_FR_gironde.py
@@ -39,11 +39,11 @@ class Analyser_Merge_Tourism_FR_Gironde_Caravan(Analyser_Merge):
     """
 
     def __init__(self, config, logger = None):
-        self.missing_official = {"item":"8140", "class": 41, "level": 3, "tag": ["merge", "tourism"], "desc": T_(u"Gironde caravan site not integrated") }
-        self.possible_merge   = {"item":"8141", "class": 43, "level": 3, "tag": ["merge", "tourism"], "desc": T_(u"Gironde caravan site, integration suggestion") }
+        self.missing_official = {"item":"8140", "class": 1, "level": 3, "tag": ["merge", "tourism"], "desc": T_(u"Gironde caravan site not integrated") }
+        self.possible_merge   = {"item":"8141", "class": 3, "level": 3, "tag": ["merge", "tourism"], "desc": T_(u"Gironde caravan site, integration suggestion") }
         Analyser_Merge.__init__(self, config, logger)
         self.officialURL = "http://www.datalocale.fr/drupal7/file/liste-aire-publique-camping-cdt33-1"
-        self.officialName = "liste-aire-publique-camping-cdt33"
+        self.officialName = "Liste des aires publiques pour camping-cars de Gironde"
         self.csv_file = "merge_data/tourism_FR_gironde_caravan.csv"
         self.csv_format = "WITH DELIMITER AS ',' NULL AS '' CSV HEADER"
         self.osmTags = {
@@ -63,3 +63,52 @@ class Analyser_Merge_Tourism_FR_Gironde_Caravan(Analyser_Merge):
         }
         self.conflationDistance = 500
         self.text = lambda tags, fields: {"en": u"Caravan site of %s" % fields["nom"], "fr": u"Site camping-cars de %s" % fields["nom"]}
+
+
+class Analyser_Merge_Tourism_FR_Gironde_Camp(Analyser_Merge):
+
+    create_table = """
+        type VARCHAR(254),
+        categorie VARCHAR(254),
+        raison_sociale VARCHAR(254),
+        adresse_1 VARCHAR(254),
+        adresse_2 VARCHAR(254),
+        code_postal VARCHAR(254),
+        commune VARCHAR(254),
+        tel__information_reservation VARCHAR(254),
+        site_web VARCHAR(254),
+        labels_et_marques VARCHAR(254),
+        tourisme_et_handicap VARCHAR(254),
+        longitude VARCHAR(254),
+        latitude VARCHAR(254),
+        column13 VARCHAR(254)
+    """
+
+    def __init__(self, config, logger = None):
+        self.missing_official = {"item":"8140", "class": 11, "level": 3, "tag": ["merge", "tourism"], "desc": T_(u"Gironde camp site not integrated") }
+        self.possible_merge   = {"item":"8141", "class": 13, "level": 3, "tag": ["merge", "tourism"], "desc": T_(u"Gironde camp site, integration suggestion") }
+        Analyser_Merge.__init__(self, config, logger)
+        self.officialURL = "http://www.datalocale.fr/drupal7/file/liste-campings-classes-cdt33-1"
+        self.officialName = "Liste des campings classes et anciennement classes de Gironde"
+        self.csv_file = "merge_data/tourism_FR_gironde_camp.csv"
+        self.csv_format = "WITH DELIMITER AS ',' NULL AS '' CSV HEADER"
+        self.osmTags = {
+            "tourism": "camp_site"
+        }
+        self.osmTypes = ["nodes", "ways"]
+        self.sourceTable = "gironde_camp"
+        self.sourceX = "longitude"
+        self.sourceY = "latitude"
+        self.sourceSRID = "4326"
+        self.defaultTag = {
+            "source": "Observatoire du comité départemental du Tourisme de la Gironde - 09/2013",
+            "tourism": "camp_site",
+        }
+        self.defaultTagMapping = {
+            "name": "raison_sociale",
+            "stars": lambda res: res["categorie"][0] if res["categorie"] != u"Non classé" else None,
+            "website": "site_web",
+        }
+        self.conflationDistance = 300
+        self.text = lambda tags, fields: {"en": u"Caravan site of %s" % fields["nom"], "fr": u"Site camping-cars de %s" % fields["nom"]}
+
