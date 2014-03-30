bd06fd82344ecf5bc6229df3d2804cd48df9be72
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2014-03-30 22:43:08 +0200

    Add simplification of json file in generate-cover.sh

--- tools/generate-cover.sh
+++ tools/generate-cover.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+MAPSHAPER=~/.npm/mapshaper/0.1.17/package/bin/mapshaper
+
 SCRIPT=$(readlink -f $0)
 SCRIPTPATH=$(dirname $SCRIPT)
 
@@ -7,3 +9,6 @@ python $SCRIPTPATH/get-polygons.py > osmose.list
 
 wget -O osmose-cover.json "http://polygons.openstreetmap.fr/get_geojson.py?id=$(cat osmose.list)"
 wget -O osmose-cover.png "http://polygons.openstreetmap.fr/get_image_union.py?id=$(cat osmose.list)"
+
+rm osmose-cover-simplified.json
+$MAPSHAPER --keep-shapes --auto-snap -p 0.001 osmose-cover.json -f geojson -o osmose-cover-simplified.json
