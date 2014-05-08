2aa1c6e0f0372dfc402311a87c064d3d9ca20397
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2014-05-08 13:35:02 +0200

    Fix typo in Analyser_Merge.py

--- analysers/Analyser_Merge.py
+++ analysers/Analyser_Merge.py
@@ -396,7 +396,7 @@ class Analyser_Merge(Analyser_Osmosis):
         if self.missing_official:
             self.run(sql12, lambda res: {
                 "class": self.missing_official["class"],
-                "subclass": str(self.stablehash("%s%s"%(res[0],res[1])),
+                "subclass": str(self.stablehash("%s%s"%(res[0],res[1]))),
                 "self": lambda r: [0]+r[1:],
                 "data": [self.node_new, self.positionAsText],
                 "text": self.text(defaultdict(lambda:None,res[2]), defaultdict(lambda:None,res[3])),
