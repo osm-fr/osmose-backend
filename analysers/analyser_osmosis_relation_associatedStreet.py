73293ef3c62c5186c6538afac19f75a49aa157fe
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2014-07-23 23:40:33 +0200

    Take care of addr:place in addition of addr:street into analyser_osmosis_relation_associatedStreet

--- analysers/analyser_osmosis_relation_associatedStreet.py
+++ analysers/analyser_osmosis_relation_associatedStreet.py
@@ -39,6 +39,7 @@ FROM
 WHERE
     ways.tags?'addr:housenumber' AND
     (NOT ways.tags?'addr:street') AND
+    (NOT ways.tags?'addr:place') AND
     relations.id IS NULL
 ;
 """
@@ -60,6 +61,7 @@ FROM
 WHERE
     nodes.tags?'addr:housenumber' AND
     (NOT nodes.tags?'addr:street') AND
+    (NOT nodes.tags?'addr:place') AND
     relations.id IS NULL
 ;
 """
@@ -195,7 +197,7 @@ SELECT
     nodes.id,
     ST_Transform(geom, {0}) AS geom,
     nodes.tags->'addr:housenumber' AS number,
-    nodes.tags->'addr:street' AS street
+    coalesce(nodes.tags->'addr:street', nodes.tags->'addr:place') AS street
 FROM
     nodes
     LEFT JOIN relation_members ON
@@ -205,14 +207,14 @@ FROM
 WHERE
     relation_members IS NULL AND
     nodes.tags?'addr:housenumber' AND
-    nodes.tags?'addr:street'
+    (nodes.tags?'addr:street' OR nodes.tags?'addr:place')
 ) UNION (
 SELECT
     'W'::CHAR(1) AS type,
     ways.id,
     ST_Transform(ST_Centroid(linestring), {0}) AS geom,
     ways.tags->'addr:housenumber' AS number,
-    ways.tags->'addr:street' AS street
+    coalesce(ways.tags->'addr:street', ways.tags->'addr:place') AS street
 FROM
     ways
     LEFT JOIN relation_members ON
@@ -223,7 +225,7 @@ WHERE
     ST_NPoints(linestring) > 1 AND
     relation_members IS NULL AND
     ways.tags?'addr:housenumber' AND
-    ways.tags?'addr:street'
+    (ways.tags?'addr:street' OR ways.tags?'addr:place')
 ) UNION (
 SELECT
     'N'::CHAR(1) AS type,
@@ -244,7 +246,7 @@ FROM
         relations.tags?'name'
 WHERE
     nodes.tags?'addr:housenumber' AND
-    nodes.tags?'addr:street'
+    (nodes.tags?'addr:street' OR nodes.tags?'addr:place')
 ) UNION (
 SELECT
     'W'::CHAR(1) AS type,
@@ -266,7 +268,7 @@ FROM
 WHERE
     ST_NPoints(linestring) > 1 AND
     ways.tags?'addr:housenumber' AND
-    ways.tags?'addr:street'
+    (ways.tags?'addr:street' OR ways.tags?'addr:place')
 )
 """
 
@@ -513,7 +515,7 @@ class Analyser_Osmosis_Relation_AssociatedStreet(Analyser_Osmosis):
 
     def __init__(self, config, logger = None):
         Analyser_Osmosis.__init__(self, config, logger)
-        self.classs[1] = {"item":"2060", "level": 3, "tag": ["addr", "relation", "fix:chair"], "desc": T_(u"addr:housenumber without addr:street must be in a associatedStreet relation") }
+        self.classs[1] = {"item":"2060", "level": 3, "tag": ["addr", "relation", "fix:chair"], "desc": T_(u"addr:housenumber without addr:street or addr:place must be in a associatedStreet relation") }
         self.classs_change[2] = {"item":"2060", "level": 2, "tag": ["addr", "relation", "fix:chair"], "desc": T_(u"No street role") }
         self.classs_change[3] = {"item":"2060", "level": 2, "tag": ["addr", "fix:chair"], "desc": T_(u"street role is not an highway") }
         self.classs_change[4] = {"item":"2060", "level": 3, "tag": ["addr", "relation", "fix:chair"], "desc": T_(u"Roleless member") }
