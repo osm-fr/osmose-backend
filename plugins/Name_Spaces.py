959f4e06b0ede3960b09025d214b9743763c405e
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2014-11-08 18:55:36 +0100

    Fix strings for plugin Name_Spaces

--- plugins/Name_Spaces.py
+++ plugins/Name_Spaces.py
@@ -36,11 +36,11 @@ class Name_Spaces(Plugin):
         err = []
         name = tags[u"name"]
         if u"  " in name:
-            err.append((903, 0, T_("Name contains two successive spaces")))
+            err.append((903, 0, T_("Name contains successive spaces")))
         if name.endswith(u" "):
-            err.append((903, 1, T_("Name begins with a space")))
+            err.append((903, 1, T_("Name ends with a space")))
         if name.startswith(" "):
-            err.append((903, 2, T_("Name ends with a space")))
+            err.append((903, 2, T_("Name begins with a space")))
 
         if len(err) > 0:
             name = re.sub(r' +', ' ', name.strip())
