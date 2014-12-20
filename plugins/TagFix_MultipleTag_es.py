e0ac7f3a4275627718a7bd362c1cb88263331734
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2014-12-20 17:58:21 +0100

    Add TagFix_MultipleTag_es

---
+++ plugins/TagFix_MultipleTag_es.py
@@ -0,0 +1,78 @@
+#-*- coding: utf-8 -*-
+
+###########################################################################
+##                                                                       ##
+## Copyrights Frédéric Rodrigo 2014                                      ##
+##                                                                       ##
+## This program is free software: you can redistribute it and/or modify  ##
+## it under the terms of the GNU General Public License as published by  ##
+## the Free Software Foundation, either version 3 of the License, or     ##
+## (at your option) any later version.                                   ##
+##                                                                       ##
+## This program is distributed in the hope that it will be useful,       ##
+## but WITHOUT ANY WARRANTY; without even the implied warranty of        ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         ##
+## GNU General Public License for more details.                          ##
+##                                                                       ##
+## You should have received a copy of the GNU General Public License     ##
+## along with this program.  If not, see <http://www.gnu.org/licenses/>. ##
+##                                                                       ##
+###########################################################################
+
+from plugins.Plugin import Plugin
+
+
+class TagFix_MultipleTag_es(Plugin):
+
+    only_for = ["es"]
+
+    def init(self, logger):
+        Plugin.init(self, logger)
+        self.errors[30326] = { "item": 3032, "level": 1, "tag": ["tag", "fix:chair"], "desc": T_(u"Watch multiple tags") }
+
+        import re
+        self.Punta = re.compile(u"punta .*", re.IGNORECASE)
+        self.Panaderia = re.compile(u"panader.a (.*)", re.IGNORECASE)
+
+    def node(self, data, tags):
+        err = []
+
+        if not "name" in tags:
+            return err
+
+        if not "place" in tags:
+            if self.Punta.match(tags["name"]):
+                err.append({"class": 30326, "subclass": 0, "fix": {"natural": "cape", "place": "locality"} })
+        if not "shop" in tags:
+            panaderia = self.Panaderia.match(tags["name"])
+            if panaderia:
+                err.append({"class": 30326, "subclass": 0, "fix": {"+": {"shop": "bakery"}, "~": {"name": panaderia.group(1)} }})
+
+        return err
+
+    def way(self, data, tags, nds):
+        return self.node(data, tags)
+
+    def relation(self, data, tags, members):
+        return self.node(data, tags)
+
+###########################################################################
+from plugins.Plugin import TestPluginCommon
+
+class Test(TestPluginCommon):
+    def test(self):
+        a = TagFix_MultipleTag_es(None)
+        class _config:
+            options = {"language": "es"}
+        class father:
+            config = _config()
+        a.father = father()
+        a.init(None)
+        for t in [{"name": u"Punta de Maisi"},
+                  {"name": u"Panadería Doña Neli"},
+                 ]:
+            self.check_err(a.node(None, t), t)
+
+        for t in [{"name": u"Panadería Doña Neli", "shop": "b"},
+                 ]:
+            assert not a.way(None, t, None), t
