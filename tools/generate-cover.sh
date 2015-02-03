219c70a0b658f6d754f9fbdf8116c235fa9b918f
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2015-02-03 23:14:10 +0100

    Generate cover in topojson.pbf

--- tools/generate-cover.sh
+++ tools/generate-cover.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-MAPSHAPER=~/.npm/mapshaper/0.1.17/package/bin/mapshaper
+MAPSHAPER=~/.npm/mapshaper/0.2.14/package/bin/mapshaper
+JSON2GEOBUF=~/.npm/geobuf/1.0.1/package/bin/json2geobuf
 
 SCRIPT=$(readlink -f $0)
 SCRIPTPATH=$(dirname $SCRIPT)
@@ -8,7 +9,7 @@ SCRIPTPATH=$(dirname $SCRIPT)
 python $SCRIPTPATH/get-polygons.py > osmose.list
 
 wget -O osmose-cover.json "http://polygons.openstreetmap.fr/get_geojson.py?id=$(cat osmose.list)"
-wget -O osmose-cover.png "http://polygons.openstreetmap.fr/get_image_union.py?id=$(cat osmose.list)"
 
-rm osmose-cover-simplified.json
-$MAPSHAPER --keep-shapes --auto-snap -p 0.001 osmose-cover.json -f geojson -o osmose-cover-simplified.json
+rm -f osmose-cover-simplified.topojson
+$MAPSHAPER osmose-cover.json -simplify 0.1% keep-shapes -o osmose-cover-simplified.topojson
+$JSON2GEOBUF osmose-cover-simplified.topojson > osmose-cover-simplified.topojson.pbf
