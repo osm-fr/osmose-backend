5c87c9a40cf7f4eb488b3fabd3739e7f3cc3455c
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2015-09-20 18:07:57 +0200

    Update data source on analyser_merge_school_FR. github #72

--- analysers/analyser_merge_school_FR.py
+++ analysers/analyser_merge_school_FR.py
@@ -3,7 +3,7 @@
 
 ###########################################################################
 ##                                                                       ##
-## Copyrights Frédéric Rodrigo 2012                                      ##
+## Copyrights Frédéric Rodrigo 2012-2015                                 ##
 ##                                                                       ##
 ## This program is free software: you can redistribute it and/or modify  ##
 ## it under the terms of the GNU General Public License as published by  ##
@@ -31,14 +31,14 @@ class _Analyser_Merge_School_FR(Analyser_Merge):
         self.possible_merge   = {"item":"8031", "class": classs+3, "level": 3, "tag": ["merge"], "desc": T_(u"School, integration suggestion") }
         Analyser_Merge.__init__(self, config, logger,
             Source(
-                url = "http://www.data.gouv.fr/donnees/view/G%C3%A9olocalisation-des-%C3%A9tablissements-d%27enseignement-du-premier-degr%C3%A9-et-du-second-degr%C3%A9-du-minist%C3%A8re-d-30378093",
-                name = u"établissements d'enseignement du premier degré et du second degré - " + officialName,
+                url = u"https://www.data.gouv.fr/fr/datasets/adresse-et-geolocalisation-des-etablissements-denseignement-du-premier-et-second-degres/",
+                name = u"Adresse et géolocalisation des établissements d'enseignement du premier et second degrés - " + officialName,
                 file = "school_FR.csv.bz2",
-                encoding = "ISO-8859-15",
-                csv = CSV(separator = ";", null = "null")),
+                encoding = "ISO-8859-15"),
             Load("X", "Y", srid = srid, table = "School_FR",
-                filter = lambda t: t.replace("; ", ";null").replace(";.", ";null").replace("Ecole", u"École").replace("Saint ", "Saint-").replace("Sainte ", "Sainte-").replace(u"élementaire", u"élémentaire"),
-                where = lambda res: res["_x"] and res["_y"] and self.is_in(float(res["_x"]), float(res["_y"]))),
+                filter = lambda t: t.replace("Ecole", u"École").replace("Saint ", "Saint-").replace("Sainte ", "Sainte-").replace(u"élementaire", u"élémentaire"),
+                select = {"etat_etablissement": ["1", "3"]},
+                where = lambda res: res["nature_uai"][0] != "8" and res["code_postal_uai"] and self.is_in(res["code_postal_uai"])),
             Mapping(
                 select = Select(
                     types = ["nodes", "ways", "relations"],
@@ -48,68 +48,117 @@ class _Analyser_Merge_School_FR(Analyser_Merge):
                 generate = Generate(
                     static = {
                         "amenity": "school",
-                        "source": u"data.gouv.fr:Ministère de l'Éducation nationale, de la Jeunesse et de la Vie associative - 05/2012"},
+                        "source": u"data.gouv.fr:Éducation Nationale - 04/2014"},
                     mapping = {
                         "ref:UAI": "numero_uai",
-                        "school:FR": self.School_FR,
+                        "school:FR": lambda res: self.School_FR_nature_uai[res["nature_uai"]],
                         "name": "appellation_officielle_uai",
-                        "operator:type": lambda res: "private" if res["denomination_principale_uai"] and "PRIVE" in res["denomination_principale_uai"] else None},
-                    text = lambda tags, fields: {"en":fields["appellation_officielle_uai"] if fields["appellation_officielle_uai"] else ""} )))
+                        "operator:type": lambda res: "private" if res["secteur_public_prive"] == "PR" else None},
+                    text = self.text)))
 
-    School_FR_token = {
-        "ECOLE ELEMENTAIRE": u"élémentaire",
-        "ECOLE DE NIVEAU ELEMENTAIRE": u"élémentaire",
-        "ECOLE MATERNELLE": u"maternelle",
-        "ECOLE PRIMAIRE": u"primaire",
-        "COLLEGE": u"collège",
-        "LYCEE": u"lycée",
-        "LP": u"lycée",
-        "LYC": u"lycée",
-        "ECOLE SECONDAIRE": u"secondaire",
+    def text(self, tags, fields):
+      lib = ', '.join(filter(lambda x: x and x != 'None', [fields["appellation_officielle_uai"], fields["adresse_uai"], fields["lieu_dit_uai"], fields["boite_postale_uai"], fields["code_postal_uai"], fields["localite_acheminement_uai"], fields[""]]))
+      return {
+          "en": lib + " (positioned: %s, matching: %s)" % (self.School_FR_loc[fields["loc"]]["en"], self.School_FR_app[fields["app"]]["en"]),
+          "fr": lib + " (position : %s, appariement : %s)" % (self.School_FR_loc[fields["loc"]]["fr"], self.School_FR_app[fields["app"]]["fr"]),
+      }
+
+    School_FR_loc = {
+        "None": {"en": u"none", "fr": u"aucun"},
+        "AUCUN": {"en": u"none", "fr": u"aucun"},
+        "BATIMENT": {"en": u"building", "fr": u"bâtiment"},
+        "CENTRE_PARCELLE_PROJETE": {"en": u"parcel", "fr": u"parcelle"},
+        "COMMUNE": {"en": u"city", "fr": u"commune"},
+        "DEFAUT_DE_NUMERO": {"en": u"missing number", "fr": u"défaut de numéro"},
+        "DEFAUT_DE_TRONCON": {"en": u"missing street", "fr": u"défaut de troncon"},
+        "DIFF_NOM": {"en": u"different name", "fr": u"nom dfférent"},
+        "DIFF_TYPE": {"en": u"different type", "fr": u"type différent"},
+        "INTERPOLATION": {"en": u"interpolated", "fr": u"interpolation"},
+        "MANUEL": {"en": u"manual", "fr": u"manuel"},
+        "PLAQUE_ADRESSE": {"en": u"house number", "fr": u"plaque adresse"},
+        "SIMILAIRE": {"en": u"similar", "fr": u"similaire"},
+        "ZONE_ADRESSAGE": {"en": u"addresse area", "fr": u"zone d'adressage"},
     }
 
-    def School_FR(self, res):
-        for k, v in self.School_FR_token.items():
-            if res["lib_nature"].startswith(k):
-                return v
-        return res["lib_nature"]
+    School_FR_app = {
+        "None": {"en": u"none", "fr": u"aucun"},
+        "AUCUN": {"en": u"none", "fr": u"aucun"},
+        "DIFF_NOM": {"en": u"different name", "fr": u"nom dfférent"},
+        "DIFF_TYPE": {"en": u"different type", "fr": u"type différent"},
+        "MANUEL": {"en": u"manual", "fr": u"manuel"},
+        "SIMILAIRE": {"en": u"similar", "fr": u"similaire"},
+    }
 
+    School_FR_nature_uai = {
+        "101": u"maternelle",
+        "102": u"maternelle",
+        "103": u"maternelle",
+        "111": u"maternelle",
+        "151": u"élémentaire",
+        "152": u"élémentaire",
+        "153": u"élémentaire",
+        "160": None,
+        "161": None,
+        "162": u"élémentaire",
+        "169": u"primaire",
+        "170": None,
+        "300": u"lycée",
+        "301": u"lycée",
+        "302": u"lycée",
+        "306": u"lycée",
+        "307": u"lycée",
+        "310": u"lycée",
+        "312": u"secondaire",
+        "315": u"lycée",
+        "320": u"lycée",
+        "332": None,
+        "334": None,
+        "335": None,
+        "336": None,
+        "340": u"secondaire",
+        "349": None,
+        "350": u"secondaire",
+        "352": u"secondaire",
+        "370": None,
+        "380": None,
+        "390": None,
+    }
 
 class Analyser_Merge_School_FR_Metropole(_Analyser_Merge_School_FR):
     def __init__(self, config, logger = None):
         _Analyser_Merge_School_FR.__init__(self, config, 0, u"Métropole", 2154, logger)
 
-    def is_in(self, x, y):
-        return x > -357823.2365 and x < 1313632.3628 and y > 6037008.6939 and y < 7230727.3772
+    def is_in(self, code_postal):
+        return code_postal[0:2] != "97"
 
 
 class Analyser_Merge_School_FR_Guadeloupe(_Analyser_Merge_School_FR):
     def __init__(self, config, logger = None):
         _Analyser_Merge_School_FR.__init__(self, config, 10, u"Guadeloupe", 2970, logger)
 
-    def is_in(self, x, y):
-        return x > 627264 and x < 693664 and y > 1755023 and y < 1826414
+    def is_in(self, code_postal):
+        return code_postal[0:3] == "971"
 
 
 class Analyser_Merge_School_FR_Guyane(_Analyser_Merge_School_FR):
     def __init__(self, config, logger = None):
         _Analyser_Merge_School_FR.__init__(self, config, 20, u"Guyane", 2972, logger)
 
-    def is_in(self, x, y):
-        return x > 166254.8015 and x < 653463.1459 and y > 238817.6898 and y < 1002848.2203
+    def is_in(self, code_postal):
+        return code_postal[0:3] == "973"
 
 
 class Analyser_Merge_School_FR_Reunion(_Analyser_Merge_School_FR):
     def __init__(self, config, logger = None):
         _Analyser_Merge_School_FR.__init__(self, config, 30, u"Réunion", 2975, logger)
 
-    def is_in(self, x, y):
-        return x > 312514 and x < 379436 and y > 7634041 and y < 7693301
+    def is_in(self, code_postal):
+        return code_postal[0:3] == "974"
 
 
 class Analyser_Merge_School_FR_Martinique(_Analyser_Merge_School_FR):
     def __init__(self, config, logger = None):
         _Analyser_Merge_School_FR.__init__(self, config, 40, u"Martinique", 2973, logger)
 
-    def is_in(self, x, y):
-        return x > 689211 and x < 735159 and y > 1591528 and y < 1646407
+    def is_in(self, code_postal):
+        return code_postal[0:3] == "972"
