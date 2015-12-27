e035b841fb84ecf480a7a14e6de0bfec1653783a
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2015-12-27 17:35:14 +0100

    Add translation to string in analyser_osmosis_relation_associatedStreet

--- analysers/analyser_osmosis_relation_associatedStreet.py
+++ analysers/analyser_osmosis_relation_associatedStreet.py
@@ -561,7 +561,8 @@ class Analyser_Osmosis_Relation_AssociatedStreet(Analyser_Osmosis):
             self.run(sql61)
             self.run(sql62, lambda res: {"class":6, "subclass":1,
                 "data":[lambda t: self.typeMapping[res[1]](t), None, self.positionAsText],
-                "text":{"fr": u"Multiples numéros \"%s\" dans la voie \"%s\"" % (", ".join(lambda z: z, res[4:]), res[3]), "en": u"Multiple numbers \"%s\" in way \"%s\"" % (", ".join(lambda z: z, res[4:]), res[3])} } )
+                "text": T_(u"Multiple numbers \"%(numbers)s\" in way \"%(way)s\"", {"numbers": ", ".join(lambda z: z, res[4:]), "way": res[3]}),
+                } )
         self.run(sql70)
         self.run(sql80, lambda res: {"class":7, "subclass":1, "data":[self.relation_full, self.positionAsText], "text":{"en": res[2]}} )
         self.run(sql90)
