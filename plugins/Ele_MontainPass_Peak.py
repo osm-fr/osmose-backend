f83f989bba70c7b5e0961925c9ab8ad6ab1a37c3
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2016-02-13 17:30:58 +0100

    Merge Name_UpperCasePeak and Name_UpperCaseMontainPass to Ele_MontainPass_Peak. github #89

--- plugins/Ele_MontainPass_Peak.py
+++ plugins/Ele_MontainPass_Peak.py
@@ -22,34 +22,34 @@
 from plugins.Plugin import Plugin
 
 
-class Name_UpperCaseMontainPass(Plugin):
+class Ele_MontainPass_Peak(Plugin):
 
     def init(self, logger):
         Plugin.init(self, logger)
         self.errors[804] = { "item": 2020, "level": 3, "tag": ["tag", "fix:survey"], "desc": T_(u"Missing altitude") }
 
     def node(self, data, tags):
-        if u"mountain_pass" not in tags:
-            return
-        if tags["mountain_pass"] not in ["yes", "1"]:
-            return
         err = []
-        if u"ele" not in tags:
+        if tags.get("mountain_pass") in ["yes", "1"] and u"ele" not in tags:
             err.append((804, 0, {}))
+        if tags.get("natural") in ["peak"] and u"ele" not in tags:
+            err.append((804, 1, {}))
         return err
 
     def way(self, data, tags, nds):
         return self.node(data, tags)
+
 
 ###########################################################################
 from plugins.Plugin import TestPluginCommon
 
 class Test(TestPluginCommon):
     def test(self):
-        a = Name_UpperCaseMontainPass(None)
+        a = Ele_MontainPass_Peak(None)
         a.init(None)
         for t in [{"mountain_pass": "yes"},
                   {"mountain_pass": "1"},
+                  {"natural": "peak"},
                  ]:
             self.check_err(a.node(None, t), t)
             self.check_err(a.way(None, t, None), t)
@@ -58,5 +58,6 @@ class Test(TestPluginCommon):
                   {"mountain_pass": "no"},
                   {"mountain_pass": "-1"},
                   {"mountain_pass": "yes", "ele": "1000"},
+                  {"natural": "peak", "ele": "1000"},
                  ]:
             assert not a.node(None, t), t
