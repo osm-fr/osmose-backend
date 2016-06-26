fab9d69a6e46d2b003ee17131e9d33c8d4e90527
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2016-06-26 23:02:41 +0200

    Fix polygon used by france_polynesie

--- osmose_config.py
+++ osmose_config.py
@@ -285,7 +285,7 @@ default_country_fr("north-america", "france_saintpierreetmiquelon", 233377,
                    proj=32621, download_repo=OSMFR, download_country="saint_pierre_et_miquelon")
 default_country_fr("south-america", "france_wallisetfutuna", 290162,
                    proj=32701, download_repo=OSMFR, download_country="wallis_et_futuna")
-default_country_fr("south-america", "france_polynesie", 1363099,
+default_country_fr("south-america", "france_polynesie", 3412620,
                    proj=32706, download_repo=OSMFR, download_country="polynesie")
 default_country("australia-oceania", "france_nouvellecaledonie", 3407643,
                    download_repo=GEOFABRIK, download_country="new-caledonia", analyser_options={"country": "NC", "language": "fr", "proj": 3163})
