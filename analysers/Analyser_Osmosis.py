3bafd6ca625a99324389f4d310d5a98dc55c9651
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2016-07-22 21:33:34 +0200

    Don't run EXPLAIN on basic table creation

--- analysers/Analyser_Osmosis.py
+++ analysers/Analyser_Osmosis.py
@@ -131,7 +131,7 @@ class Analyser_Osmosis(Analyser):
     def run0(self, sql, callback = None):
         if self.explain_sql:
             self.logger.log(sql.strip())
-        if self.explain_sql and (sql.strip().startswith("SELECT") or sql.strip().startswith("CREATE TABLE")) and not ';' in sql[:-1]:
+        if self.explain_sql and (sql.strip().startswith("SELECT") or sql.strip().startswith("CREATE TABLE")) and not ';' in sql[:-1] and " AS " in sql:
             sql_explain = "EXPLAIN " + sql.split(";")[0]
             self.giscurs.execute(sql_explain)
             for res in self.giscurs.fetchall():
