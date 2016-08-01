a436aae6b9588207e10f32ca9759f029f4a88709
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2016-08-01 20:55:23 +0200

    Remove check_date tag from Construction. github #123

--- plugins/Construction.py
+++ plugins/Construction.py
@@ -32,7 +32,7 @@ class Construction(Plugin):
         self.errors[4070] = { "item": 4070, "level": 2, "tag": ["tag", "fix:survey"], "desc": T_(u"Finished construction") }
 
         self.tag_construction = ["highway", "landuse", "building"]
-        self.tag_date = ["opening_date", "check_date", "open_date", "construction:date", "temporary:date_on", "date_on"]
+        self.tag_date = ["opening_date", "open_date", "construction:date", "temporary:date_on", "date_on"]
         self.default_date = datetime.datetime(9999, 12, 1)
         self.today = datetime.datetime.today()
         self.date_limit = datetime.timedelta(days=2 * 365)
