321e6ce8742f793deb156df0d5636a92f69e86c0
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2016-12-24 15:37:08 +0100

    Support also multipolygon in analyser_osmosis_polygon. github #171

--- analysers/analyser_osmosis_polygon.py
+++ analysers/analyser_osmosis_polygon.py
@@ -51,9 +51,70 @@ FROM
             WHERE
                 NOT is_polygon AND
                 NOT (tags ? 'attraction' AND tags->'attraction' = 'roller_coaster') AND
-                nodes[array_lower(nodes,1)] = nodes[array_upper(nodes,1)] AND
-                ST_NumPoints(linestring) > 3 AND ST_IsClosed(linestring) AND
+                nodes[1] = nodes[array_length(nodes,1)] AND
+                ST_NumPoints(linestring) > 3 AND
+                ST_IsClosed(linestring) AND
                 NOT ST_IsValid(ST_MakePolygon(linestring))
+        ) AS p
+) AS tmp
+WHERE
+    NOT ST_IsEmpty(selfinter)
+"""
+
+sql20 = """
+DROP TABLE IF EXISTS relation_linestrings;
+CREATE TEMP TABLE relation_linestrings AS
+SELECT
+  relations.id,
+  ST_LineMerge(ST_Collect(linestring)) AS linestring
+FROM
+  {0}relations AS relations
+  JOIN relation_members ON
+    relation_members.relation_id = relations.id AND
+    relation_members.member_type = 'W' AND
+    relation_members.member_role = 'outer'
+  JOIN {1}ways AS ways ON
+    ways.id = relation_members.member_id AND
+    ST_NumPoints(ways.linestring) >= 2
+WHERE
+  relations.tags != ''::hstore AND
+  relations.tags?'type' AND
+  relations.tags->'type' IN ('multipolygon', 'boundary')
+GROUP BY
+  relations.id
+"""
+
+sql21 = """
+SELECT
+    id,
+    ST_AsText(selfinter),
+    detail
+FROM
+(
+    SELECT
+        id,
+        ST_Difference(
+          ST_Endpoint(
+            ST_Union(
+              ST_Exteriorring(polygon),
+              ST_Endpoint(st_exteriorring(polygon))
+            )
+          ),
+          ST_Endpoint(ST_Exteriorring(polygon))
+        ) AS selfinter,
+        ST_IsValidReason(polygon) AS detail
+      FROM
+        (
+SELECT
+  id,
+  ST_MakePolygon(linestring) AS polygon
+FROM
+  relation_linestrings
+WHERE
+  (ST_NumGeometries(linestring) IS NULL OR ST_NumGeometries(linestring) = 1) AND
+  ST_NumPoints(linestring) > 3 AND
+  ST_IsClosed(linestring) AND
+  NOT ST_IsValid(ST_MakePolygon(linestring))
         ) AS p
 ) AS tmp
 WHERE
@@ -65,10 +126,20 @@ class Analyser_Osmosis_Polygon(Analyser_Osmosis):
     def __init__(self, config, logger = None):
         Analyser_Osmosis.__init__(self, config, logger)
         self.classs_change[1] = {"item":"1040", "level": 1, "tag": ["geom", "fix:chair"], "desc": T_(u"Invalid polygon") }
+        self.classs_change[2] = {"item":"1040", "level": 1, "tag": ["geom", "fix:chair"], "desc": T_(u"Invalid multipolygon") }
         self.callback10 = lambda res: {"class":1, "data":[self.way_full, self.positionAsText], "text": {"en": res[2]}}
+        self.callback20 = lambda res: {"class":2, "data":[self.relation, self.positionAsText], "text": {"en": res[2]}}
 
     def analyser_osmosis_all(self):
         self.run(sql10.format(""), self.callback10)
+        self.run(sql20.format("", ""))
+        self.run(sql21, self.callback20)
 
     def analyser_osmosis_touched(self):
         self.run(sql10.format("touched_"), self.callback10)
+        self.run(sql20.format("touched_", ""))
+        self.run(sql21, self.callback20)
+        self.run(sql20.format("", "touched_"))
+        self.run(sql21, self.callback20)
+        self.run(sql20.format("touched_", "touched_"))
+        self.run(sql21, self.callback20)
