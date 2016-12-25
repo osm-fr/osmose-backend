e0536f422c1918aa3abdf38c95f87525b370d9db
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2016-12-25 11:34:50 +0100

    Use index on tags on N/W/R

--- osmosis/CreateTagsIndex.sql
+++ osmosis/CreateTagsIndex.sql
@@ -3,6 +3,6 @@
 -- Add key indexes on tags tables - full content index
 ------------------------------------------------------------------------------
 
-CREATE INDEX idx_node_tags_k ON node_tags (k);
-CREATE INDEX idx_way_tags_k ON way_tags (k);
-CREATE INDEX idx_relation_tags_k ON relation_tags (k);
+CREATE INDEX idx_nodes_tags ON nodes USING gist(tags);
+CREATE INDEX idx_ways_tags ON ways USING gist(tags);
+CREATE INDEX idx_relations_tags ON relations USING gist(tags);
