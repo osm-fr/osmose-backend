1a62c1854f348d0e4a18d0d335267e198049a34b
Noémie Lehuby <noemie.lehuby@openmailbox.org>
2017-06-13 10:07:53 +0200

    Add sax test on public_transport tag

---
+++ plugins/Public_Transport_Stop.py
@@ -0,0 +1,65 @@
+#-*- coding: utf-8 -*-
+
+###########################################################################
+##                                                                       ##
+## Copyrights Noémie Lehuby 2017                                         ##
+##                                                                       ##
+## This program is free software: you can redistribute it and/or modify  ##
+## it under the terms of the GNU General Public License as published by  ##
+## the Free Software Foundation, either version 3 of the License, or     ##
+## (at your option) any later version.                                   ##
+##                                                                       ##
+## This program is distributed in the hope that it will be useful,       ##
+## but WITHOUT ANY WARRANTY; without even the implied warranty of        ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         ##
+## GNU General Public License for more details.                          ##
+##                                                                       ##
+## You should have received a copy of the GNU General Public License     ##
+## along with this program.  If not, see <http://www.gnu.org/licenses/>. ##
+##                                                                       ##
+###########################################################################
+
+from plugins.Plugin import Plugin
+
+
+class Public_Transport_Stop(Plugin):
+
+    def init(self, logger):
+        Plugin.init(self, logger)
+        self.errors[21411] = {"item": 2141, "level": 3, "tag": ["tag", "public_transport", "fix:chair"], "desc": T_(
+            u"Missing public_transport tag on a public tranport stop")}
+
+    def node(self, data, tags):
+        err = []
+        if u"public_transport" not in tags:
+            if "highway" in tags and tags["highway"] == "bus_stop":
+                err.append({"class": 21411, "subclass": 0,
+                            "text": T_("Bus stop without public_transport tag"),
+                            "fix": {"public_transport": "platform"}
+                            })
+            if "railway" in tags and tags["railway"] == "tram_stop":
+                err.append({"class": 21411, "subclass": 0,
+                            "text": T_("Tram stop without public_transport tag"),
+                            "fix": {"public_transport": "stop_position"}
+                            })
+        return err
+
+    def way(self, data, tags, nds):
+        return self.node(data, tags)
+
+
+###########################################################################
+from plugins.Plugin import TestPluginCommon
+
+
+class Test(TestPluginCommon):
+    def test(self):
+        a = Public_Transport_Stop(None)
+        a.init(None)
+
+        assert a.node(None, {"highway":"bus_stop"})
+        assert a.way(None, {"highway":"bus_stop"}, None)
+        assert not a.node(None, {"highway":"bus_stop", "public_transport":"platform"})
+        assert not a.node(None, {"highway":"bus_stop", "public_transport":"stop_position"})
+        assert a.node(None, {"railway":"tram_stop"})
+        assert not a.node(None, {"railway":"tram_stop", "public_transport":"stop_position"})
