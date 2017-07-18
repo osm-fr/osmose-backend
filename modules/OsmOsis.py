4d97949c11bb3fc56d3e44f3ecb9301110be4049
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2017-07-18 21:49:45 +0200

    Clean source into OsmOsis.py

--- modules/OsmOsis.py
+++ modules/OsmOsis.py
@@ -26,7 +26,7 @@ import psycopg2.extensions
 ## Reader / Writer
 
 class OsmOsis:
-    
+
     def __init__(self, dbstring, schema, dump_sub_elements=True):
         psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
         psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
@@ -34,7 +34,7 @@ class OsmOsis:
         self._PgCurs = self._PgConn.cursor()
         self._PgCurs.execute("SET search_path TO %s,public;" % schema)
         self.dump_sub_elements = dump_sub_elements
-        
+
     def __del__(self):
         try:
             self.close()
@@ -42,17 +42,19 @@ class OsmOsis:
             # psycopg2.InterfaceError can happen if connection was already closed
             pass
 
+
     def close(self):
         self._PgCurs.close()
         self._PgConn.close()
+
 
     def timestamp(self):
         self._PgCurs.execute('SELECT GREATEST((SELECT MAX(tstamp) FROM nodes), (SELECT MAX(tstamp) FROM ways), (SELECT MAX(tstamp) FROM relations))')
         (timestamp,) = self._PgCurs.fetchone()
         return timestamp
 
+
     def NodeGet(self, NodeId):
-        
         self._PgCurs.execute("SELECT nodes.id, st_y(nodes.geom), st_x(nodes.geom), nodes.version, users.name FROM nodes LEFT JOIN users ON nodes.user_id = users.id WHERE nodes.id = %d;" % NodeId)
         r1 = self._PgCurs.fetchone()
         if not r1: return None
@@ -62,16 +64,16 @@ class OsmOsis:
         data[u"lon"]     = float(r1[2])
         data[u"version"] = r1[3]
         data[u"user"]    = r1[4] or ""
-        
+
         data[u"tag"] = {}
         self._PgCurs.execute("SELECT (each(tags)).key, (each(tags)).value FROM nodes WHERE id = %d;" % NodeId)
         for r1 in self._PgCurs.fetchall():
             data[u"tag"][r1[0]] = r1[1]
-            
+
         return data
-    
+
+
     def WayGet(self, WayId):
-        
         self._PgCurs.execute("SELECT ways.id, ways.version, users.name FROM ways LEFT JOIN users ON ways.user_id = users.id WHERE ways.id = %d;" % WayId)
         r1 = self._PgCurs.fetchone()
         if not r1: return None
@@ -79,22 +81,22 @@ class OsmOsis:
         data[u"id"]      = r1[0]
         data[u"version"] = r1[1]
         data[u"user"]    = r1[2] or ""
-        
+
         data[u"tag"] = {}
         self._PgCurs.execute("SELECT (each(tags)).key, (each(tags)).value FROM ways WHERE id = %d;" % WayId)
         for r1 in self._PgCurs.fetchall():
             data[u"tag"][r1[0]] = r1[1]
-        
+
         data[u"nd"] = []
         if self.dump_sub_elements:
             self._PgCurs.execute("SELECT node_id FROM way_nodes WHERE way_id = %d ORDER BY sequence_id;" % WayId)
             for r1 in self._PgCurs.fetchall():
                 data[u"nd"].append(r1[0])
-            
+
         return data
 
+
     def RelationGet(self, RelationId):
-        
         self._PgCurs.execute("SELECT relations.id, relations.version, users.name FROM relations LEFT JOIN users ON relations.user_id = users.id WHERE relations.id = %d;" % RelationId)
         r1 = self._PgCurs.fetchone()
         if not r1: return None
@@ -102,22 +104,22 @@ class OsmOsis:
         data[u"id"]      = r1[0]
         data[u"version"] = r1[1]
         data[u"user"]    = r1[2] or ""
-        
+
         data[u"tag"] = {}
         self._PgCurs.execute("SELECT (each(tags)).key, (each(tags)).value FROM relations WHERE id = %d;" % RelationId)
         for r1 in self._PgCurs.fetchall():
             data[u"tag"][r1[0]] = r1[1]
-        
+
         data[u"member"] = []
         if self.dump_sub_elements:
             self._PgCurs.execute("SELECT member_id, member_type, member_role FROM relation_members WHERE relation_id = %d ORDER BY sequence_id;" % RelationId)
             for r1 in self._PgCurs.fetchall():
                 data[u"member"].append({u"ref":r1[0], u"type":{"N":"node","W":"way","R":"relation"}[r1[1]], u"role":r1[2]})
-            
+
         return data
 
-    def UserGet(self, UserId):
 
+    def UserGet(self, UserId):
         self._PgCurs.execute("SELECT name FROM users WHERE id = %d;" % UserId)
         r1 = self._PgCurs.fetchone()
         if not r1: return None
