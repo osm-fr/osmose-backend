6c248d8ccf3c27f653faa2c6b0ea73b41af6ea86
Francois Gouget <fgouget@free.fr>
2017-08-31 17:08:38 +0200

    Phone_fr: Some cleanups and new tests.
    Signed-off-by: Francois Gouget <fgouget@free.fr>

--- plugins/Phone_fr.py
+++ plugins/Phone_fr.py
@@ -26,48 +26,60 @@ class Phone_fr(Plugin):
 
     only_for = ["FR"]
 
+    PHONE_TAGS = set((u"contact:fax", u"contact:phone", u"fax", u"phone"))
+
     def init(self, logger):
         Plugin.init(self, logger)
 
         import re
         # Short numbers cannot be internationalized
-        self.BadShort = re.compile(r"^(\+33 *)([0-9]{4,6})$")
+        self.BadShort = re.compile(r"^([+]33 *)([0-9]{4,6})$")
 
         # Regular numbers must not have a 0 after +33
-        self.BadInter = re.compile(r"^(\+33 *0)([0-9 ]{8,})$")
+        self.BadInter = re.compile(r"^([+]33 *0)([0-9 ]{8,})$")
 
         # National numbers to internationalize. Note that in addition to
         # short numbers this also skips special numbers starting with 08
         # or 09 since these may or may not be callable from abroad.
         self.National = re.compile(r"^(0 *)([1-7][0-9 ]{8,})$")
 
-        self.errors[30921] = { "item": 3092, "level": 2, "tag": ["value", "fix:chair"], "desc": T_(u"Extra \"0\" after international code") }
-        self.errors[30922] = { "item": 3092, "level": 2, "tag": ["value", "fix:chair"], "desc": T_(u"National short code can't be internationalized") }
-        self.errors[30923] = { "item": 3092, "level": 3, "tag": ["value", "fix:chair"], "desc": T_(u"Missing international prefix") }
+        self.errors[30921] = {"item": 3092, "level": 2,
+                              "tag": ["value", "fix:chair"],
+                              "desc": T_(u"Extra \"0\" after international code")}
+        self.errors[30922] = {"item": 3092, "level": 2,
+                              "tag": ["value", "fix:chair"],
+                              "desc": T_(u"National short code can't be internationalized")}
+        self.errors[30923] = {"item": 3092, "level": 3,
+                              "tag": ["value", "fix:chair"],
+                              "desc": T_(u"Missing international prefix")}
 
-    def node(self, data, tags):
-        for tag in (u"contact:fax", u"contact:phone", u"fax", u"phone"):
-            if tag not in tags:
+    def check(self, tags):
+        for tag in tags:
+            if tag not in self.PHONE_TAGS:
                 continue
             phone = tags[tag]
 
             r = self.BadInter.match(phone)
             if r:
-                return [{"class":30921, "fix": {tag: "+33 " + r.group(2)}}]
+                return {"class": 30921, "fix": {tag: "+33 " + r.group(2)}}
 
             r = self.BadShort.match(phone)
             if r:
-                 return [{"class":30922, "fix": {tag: r.group(2)}}]
+                return {"class": 30922, "fix": {tag: r.group(2)}}
 
             r = self.National.match(phone)
             if r:
-                return [{"class":30923, "fix": {tag: "+33 " + r.group(2)}}]
+                return {"class": 30923, "fix": {tag: "+33 " + r.group(2)}}
+        return None
 
-    def way(self, data, tags, nds):
-        return self.node(data, tags)
+    def node(self, _data, tags):
+        return self.check(tags)
 
-    def relation(self, data, tags, members):
-        return self.node(data, tags)
+    def way(self, _data, tags, _nds):
+        return self.check(tags)
+
+    def relation(self, _data, tags, _members):
+        return self.check(tags)
 
 
 ###########################################################################
@@ -75,18 +87,25 @@ from plugins.Plugin import TestPluginCommon
 
 class Test(TestPluginCommon):
     def test(self):
-        a = Phone_fr(None)
-        a.init(None)
-        for (d, f) in [(u"+33 0102030405", u"+33 102030405"),
-                       (u"+330102030405", u"+33 102030405"),
-                       (u"+33 01 02 03 04 05", u"+33 1 02 03 04 05"),
-                       (u"+33 3631", u"3631"),
-                       (u"0102030405", u"+33 102030405"),
-                       (u"01 02 03 04 05", u"+33 1 02 03 04 05"),
-                      ]:
-            self.check_err(a.node(None, {"phone": d}), ("phone='%s'" % d))
-            self.assertEquals(a.node(None, {"phone": d})[0]["fix"]["phone"], f)
+        p = Phone_fr(None)
+        p.init(None)
 
-        for d in [u"118987",
-                 ]:
-            assert not a.node(None, {"phone": f}), ("phone='%s'" % f)
+        for (bad, good) in ((u"+330102030405", u"+33 102030405"),
+                            # Preserve formatting
+                            (u"+33 0102030405", u"+33 102030405"),
+                            (u"+33  01 02 03 04 05", u"+33 1 02 03 04 05"),
+                            (u"+33  3631", u"3631"),
+                            (u"0102030405", u"+33 102030405"),
+                            (u"01 02 03 04 05", u"+33 1 02 03 04 05"),
+                            (u"01 02 03 04 05 06", u"+33 1 02 03 04 05 06")):
+            # Check the bad number's error and fix
+            err = p.node(None, {"phone": bad})
+            self.check_err(err, ("phone='%s'" % bad))
+            self.assertEquals(err["fix"]["phone"], good)
+
+            # The correct number does not need fixing
+            assert not p.node(None, {"phone": good}), ("phone='%s'" % good)
+
+        # Verify we get no error for other correct numbers
+        for good in (u"3631", u"118987"):
+            assert not p.node(None, {"phone": good}), ("phone='%s'" % good)
