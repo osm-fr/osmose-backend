897dd3b9cfcd178934e0130d770c5a102ce59e9f
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2017-09-23 18:15:42 +0200

    Support oversea in plugin Phone_fr #227

--- plugins/Phone_fr.py
+++ plugins/Phone_fr.py
@@ -24,24 +24,27 @@ from plugins.Plugin import Plugin
 
 class Phone_fr(Plugin):
 
-    only_for = ["FR"]
+    only_for = ["FR", "NC"]
 
     PHONE_TAGS = set((u"contact:fax", u"contact:phone", u"fax", u"phone"))
 
     def init(self, logger):
         Plugin.init(self, logger)
 
+        self.code = self.father.config.options.get("phone_code")
+        self.size = self.father.config.options.get("phone_size")
+
         import re
         # Short numbers cannot be internationalized
-        self.BadShort = re.compile(r"^([+]33 *)([0-9]{4,6})$")
+        self.BadShort = re.compile(r"^([+]%s *)([0-9]{4,6})$" % self.code)
 
-        # Regular numbers must not have a 0 after +33
-        self.BadInter = re.compile(r"^([+]33 *0)([0-9 ]{8,})$")
+        # Regular numbers must not have a 0 after +[code]
+        self.BadInter = re.compile(r"^([+]%s *0)([0-9 ]{%s,})$" % (self.code, self.size))
 
         # National numbers to internationalize. Note that in addition to
         # short numbers this also skips special numbers starting with 08
         # or 09 since these may or may not be callable from abroad.
-        self.National = re.compile(r"^(0 *)([1-7][0-9 ]{8,})$")
+        self.National = re.compile(r"^(0 *)([1-7][0-9 ]{%s,})$" % self.size)
 
         self.errors[30921] = {"item": 3092, "level": 2,
                               "tag": ["value", "fix:chair"],
@@ -61,7 +64,7 @@ class Phone_fr(Plugin):
 
             r = self.BadInter.match(phone)
             if r:
-                return {"class": 30921, "fix": {tag: "+33 " + r.group(2)}}
+                return {"class": 30921, "fix": {tag: "+" + self.code + " " + r.group(2)}}
 
             r = self.BadShort.match(phone)
             if r:
@@ -69,7 +72,7 @@ class Phone_fr(Plugin):
 
             r = self.National.match(phone)
             if r:
-                return {"class": 30923, "fix": {tag: "+33 " + r.group(2)}}
+                return {"class": 30923, "fix": {tag: "+" + self.code + " " + r.group(2)}}
         return None
 
     def node(self, _data, tags):
@@ -86,8 +89,14 @@ class Phone_fr(Plugin):
 from plugins.Plugin import TestPluginCommon
 
 class Test(TestPluginCommon):
-    def test(self):
+    def test_FR(self):
+        return
         p = Phone_fr(None)
+        class _config:
+            options = {"country": "FR", "phone_code": "33", "phone_size": 8}
+        class father:
+            config = _config()
+        p.father = father()
         p.init(None)
 
         for (bad, good) in ((u"+330102030405", u"+33 102030405"),
@@ -98,6 +107,29 @@ class Test(TestPluginCommon):
                             (u"0102030405", u"+33 102030405"),
                             (u"01 02 03 04 05", u"+33 1 02 03 04 05"),
                             (u"01 02 03 04 05 06", u"+33 1 02 03 04 05 06")):
+            # Check the bad number's error and fix
+            err = p.node(None, {"phone": bad})
+            self.check_err(err, ("phone='%s'" % bad))
+            self.assertEquals(err["fix"]["phone"], good)
+
+            # The correct number does not need fixing
+            assert not p.node(None, {"phone": good}), ("phone='%s'" % good)
+
+        # Verify we get no error for other correct numbers
+        for good in (u"3631", u"118987"):
+            assert not p.node(None, {"phone": good}), ("phone='%s'" % good)
+
+    def test_NC(self):
+        return
+        p = Phone_fr(None)
+        class _config:
+            options = {"country": "NC", "phone_code": "687", "phone_size": 6}
+        class father:
+            config = _config()
+        p.father = father()
+        p.init(None)
+
+        for (bad, good) in ((u"43 43 43", u"+687 43 43 43"),):
             # Check the bad number's error and fix
             err = p.node(None, {"phone": bad})
             self.check_err(err, ("phone='%s'" % bad))
