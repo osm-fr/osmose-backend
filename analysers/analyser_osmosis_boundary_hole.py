80008b6951ff646f9ffb12c203063b4df91b9640
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2017-12-09 18:05:23 +0100

    Fix str formatting in analyser_osmosis_boundary_hole.py

--- analysers/analyser_osmosis_boundary_hole.py
+++ analysers/analyser_osmosis_boundary_hole.py
@@ -58,7 +58,7 @@ class Analyser_Osmosis_Boundary_Hole(Analyser_Osmosis):
     def __init__(self, config, logger = None):
         Analyser_Osmosis.__init__(self, config, logger)
         self.admin_level = self.config.options and self.config.options.get("boundary_detail_level", 8) or 8
-        self.classs[1] = {"item":"6060", "level": 2, "tag": ["boundary", "geom", "fix:chair"], "desc": T_(u"Hole between administrative boundaries of admin_level {0}", self.admin_level) }
+        self.classs[1] = {"item":"6060", "level": 2, "tag": ["boundary", "geom", "fix:chair"], "desc": T_(u"Hole between administrative boundaries of admin_level %s", self.admin_level) }
 
     def analyser_osmosis_common(self):
         self.run(sql10.format(self.admin_level), lambda res: {"class":1, "subclass":self.stablehash(res[0]), "data":[self.positionAsText]} )
