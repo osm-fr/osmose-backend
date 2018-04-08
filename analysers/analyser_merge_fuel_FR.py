150bbb9e0b7f9d8af931dceda1ec178d241410d6
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2018-04-08 15:28:29 +0200

    Update fuel_FR.csv.bz2

--- analysers/analyser_merge_fuel_FR.py
+++ analysers/analyser_merge_fuel_FR.py
@@ -31,7 +31,7 @@ class Analyser_Merge_Fuel_FR(Analyser_Merge):
         Analyser_Merge.__init__(self, config, logger,
             "http://www.prix-carburants.economie.gouv.fr/rubrique/opendata/",
             u"Prix des carburants en France",
-            CSV(Source(attribution = u"Ministère de l'Economie, de l'Industrie et du Numérique", millesime = "30/08/2017",
+            CSV(Source(attribution = u"Ministère de l'Economie, de l'Industrie et du Numérique", millesime = "08/04/2018",
                     file = "fuel_FR.csv.bz2")),
             Load("lon", "lat",
                 where = lambda row: row["lat"] != "48.858858899999994" and row["lon"] != "2.3470599"),
