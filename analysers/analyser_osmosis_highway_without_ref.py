da193ad9d5fed8752baa174e9fb9ab0360f4937c
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2018-06-25 22:31:26 +0200

    Fix analyser_osmosis_highway_without_ref and does not support diff mode

--- analysers/analyser_osmosis_highway_without_ref.py
+++ analysers/analyser_osmosis_highway_without_ref.py
@@ -27,13 +27,13 @@ SELECT DISTINCT
   ways.id,
   ST_AsText(way_locate(linestring))
 FROM
-  {0}highways AS ways
+  highways AS ways
   LEFT JOIN relation_members ON
     relation_members.member_type = 'W' AND
     relation_members.member_id = ways.id
-  LEFT JOIN {1}relations AS relations ON
+  LEFT JOIN relations ON
     relations.id = relation_members.relation_id AND
-    ways.tags ?| ARRAY['noref', 'ref', 'nat_ref', 'int_ref']
+    relations.tags ?| ARRAY['noref', 'ref', 'nat_ref', 'int_ref']
 WHERE
   ways.highway = 'motorway' AND
   NOT ways.tags ?| ARRAY['noref', 'ref', 'nat_ref', 'int_ref'] AND
@@ -43,17 +43,12 @@ WHERE
 
 class Analyser_Osmosis_Highway_Without_Ref(Analyser_Osmosis):
 
-    requires_tables_full = ['highways']
-    requires_tables_diff = ['highways', 'touched_highways']
+    requires_tables_common = ['highways']
 
     def __init__(self, config, logger = None):
         Analyser_Osmosis.__init__(self, config, logger)
         self.classs[20804] = {"item":"2080", "level": 2, "tag": ["highway", "ref", "fix:chair"], "desc": T_(u"Motorway without ref, nat_ref, int_ref or noref tag") }
         self.callback10 = lambda res: {"class":20804, "data":[self.way_full, self.positionAsText]}
 
-    def analyser_osmosis_full(self):
-        self.run(sql10.format("", ""), self.callback10)
-
-    def analyser_osmosis_diff(self):
-        self.run(sql10.format("touched_", "not_touched_"), self.callback10)
-        self.run(sql10.format("", "touched_"), self.callback10)
+    def analyser_osmosis_common(self):
+        self.run(sql10, self.callback10)
