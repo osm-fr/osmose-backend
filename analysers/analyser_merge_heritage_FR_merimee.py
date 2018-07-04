da2bae0c99d5e9275c6f7b90d8db766e2cde6f14
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2018-07-04 21:44:52 +0200

    Udate of analyser_merge_heritage_FR_merimee and backlist common names #257

--- analysers/analyser_merge_heritage_FR_merimee.py
+++ analysers/analyser_merge_heritage_FR_merimee.py
@@ -39,16 +39,32 @@ class Analyser_Merge_Heritage_FR_Merimee(Analyser_Merge):
                 ret = match.group(1).replace("/", "-");
             return ret;
 
+        BLACK_WORDS = [
+            u"Eglise protestante", u"Ossuaire (ancien)", u"Polissoir", u"Citadelle",
+            u"Sol de maison à maison", u"Boulangerie", u"Domaine du château",
+            u"Monument aux morts", u"Château (ruines)", u"Croix du 16e siècle", u"Hôpital",
+            u"Tour de l'Horloge", u"Chapelle du cimetière", u"Tumulus", u"Maison Renaissance",
+            u"Abbaye (ancienne)", u"Moulin à vent", u"Théâtre municipal",
+            u"Croix de carrefour", u"Tour", u"Mairie", u"Prieuré (ancien)", u"Eglise (ancienne)",
+            u"Eglise de l'Assomption", u"Hôtel particulier", u"Beffroi", u"Ancienne église",
+            u"Restes du château", u"Palais de Justice", u"Remparts", u"Halles",
+            u"Croix en pierre", u"Croix du cimetière", u"Ferme", u"Calvaire", u"Motte féodale",
+            u"Temple protestant", u"Synagogue", u"Ancien prieuré", u"Eglise paroissiale",
+            u"Presbytère", u"Ruines du château", u"Maison du 15e siècle",
+            u"Manoir", u"Maison du 16e siècle", u"Halle", u"Château (ancien)", u"Maisons",
+            u"Menhir", u"Ancienne abbaye", u"Croix de chemin", u"Maison à pans de bois",
+            u"Dolmen", u"Hôtel", u"Ancien château", u"Immeuble", u"Eglise", u"Maison"
+        ]
+
         Analyser_Merge.__init__(self, config, logger,
-            "https://www.data.gouv.fr/fr/datasets/monuments-historiques-liste-des-immeubles-proteges-au-titre-des-monuments-historiques/",
-            u"Monuments Historiques : liste des Immeubles protégés au titre des Monuments Historiques",
-#            CSV(Source(fileUrl = "http://data.culture.fr/entrepot/MERIMEE/merimee-MH.csv.zip", zip = "merimee-MH-valid.csv.utf"),
-#            Original without location, geocoded with http://adresse.data.gouv.fr/csv/
-            CSV(Source(attribution = u"data.gouv.fr:Ministère de la Culture", millesime = "04/2015",
+            "https://data.culturecommunication.gouv.fr/explore/dataset/liste-des-immeubles-proteges-au-titre-des-monuments-historiques/",
+            u"Immeubles protégés au titre des Monuments Historiques",
+            # Original without accurate location, geocoded with https://adresse.data.gouv.fr/csv
+            CSV(Source(attribution = u"Ministère de la Culture", millesime = "07/2018",
                     file = "heritage_FR_merimee.csv.bz2"),
-                separator = '|'),
+                separator = ';'),
             Load("longitude", "latitude",
-                select = {"DPRO": True}),
+                select = {u"Date de Protection": True}),
             Mapping(
                 select = Select(
                     types = ["nodes", "ways", "relations"],
@@ -61,9 +77,9 @@ class Analyser_Merge_Heritage_FR_Merimee(Analyser_Merge):
                     static1 = {"heritage:operator": "mhs"},
                     static2 = {"source:heritage": self.source},
                     mapping1 = {
-                        "ref:mhs": "REF",
-                        "mhs:inscription_date": lambda res: parseDPRO(res["DPRO"]),
-                        "heritage": lambda res: 2 if u"classement par arrêté" in res["PPRO"] else 3 if u"inscription par arrêté" in res["PPRO"] else None},
-                    mapping2 = {"name": "TICO"},
+                        "ref:mhs": u"Référence",
+                        "mhs:inscription_date": lambda res: parseDPRO(res[u"Date de Protection"]),
+                        "heritage": lambda res: 2 if res[u"Précision sur la Protection"] and u"classement par arrêté" in res[u"Précision sur la Protection"] else 3 if res[u"Précision sur la Protection"] and u"inscription par arrêté" in res[u"Précision sur la Protection"] else None},
+                    mapping2 = {"name": lambda res: res[u"Appellation courante"] if res[u"Appellation courante"] not in BLACK_WORDS else None},
                     tag_keep_multiple_values = ["heritage:operator"],
-                    text = lambda tags, fields: {"en": u"Historical monument: %s (positioned at %s with confidence %s)" % (", ".join(filter(lambda x: x!= None and x != "", [fields["DPRO"], fields["ADRS"], fields["COM"]])), fields["result_type"], fields["result_score"])} )))
+                    text = lambda tags, fields: {"en": u"Historical monument: %s (positioned at %s with confidence %s)" % (", ".join(filter(lambda x: x!= None and x != "", [fields[u"Date de Protection"], fields[u"Adresse"], fields[u"Commune"]])), fields[u"result_type"], fields[u"result_score"])} )))
