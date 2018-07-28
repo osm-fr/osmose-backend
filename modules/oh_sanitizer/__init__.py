4cece1f3bf6fd1c885a9c9bfc2b5caf9f06982e4
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2018-07-28 14:44:11 +0200

    Add plugin for opening_hours

---
+++ modules/oh_sanitizer/__init__.py
@@ -0,0 +1,26 @@
+"""A corrector for the 'opening_hours' fields from OpenStreetMap.
+
+Provides a 'sanitize_field()' function, which tries to fix the most
+current errors in the given field (takes and returns a string).
+
+It can raise the following exceptions:
+
+- TypeError : If the given field is not a string.
+- SanitizeError : The generic exception of 'oh_sanitizer',
+    raised when the field can't be parsed (if it is too complex, or invalid).
+- InconsistentField : Inheriting from SanitizeError,
+    raised when the field contains an invalid pattern which can't
+    be corrected automatically.
+
+Example:
+>>> import oh_sanitizer
+>>> print(oh_sanitizer.sanitize_field("mo-fr 10h - 7:00 pm"))
+"Mo-Fr 10:00-19:00"
+"""
+
+__version__ = "0.1.7"
+__appname__ = "oh_sanitizer"
+__author__ = "rezemika <reze.mika@gmail.com>"
+__licence__ = "AGPLv3"
+
+from main import sanitize_field, SanitizeError, InconsistentField
---
+++ plugins/TagFix_Opening_Hours.py
@@ -0,0 +1,70 @@
+#-*- coding: utf-8 -*-
+
+###########################################################################
+##                                                                       ##
+## Copyrights rezemika 2018                                              ##
+##                                                                       ##
+## This program is free software: you can redistribute it and/or modify  ##
+## it under the terms of the GNU General Public License as published by  ##
+## the Free Software Foundation, either version 3 of the License, or     ##
+## (at your option) any later version.                                   ##
+##                                                                       ##
+## This program is distributed in the hope that it will be useful,       ##
+## but WITHOUT ANY WARRANTY; without even the implied warranty of        ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         ##
+## GNU General Public License for more details.                          ##
+##                                                                       ##
+## You should have received a copy of the GNU General Public License     ##
+## along with this program.  If not, see <http://www.gnu.org/licenses/>. ##
+##                                                                       ##
+###########################################################################
+from plugins.Plugin import Plugin
+
+try:
+    from modules.oh_sanitizer import sanitize_field, SanitizeError, InconsistentField
+except ImportError, e:
+    print(e)
+
+class TagFix_Opening_Hours(Plugin):
+
+    def init(self, logger):
+        Plugin.init(self, logger)
+        self.errors[32501] = {"item": 3250, "level": 3, "tag": ["value", "fix:chair"], "desc": T_(u"Invalid Opening Hours")}
+
+    def sanitize_tags(self, tags):
+        if 'opening_hours' not in tags:
+            return
+
+        try:
+            sanitized_field = sanitize_field(tags['opening_hours'])
+            if sanitized_field.replace(' ', '').replace('0', '').lower() != tags['opening_hours'].replace(' ', '').replace('0', '').lower(): # Ignore sapce and 0 changes
+                return {"class": 32501, "subclass": 0, 'fix': {'opening_hours': sanitized_field}}
+        except InconsistentField as e:
+            return {"class": 32501, "subclass": 1, 'text': str(e)}
+        except SanitizeError as e:
+            return {"class": 32501, "subclass": 2}
+
+    def node(self, data, tags):
+        return self.sanitize_tags(tags)
+
+    def way(self, data, tags, nds):
+        return self.sanitize_tags(tags)
+
+    def relation(self, data, tags, members):
+        return self.sanitize_tags(tags)
+
+
+###########################################################################
+from plugins.Plugin import TestPluginCommon
+
+class Test(TestPluginCommon):
+    def test(self):
+        a = TagFix_Opening_Hours(None)
+        a.init(None)
+
+        print(a.node(None, {'opening_hours': 'mo-fr 10h - 19h00'}))
+        self.check_err(a.node(None, {'opening_hours': 'mo-fr 10h - 19h00'}))
+        self.check_err(a.node(None, {'opening_hours': '2010 - 2020/2 dec-feb 10:00 am - 12:00 am/1:00 pm-7:00pm'}))
+
+        assert not a.node(None, {'opening_hours': 'Mo-Fr 10:00-19:00'})
+        assert not a.node(None, {'opening_hours': 'Mo-Fr   10:00 -19:00'})
