eb3337039bd75c152444182d787c4d350d8506ff
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2018-08-07 21:54:36 +0200

    Check for intersection with power towers and poles in analyser_osmosis_highway_vs_building #153

--- analysers/analyser_osmosis_highway_vs_building.py
+++ analysers/analyser_osmosis_highway_vs_building.py
@@ -54,7 +54,7 @@ sql01 = """
 CREATE INDEX idx_highway_linestring ON highway USING gist(linestring)
 """
 
-sql04 = """
+sql02 = """
 CREATE TEMP TABLE tree AS
 SELECT
     id,
@@ -67,8 +67,25 @@ WHERE
     tags->'natural' = 'tree'
 """
 
+sql03 = """
+CREATE INDEX idx_tree_geom ON tree USING gist(geom)
+"""
+
+sql04 = """
+CREATE TEMP TABLE power AS
+SELECT
+    id,
+    geom
+FROM
+    nodes
+WHERE
+    tags != ''::hstore AND
+    tags?'power' AND
+    tags->'power' IN ('tower', 'pole', 'portal')
+"""
+
 sql05 = """
-CREATE INDEX idx_tree_linestring ON tree USING gist(geom)
+CREATE INDEX idx_power_geom ON power USING gist(geom)
 """
 
 sql10 = """
@@ -100,6 +117,21 @@ FROM
         NOT building.layer
 """
 
+sql21 = """
+SELECT
+    power.id,
+    building.id,
+    ST_AsText(power.geom)
+FROM
+    {0}power AS power
+    JOIN {1}buildings AS building ON
+        power.geom && building.linestring AND
+        ST_Intersects(power.geom, ST_MakePolygon(building.linestring)) AND
+        NOT building.relation AND
+        building.wall AND
+        NOT building.layer
+"""
+
 sql30 = """
 SELECT
     tree.id,
@@ -110,6 +142,18 @@ FROM
     JOIN {1}highway AS highway ON
         tree.geom && highway.linestring AND
         ST_Intersects(ST_Buffer(tree.geom::geography, 0.25)::geometry, highway.linestring)
+"""
+
+sql31 = """
+SELECT
+    power.id,
+    highway.id,
+    ST_AsText(power.geom)
+FROM
+    {0}power AS power
+    JOIN {1}highway AS highway ON
+        power.geom && highway.linestring AND
+        ST_Intersects(ST_Buffer(power.geom::geography, 0.25)::geometry, highway.linestring)
 """
 
 sql40 = """
@@ -156,35 +200,53 @@ class Analyser_Osmosis_Highway_VS_Building(Analyser_Osmosis):
         self.classs_change[3] = {"item":"1070", "level": 2, "tag": ["highway", "tree", "geom", "fix:imagery"], "desc": T_(u"Tree and highway too close") }
         self.classs_change[4] = {"item":"1070", "level": 3, "tag": ["highway", "waterway", "geom", "fix:imagery"], "desc": T_(u"Highway intersecting small water piece") }
         self.classs_change[5] = {"item":"1070", "level": 2, "tag": ["highway", "waterway", "geom", "fix:imagery"], "desc": T_(u"Highway intersecting large water piece") }
+        self.classs_change[6] = {"item":"1070", "level": 2, "tag": ["power", "building", "geom", "fix:imagery"], "desc": T_(u"Power object intersecting building") }
+        self.classs_change[7] = {"item":"1070", "level": 2, "tag": ["highway", "power", "geom", "fix:imagery"], "desc": T_(u"Power object and highway too close") }
         self.callback10 = lambda res: {"class":1, "data":[self.way_full, self.way_full, self.positionAsText]}
         self.callback20 = lambda res: {"class":2, "data":[self.node_full, self.way_full, self.positionAsText]}
+        self.callback21 = lambda res: {"class":6, "data":[self.node_full, self.way_full, self.positionAsText]}
         self.callback30 = lambda res: {"class":3, "data":[self.node_full, self.way_full, self.positionAsText]}
+        self.callback31 = lambda res: {"class":7, "data":[self.node_full, self.way_full, self.positionAsText]}
         self.callback40 = lambda res: {"class":res[3], "data":[self.way_full, self.way_full, self.positionAsText]}
 
     def analyser_osmosis_full(self):
         self.run(sql00)
+        self.run(sql01)
+        self.run(sql02)
+        self.run(sql03)
         self.run(sql04)
         self.run(sql05)
 
         self.run(sql10.format("", ""), self.callback10)
         self.run(sql20.format("", ""), self.callback20)
+        self.run(sql21.format("", ""), self.callback21)
         self.run(sql30.format("", ""), self.callback30)
+        self.run(sql31.format("", ""), self.callback31)
         self.run(sql40.format("", ""), self.callback40)
 
     def analyser_osmosis_diff(self):
         self.run(sql00)
+        self.run(sql01)
+        self.run(sql02)
+        self.run(sql03)
         self.run(sql04)
         self.run(sql05)
         self.create_view_touched("highway", "W")
         self.create_view_touched("tree", "N")
+        self.create_view_touched("power", "N")
         self.create_view_not_touched("highway", "W")
         self.create_view_not_touched("tree", "N")
+        self.create_view_not_touched("power", "N")
 
         self.run(sql10.format("touched_", "not_touched_"), self.callback10)
         self.run(sql10.format("", "touched_"), self.callback10)
         self.run(sql20.format("touched_", ""), self.callback20)
         self.run(sql20.format("not_touched_", "touched_"), self.callback20)
+        self.run(sql21.format("touched_", ""), self.callback21)
+        self.run(sql21.format("not_touched_", "touched_"), self.callback21)
         self.run(sql30.format("touched_", ""), self.callback30)
         self.run(sql30.format("not_touched_", "touched_"), self.callback30)
+        self.run(sql31.format("touched_", ""), self.callback31)
+        self.run(sql31.format("not_touched_", "touched_"), self.callback31)
         self.run(sql40.format("touched_", "not_touched_"), self.callback40)
         self.run(sql40.format("", "touched_"), self.callback40)
