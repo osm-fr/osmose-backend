3491240efede8a2f85e001e2dc830e7cfd39009d
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2018-11-05 21:17:34 +0100

    Allow '/' in name in Switzerland

--- plugins/Name_Multiple.py
+++ plugins/Name_Multiple.py
@@ -33,6 +33,7 @@ class Name_Multiple(Plugin):
         self.NoExtra = False
         self.HighwayOnly = False
         self.streetSubNumber = False
+        self.allowSlash = False
         if self.father.config.options.get("country"):
             self.NoExtra = any(map(lambda c: self.father.config.options.get("country").startswith(c), ['DE', 'US', 'CA']))
 
@@ -40,6 +41,8 @@ class Name_Multiple(Plugin):
 
             # In Thailand street added into existing street are named like "บ้านแพะแม่คือ ซอย 5/1"
             self.streetSubNumber = any(map(lambda c: self.father.config.options.get("country").startswith(c), ['TH', 'VN', 'MY']))
+
+            self.allowSlash = any(map(lambda c: self.father.config.options.get("country").startswith(c), ['CH']))
 
         self.streetSubNumberRe = re.compile(u".*[0-9๐๑๒๓๔๕๖๗๘๙]/[0-9๐๑๒๓๔๕๖๗๘๙].*")
 
@@ -57,56 +60,45 @@ class Name_Multiple(Plugin):
         if self.NoExtra:
             return
 
-        if '/' in tags["name"] and not (self.streetSubNumber and self.streetSubNumberRe.match(tags["name"])):
+        if not self.allowSlash and '/' in tags["name"] and not (self.streetSubNumber and self.streetSubNumberRe.match(tags["name"])):
             return {"class": 705, "subclass": 1, "text": {"en": "name=%s" % tags["name"]}}
         if '+' in tags["name"][0:-1]:
             return {"class": 705, "subclass": 2, "text": {"en": "name=%s" % tags["name"]}}
 
 ###########################################################################
-from plugins.Plugin import TestPluginCommon
+from plugins.Plugin import TestPluginCommon, with_options
 
 class Test(TestPluginCommon):
-    def test_th(self):
+    def test(self):
         TestPluginCommon.setUp(self)
-        self.p = Name_Multiple(None)
+        p = Name_Multiple(None)
         class _config:
-            options = {"country": "TH"}
+            options = {"country": None}
         class father:
             config = _config()
-        self.p.father = father()
-        self.p.init(None)
+        p.father = father()
 
-        self.check_err(self.p.way(None, {"name": "aueuie ; ueuaeuie"}, None))
-        self.check_err(self.p.way(None, {"name": "aueuie / ueuaeuie"}, None))
-        self.check_err(self.p.way(None, {"name": "aueuie + ueuaeuie"}, None))
-        assert not self.p.way(None, {"amenity": "aueuie + ueuaeuie"}, None)
-        assert not self.p.way(None, {"name": "aueuie + ueuaeuie", "aeroway": "yes"}, None)
-        assert not self.p.way(None, {"name": "Profil+"}, None)
-        assert not self.p.way(None, {"name": u"บ้านแพะแม่คือ ซอย 5/10"}, None)
-        assert not self.p.way(None, {"name": u"บ้านแพะแม่คือ ซอย 5/๓๔๕"}, None)
+        with with_options(p, {'country': 'TH'}):
+            p.init(None)
+            self.check_err(p.way(None, {"name": "aueuie ; ueuaeuie"}, None))
+            self.check_err(p.way(None, {"name": "aueuie / ueuaeuie"}, None))
+            self.check_err(p.way(None, {"name": "aueuie + ueuaeuie"}, None))
+            assert not p.way(None, {"amenity": "aueuie + ueuaeuie"}, None)
+            assert not p.way(None, {"name": "aueuie + ueuaeuie", "aeroway": "yes"}, None)
+            assert not p.way(None, {"name": "Profil+"}, None)
+            assert not p.way(None, {"name": u"บ้านแพะแม่คือ ซอย 5/10"}, None)
+            assert not p.way(None, {"name": u"บ้านแพะแม่คือ ซอย 5/๓๔๕"}, None)
 
-    def test_us(self):
-        TestPluginCommon.setUp(self)
-        self.p = Name_Multiple(None)
-        class _config:
-            options = {"country": "US-TX"}
-        class father:
-            config = _config()
-        self.p.father = father()
-        self.p.init(None)
+        with with_options(p, {'country': 'US-TX'}):
+            p.init(None)
+            assert not p.way(None, {"name": u"County Route 7/2"}, None)
+            assert not p.way(None, {"name": u"16 5/10 Road"}, None)
 
-        assert not self.p.way(None, {"name": u"County Route 7/2"}, None)
-        assert not self.p.way(None, {"name": u"16 5/10 Road"}, None)
+        with with_options(p, {'country': 'BY'}):
+            p.init(None)
+            assert not p.way(None, {"name": u"д/с №68"}, None)
+            assert p.way(None, {"name": u"д/с №68", "highway": "terciary"}, None)
 
-    def test_by(self):
-        TestPluginCommon.setUp(self)
-        self.p = Name_Multiple(None)
-        class _config:
-            options = {"country": "BY"}
-        class father:
-            config = _config()
-        self.p.father = father()
-        self.p.init(None)
-
-        assert not self.p.way(None, {"name": u"д/с №68"}, None)
-        assert self.p.way(None, {"name": u"д/с №68", "highway": "terciary"}, None)
+        with with_options(p, {'country': 'CH'}):
+            p.init(None)
+            assert not p.way(None, {"name": u"Waffenplatz-/Bederstrasse"}, None)
