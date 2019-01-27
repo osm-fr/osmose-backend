2eb1916ee39e4d470f81eab53d8fde7fc2395cf9
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2019-01-27 12:08:23 +0100

    Use custom osm pbf parser

---
+++ modules/osm_pbf_parser/osm_pbf_parser_test.py
@@ -0,0 +1,42 @@
+#-*- coding: utf-8 -*-
+
+###########################################################################
+##                                                                       ##
+## Copyrights Frédéric Rodrigo 2019                                      ##
+##                                                                       ##
+## This program is free software: you can redistribute it and/or modify  ##
+## it under the terms of the GNU General Public License as published by  ##
+## the Free Software Foundation, either version 3 of the License, or     ##
+## (at your option) any later version.                                   ##
+##                                                                       ##
+## This program is distributed in the hope that it will be useful,       ##
+## but WITHOUT ANY WARRANTY; without even the implied warranty of        ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         ##
+## GNU General Public License for more details.                          ##
+##                                                                       ##
+## You should have received a copy of the GNU General Public License     ##
+## along with this program.  If not, see <http://www.gnu.org/licenses/>. ##
+##                                                                       ##
+###########################################################################
+
+import sys
+import osm_pbf_parser
+
+class V(osm_pbf_parser.Visitor):
+  def node(self, osmid, lon, lat, tags):
+    pass
+    #print('node', osmid, tags)
+#    return "e"
+
+  def way(self, osmid, tags, refs):
+    #print('way', osmid, tags)
+    #print('way', osmid, refs)
+    return "e"
+
+  def relation(self, osmid, tags, ref):
+    #print('relation', osmid, tags, ref)
+    return "e"
+
+v = V()
+
+osm_pbf_parser.read_osm_pbf(sys.argv[1], v)
