e7fd0ff30e019d18b2a436801b2295f5b1bd09c2
Francois Gouget <fgouget@free.fr>
2019-02-09 22:52:55 +0100

    oh_sanitizer: Add support for three-letter weekday abbreviations
    These are probably coming in via copy/paste from websites.
    Add a test to make sure the three-letter abbreviations are recognized.
    
    Signed-off-by: Francois Gouget <fgouget@free.fr>

--- modules/oh_sanitizer/main.py
+++ modules/oh_sanitizer/main.py
@@ -444,6 +444,7 @@ class TestSanitize(_unittest.TestCase):
         
         # Global
         self.assertEqual(sanitize_field("2010-2020/2 WEEK 1-12/2 mo-fr 10h- 12h am, 1:00 pm - 20:00"), "2010-2020/2 week 1-12/2 Mo-Fr 10:00-12:00,13:00-20:00")
+        self.assertEqual(sanitize_field("Mon-fri 10:00-20:00"), "Mo-Fr 10:00-20:00")
         self.assertEqual(sanitize_field("2020 mo-fr 1000 - 2000 / 22:20-23:00"), "2020 Mo-Fr 10:00-20:00,22:20-23:00")
         self.assertEqual(sanitize_field("Monday-friday 10h am - 12h / 13h-20h"), "Mo-Fr 10:00-12:00,13:00-20:00")
         self.assertEqual(sanitize_field("lundi-vendredi 10h am - 12h / 13h-20h"), "Mo-Fr 10:00-12:00,13:00-20:00")
