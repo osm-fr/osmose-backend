d20fe500834971c37a135400531c622560b4b59e
HubMiner <g246020@aol.com>
2019-05-07 23:07:55 -0500

    #498: handling 12h

--- modules/oh_sanitizer/main.py
+++ modules/oh_sanitizer/main.py
@@ -290,7 +290,9 @@ class SanitizerTransformer(_lark.Transformer):
         for arg in args:
             if arg.type == 'MINUTE':
                 m = int(arg)
-            elif arg.type == 'PM':
+            elif ( (arg.type == 'AM') and (h == 12) ):
+                h = 24
+            elif ( (arg.type == 'PM') and (h < 12) ):
                 h = h + 12
         return str(h).zfill(2) + ':' + str(m).zfill(2)
 
@@ -465,6 +467,7 @@ class TestSanitize(_unittest.TestCase):
         self.assertEqual(sanitize_field("8h45 am - 11.45 a.m."), "08:45-11:45")
         self.assertEqual(sanitize_field("9h p.m. 6 - 10 pm 15"), "21:06-22:15")
         self.assertEqual(sanitize_field("9 am - 12+"), "09:00-12:00+")
+        self.assertEqual(sanitize_field("12:33 pm - 12:44 am"), "12:33-24:44")
 
         # Timespan correction
         self.assertEqual(sanitize_field("09:00-12:00/13:00-19:00"), "09:00-12:00,13:00-19:00")
@@ -477,7 +480,7 @@ class TestSanitize(_unittest.TestCase):
         self.assertEqual(sanitize_field(u"Mo–Fr 09:00–12:00"), "Mo-Fr 09:00-12:00")
 
         # Global
-        self.assertEqual(sanitize_field("2010-2020/2 WEEK 1-12/2 mo-fr 10h- 12h am, 1:00 pm - 20:00"), "2010-2020/2 week 1-12/2 Mo-Fr 10:00-12:00,13:00-20:00")
+        self.assertEqual(sanitize_field("2010-2020/2 WEEK 1-12/2 mo-fr 10h- 12h am, 1:00 pm - 20:00"), "2010-2020/2 week 1-12/2 Mo-Fr 10:00-24:00,13:00-20:00")
         self.assertEqual(sanitize_field("2020 mo-fr 1000 - 2000 / 22:20-23:00"), "2020 Mo-Fr 10:00-20:00,22:20-23:00")
         self.assertEqual(sanitize_field("Monday-friday 10h am - 12h / 13h-20h"), "Mo-Fr 10:00-12:00,13:00-20:00")
         self.assertEqual(sanitize_field(u"lundi-vendredi 10h am - 12h / 13h-20h; dimanche fermé"), "Mo-Fr 10:00-12:00,13:00-20:00; Su off")
