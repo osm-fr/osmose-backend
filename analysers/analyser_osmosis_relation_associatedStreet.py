5853f64416a09bfe55cdfa2c49b57fa5eabc1390
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2019-05-09 18:17:27 +0200

    Missing street with name of addr:street close #493

--- analysers/analyser_osmosis_relation_associatedStreet.py
+++ analysers/analyser_osmosis_relation_associatedStreet.py
@@ -27,7 +27,7 @@ CREATE TEMP TABLE ways_addr AS
 SELECT
     'W'::CHAR(1) AS type,
     ways.id,
-    linestring,
+    ST_Transform(linestring, {0}) AS linestring_proj,
     relations.id AS rid,
     relation_members.member_role AS role,
     ways.tags?'addr:flats' AS flats,
@@ -35,6 +35,7 @@ SELECT
     ways.tags->'addr:door' AS door,
     ways.tags->'addr:unit' AS unit,
     relations.tags->'name' AS r_name,
+    ways.tags->'addr:street' AS addr_street,
     coalesce(ways.tags->'addr:street', ways.tags->'addr:district', ways.tags->'addr:neighbourhood', ways.tags->'addr:quarter', ways.tags->'addr:suburb', ways.tags->'addr:place', ways.tags->'addr:hamlet') AS name
 FROM
     ways
@@ -55,7 +56,7 @@ CREATE TEMP TABLE nodes_addr AS
 SELECT
     'N'::CHAR(1) AS type,
     nodes.id,
-    geom,
+    ST_Transform(geom, {0}) AS geom_proj,
     relations.id AS rid,
     relation_members.member_role AS role,
     nodes.tags?'addr:flats' AS flats,
@@ -63,6 +64,7 @@ SELECT
     nodes.tags->'addr:door' AS door,
     nodes.tags->'addr:unit' AS unit,
     relations.tags->'name' AS r_name,
+    nodes.tags->'addr:street' AS addr_street,
     coalesce(nodes.tags->'addr:street', nodes.tags->'addr:district', nodes.tags->'addr:neighbourhood', nodes.tags->'addr:quarter', nodes.tags->'addr:suburb', nodes.tags->'addr:place', nodes.tags->'addr:hamlet') AS name
 FROM
     nodes
@@ -82,7 +84,7 @@ WHERE
 sql10 = """
 SELECT
     id,
-    ST_AsText(way_locate(linestring))
+    ST_AsText(ST_Transform(way_locate(linestring_proj), 4326))
 FROM
     ways_addr
 WHERE
@@ -94,7 +96,7 @@ WHERE
 sql11 = """
 SELECT
     id,
-    ST_AsText(geom)
+    ST_AsText(ST_Transform(geom_proj, 4326))
 FROM
     nodes_addr
 WHERE
@@ -222,7 +224,7 @@ CREATE TEMP TABLE housenumber AS (
 SELECT
     type,
     id,
-    ST_Transform(geom, {0}) AS geom,
+    geom_proj,
     number,
     door,
     unit,
@@ -236,7 +238,7 @@ WHERE
 SELECT
     type,
     id,
-    ST_Transform(ST_Centroid(linestring), {0}) AS geom,
+    ST_Centroid(linestring_proj) AS geom_proj,
     number,
     door,
     unit,
@@ -254,14 +256,14 @@ CREATE INDEX idx_housenumber_r_name_number ON housenumber(r_name, number)
 """
 
 sql62 = """
-CREATE INDEX idx_housenumber_geom ON housenumber USING gist(geom)
+CREATE INDEX idx_housenumber_geom ON housenumber USING gist(geom_proj)
 """
 
 sql63 = """
 SELECT
     CAST(substr(LEAST(hn1.type || hn1.id, hn2.type || hn2.id), 2) AS BIGINT) AS id,
     substr(LEAST(hn1.type || hn1.id, hn2.type || hn2.id), 1, 1) AS type,
-    ST_AsText(ST_Transform(hn1.geom, 4326)),
+    ST_AsText(ST_Transform(hn1.geom_proj, 4326)),
     hn1.r_name,
     hn1.number,
     hn1.door,
@@ -274,14 +276,14 @@ FROM
         hn1.number = hn2.number AND
         ((hn1.door IS NULL AND hn2.door IS NULL) OR hn1.door = hn2.door) AND
         ((hn1.unit IS NULL AND hn2.unit IS NULL) OR hn1.unit = hn2.unit) AND
-        ST_DWithin(hn1.geom, hn2.geom, 1000)
+        ST_DWithin(hn1.geom_proj, hn2.geom_proj, 1000)
 GROUP BY
     LEAST(hn1.type || hn1.id, hn2.type || hn2.id),
     hn1.r_name,
     hn1.number,
     hn1.door,
     hn1.unit,
-    hn1.geom
+    hn1.geom_proj
 """
 
 # Many name in relation
@@ -504,6 +506,46 @@ FROM
     NATURAL JOIN addr_city
 """
 
+# Check addr:street
+sqlD0 = """
+CREATE TEMP TABLE addr_street AS (
+SELECT
+    'N'::char(1) AS type,
+    id,
+    addr_street,
+    geom_proj
+FROM
+    nodes_addr
+WHERE
+    addr_street IS NOT NULL
+) UNION (
+SELECT
+    'W'::char(1) AS type,
+    id,
+    addr_street,
+    linestring_proj AS geom_proj
+FROM
+    ways_addr
+WHERE
+    addr_street IS NOT NULL
+)
+"""
+
+sqlD1 = """
+SELECT
+    addr_street.id,
+    addr_street.type,
+    addr_street.addr_street,
+    ST_AsText(ST_Transform(any_locate(addr_street.type, addr_street.id), 4326))
+FROM
+    addr_street
+    LEFT JOIN highways ON
+        highways.tags->'name' = addr_street.addr_street AND
+        ST_DWithIn(highways.linestring_proj, addr_street.geom_proj, 200)
+WHERE
+    highways.id IS NULL
+"""
+
 # Missing highway in associatedStreet
 sqlF0 = """
 SELECT
@@ -537,6 +579,7 @@ class Analyser_Osmosis_Relation_AssociatedStreet(Analyser_Osmosis):
         if self.config.options.get("addr:city-admin_level"):
             self.classs[12] = {"item":"2060", "level": 2, "tag": ["addr", "fix:chair"], "desc": T_(u"Tag \"addr:city\" not matching a city") }
         self.classs[18] = {"item":"2060", "level": 2, "tag": ["addr", "fix:chair"], "desc": T_(u"Missing highway in associatedStreet relation") }
+        self.classs[19] = {"item":"2060", "level": 2, "tag": ["addr", "fix:chair"], "desc": T_(u"Tag \"addr:street\" not matching a street name around") }
         self.callback20 = lambda res: res[1] and {"class":2, "subclass":1, "data":[self.relation_full, self.positionAsText]}
         self.callback30 = lambda res: {"class":3, "subclass":1, "data":[self.way_full, self.relation, self.positionAsText]}
         self.callback40 = lambda res: {"class":4, "subclass":1, "data":[self.node_full, self.relation, self.positionAsText]}
@@ -547,8 +590,8 @@ class Analyser_Osmosis_Relation_AssociatedStreet(Analyser_Osmosis):
         self.callbackF0 = lambda res: {"class":18, "subclass":1, "data":[self.way_full, self.relation, self.positionAsText]}
 
     def analyser_osmosis_common(self):
-        self.run(sql00)
-        self.run(sql01)
+        self.run(sql00.format(self.config.options.get("proj", 4326)))
+        self.run(sql01.format(self.config.options.get("proj", 4326)))
         self.run(sql10, lambda res: {"class":1, "subclass":1, "data":[self.way_full, self.positionAsText]} )
         self.run(sql11, lambda res: {"class":1, "subclass":2, "data":[self.node_full, self.positionAsText]} )
         if "proj" in self.config.options:
@@ -565,6 +608,9 @@ class Analyser_Osmosis_Relation_AssociatedStreet(Analyser_Osmosis):
         self.run(sql91)
         self.run(sqlA0, lambda res: {"class":8, "subclass":1, "data":[self.relation_full, self.relation_full, self.positionAsText]} )
         self.run(sqlB0, lambda res: {"class":9, "subclass":1, "data":[lambda t: self.typeMapping[res[1]](t), None, self.positionAsText, self.relation_full]} )
+        self.run(sqlD0)
+        if "proj" in self.config.options:
+            self.run(sqlD1, lambda res: {"class":19, "subclass":1, "data":[lambda t: self.typeMapping[res[1]](t), None, None, self.positionAsText], "text":{"en": res[2]}})
         self.run(sqlF0.format(self.config.options.get("proj")), self.callbackF0)
 
     def analyser_osmosis_full(self):
