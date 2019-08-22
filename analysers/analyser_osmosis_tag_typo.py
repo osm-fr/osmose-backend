f3b64bf0660e30b67fa799c406ee5354d9e87b51
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2019-08-22 13:26:24 +0200

    With list charge & change in analyser_osmosis_tag_typo #592

--- analysers/analyser_osmosis_tag_typo.py
+++ analysers/analyser_osmosis_tag_typo.py
@@ -74,6 +74,7 @@ FROM
             'camp_type', 'lamp_type',
             'static_caravans', 'static_caravan',
             'loc_ref', 'lock_ref',
+            'charge', 'change',
             'name_1', 'name_2', 'name_3', 'name_4', 'name_5', 'name_6', 'name_7', 'name_8', 'name_9' -- Tiger mess
         ) AND
         NOT key LIKE 'AND_%'
