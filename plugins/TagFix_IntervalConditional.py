48e8db229b47a79cf2f36eac76e4104ac98d4a5e
PanierAvide <panieravide@riseup.net>
2019-09-16 11:43:21 +0200

    Removed comments, using simple quotes in interval:conditional plugin

--- plugins/TagFix_IntervalConditional.py
+++ plugins/TagFix_IntervalConditional.py
@@ -25,31 +25,31 @@ class TagFix_IntervalConditional(Plugin):
 
     def init(self, logger):
         Plugin.init(self, logger)
-        self.errors[32502] = { "item": 3250, "level": 3, "tag": ["value", "fix:chair", "public_transport"], "desc": T_(u"Invalid Conditional Intervals") }
+        self.errors[32502] = { 'item': 3250, 'level': 3, 'tag': ['value', 'fix:chair', 'public_transport'], 'desc': T_(u'Invalid Conditional Intervals') }
         self._th = Main()
 
     def check_tags(self, tags):
-        if "interval" not in tags and "interval:conditional" not in tags:
+        if 'interval' not in tags and 'interval:conditional' not in tags:
             return
 
         # Analyse all tags related to transport hours (interval, interval:conditional, opening_hours)
         parsedData = self._th.tagsToHoursObject(tags)
 
         # Check validity of interval tag
-        if parsedData["defaultInterval"] == "invalid":
-            return { "class": 32502, "subclass": 1, "text": T_(u"Invalid interval tag format") }
+        if parsedData['defaultInterval'] == 'invalid':
+            return { 'class': 32502, 'subclass': 1, 'text': T_(u'Invalid interval tag format') }
 
         # Check validity of interval:conditional tag
-        if parsedData["otherIntervals"] == "invalid":
-            return { "class": 32502, "subclass": 2, "text": T_(u"Conditional intervals tag is not valid") }
+        if parsedData['otherIntervals'] == 'invalid':
+            return { 'class': 32502, 'subclass': 2, 'text': T_(u'Conditional intervals tag is not valid') }
 
         # Check presence of interval tag if interval:conditional is defined
-        if type(parsedData["otherIntervals"]) is list and parsedData["defaultInterval"] == "unset":
-            return { "class": 32502, "subclass": 3, "text": T_(u"An interval:conditional tag is defined without a default interval tag") }
+        if type(parsedData['otherIntervals']) is list and parsedData['defaultInterval'] == 'unset':
+            return { 'class': 32502, 'subclass': 3, 'text': T_(u'An interval:conditional tag is defined without a default interval tag') }
 
         # Check combination of opening_hours, interval and interval:conditional
-        if "opening_hours" in tags and type(parsedData["opens"]) is dict and parsedData["allComputedIntervals"] == "invalid":
-            return { "class": 32502, "subclass": 4, "text": T_(u"Conditional intervals does not fit into opening hours") }
+        if 'opening_hours' in tags and type(parsedData['opens']) is dict and parsedData['allComputedIntervals'] == 'invalid':
+            return { 'class': 32502, 'subclass': 4, 'text': T_(u'Conditional intervals does not fit into opening hours') }
 
 
     def relation(self, data, tags, members):
@@ -64,11 +64,11 @@ class Test(TestPluginCommon):
         a = TagFix_IntervalConditional(None)
         a.init(None)
 
-        self.check_err(a.relation(None, {"interval": "not minutes"}, None))# Invalid interval=*
-        self.check_err(a.relation(None, {"interval": "01:30", "interval:conditional": "Invalid conditional"}, None)) # Invalid interval:conditional=*
-        self.check_err(a.relation(None, {"interval:conditional": "15 @ (Mo-Sa 10:00-18:00)"}, None)) # interval:conditional without interval
-        self.check_err(a.relation(None, {"opening_hours": "Mo-Fr 05:00-23:00", "interval": "15", "interval:conditional": "5 @ (Mo-Sa 12:00-15:00)"}, None)) # interval:conditional don"t fit in opening_hours
+        self.check_err(a.relation(None, {'interval': 'not minutes'}, None))
+        self.check_err(a.relation(None, {'interval': '01:30', 'interval:conditional': 'Invalid conditional'}, None))
+        self.check_err(a.relation(None, {'interval:conditional': '15 @ (Mo-Sa 10:00-18:00)'}, None))
+        self.check_err(a.relation(None, {'opening_hours': 'Mo-Fr 05:00-23:00', 'interval': '15', 'interval:conditional': '5 @ (Mo-Sa 12:00-15:00)'}, None))
 
-        assert not a.relation(None, {"interval": "00:10"}, None)
-        assert not a.relation(None, {"interval": "00:10", "interval:conditional": "15 @ (Mo-Sa 15:00-17:00)"}, None)
-        assert not a.relation(None, {"opening_hours": "Mo-Su 05:00-23:00", "interval": "00:10", "interval:conditional": "15 @ (Mo-Sa 15:00-17:00)"}, None)
+        assert not a.relation(None, {'interval': '00:10'}, None)
+        assert not a.relation(None, {'interval': '00:10', 'interval:conditional': '15 @ (Mo-Sa 15:00-17:00)'}, None)
+        assert not a.relation(None, {'opening_hours': 'Mo-Su 05:00-23:00', 'interval': '00:10', 'interval:conditional': '15 @ (Mo-Sa 15:00-17:00)'}, None)
