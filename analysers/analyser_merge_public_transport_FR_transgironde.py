51c5d9b2e40ca201cad86bc4cb968e434b4e3458
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2019-09-24 23:16:18 +0200

    Fix typo in analyser_merge_public_transport_FR_transgironde

--- analysers/analyser_merge_public_transport_FR_transgironde.py
+++ analysers/analyser_merge_public_transport_FR_transgironde.py
@@ -64,7 +64,7 @@ class Analyser_Merge_Public_Transport_FR_TransGironde(Analyser_Merge):
         u'Eglise': u'Église',
         u'Rte ': u'Route ',
         u'Bld ': u'Boulevard',
-        u'St ': u'Staint ',
+        u'St ': u'Saint ',
         u'Av. ': u'Avenue',
         u'Hôp.': u'Hôpital',
     }
