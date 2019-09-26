9e209e2c7ff5fb49a9f517868d1767cef00cadd1
Jérôme Amagat <jerome.amagat@gmail.com>
2019-09-26 19:45:09 +0200

    url modified

--- analysers/analyser_merge_radio_support_FR.py
+++ analysers/analyser_merge_radio_support_FR.py
@@ -39,7 +39,7 @@ class _Analyser_Merge_Radio_Support_FR(Analyser_Merge):
                 self.communeNameIndexedByInsee[code_insee] = name_insee
                 
         Analyser_Merge.__init__(self, config, logger,
-            u"https://www.data.gouv.fr/datasets/551d4ff3c751df55da0cd89f",
+            u"https://www.data.gouv.fr/fr/datasets/donnees-sur-les-installations-radioelectriques-de-plus-de-5-watts-1/",
             u"Données sur les installations radioélectriques de plus de 5 watts",
             CSV(Source(attribution = u"data.gouv.fr:ANFR", millesime = "08/2019",
                     fileUrl = u"https://www.data.gouv.fr/fr/datasets/r/5da74526-7781-4726-b98b-232756643090", zip = "SUP_SUPPORT.txt", encoding = "ISO-8859-15"),
