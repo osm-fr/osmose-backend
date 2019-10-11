ad09a1722b4eb31ffe47fcf8c072ced167596fb4
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2019-10-11 20:49:40 +0200

    pynose: Move analyser_sax to a dedicated run
    This removes strange issues when running 'nosetests analysers/*.py'.

--- tools/pynose.sh
+++ tools/pynose.sh
@@ -7,12 +7,17 @@ cd ..
 EXCLUDE_FILES=("plugins/Josm_de_openrailwaymap.py" "plugins/Josm_territories.py")
 
 for dir in "plugins" "plugins/tests" "modules" "analysers"; do
+  echo "Testing $dir"
   PYTHON_FILES=($(ls $dir/*.py))
 
-  for del in ${EXCLUDE_FILES[@]}; do
+  for del in ${EXCLUDE_FILES[@]} analysers/analyser_sax.py; do
     # remove $del from array PYTHON_FILES
     PYTHON_FILES=("${PYTHON_FILES[@]/$del}")
   done
 
   python $(which nosetests) --testmatch='(?:\b)[Tt]est' ${PYTHON_FILES[@]}
 done
+
+# Run analyser_sax separately to remove strange issues with nosetests analysers/*.py
+echo "Testing analysers/analyser_sax.py"
+python $(which nosetests) --testmatch='(?:\b)[Tt]est' analysers/analyser_sax.py
