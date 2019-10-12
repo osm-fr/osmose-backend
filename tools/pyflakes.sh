c97421766a4df085f4df00762f36517c81967848
Stephan <stephankn@users.noreply.github.com>
2019-10-12 19:40:38 +0200

    adjust grep call as pattern is an extended regex

--- tools/pyflakes.sh
+++ tools/pyflakes.sh
@@ -4,4 +4,4 @@ cd ..
 
 # grep will return 0 only if it founds something, but our script
 # wants to return 0 when it founds nothing!
-pyflakes . | grep -v "\('T_'\|/disabled/\)" && exit 1 || exit 0
+pyflakes . | grep -vE "\('T_'\|/disabled/\)" && exit 1 || exit 0
