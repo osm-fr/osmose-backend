f6e8dc9f8554c99d75a176fbb504b2a1421adabb
Francesco Ansanelli <francians@gmail.com>
2020-02-05 19:26:39 +0100

    Import MISE dataset for italian fuel stations

---
+++ analysers/analyser_merge_fuel_IT.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+###########################################################################
+##                                                                       ##
+## Copyright 2020                                                        ##
+##                                                                       ##
+## This program is free software: you can redistribute it and/or modify  ##
+## it under the terms of the GNU General Public License as published by  ##
+## the Free Software Foundation, either version 3 of the License, or     ##
+## (at your option) any later version.                                   ##
+##                                                                       ##
+## This program is distributed in the hope that it will be useful,       ##
+## but WITHOUT ANY WARRANTY; without even the implied warranty of        ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         ##
+## GNU General Public License for more details.                          ##
+##                                                                       ##
+## You should have received a copy of the GNU General Public License     ##
+## along with this program.  If not, see <http://www.gnu.org/licenses/>. ##
+##                                                                       ##
+###########################################################################
+
+from .Analyser_Merge import Analyser_Merge, Source, CSV, Load, Mapping, Select, Generate
+
+
+class Analyser_Merge_Fuel_IT(Analyser_Merge):
+    def __init__(self, config, logger = None):
+        Analyser_Merge.__init__(self, config, logger)
+        self.missing_official = self.def_class(item = 8200, id = 1, level = 3, tags = ['merge', 'highway'],
+            title = T_('Gas station not integrated'))
+        self.possible_merge   = self.def_class(item = 8201, id = 3, level = 3, tags = ['merge', 'highway'],
+            title = T_('Gas station integration suggestion'))
+        self.update_official  = self.def_class(item = 8202, id = 4, level = 3, tags = ['merge', 'highway'],
+            title = T_('Gas station update'))
+
+        self.init(
+            u"https://www.mise.gov.it/index.php/it/open-data/elenco-dataset/2032336-carburanti-prezzi-praticati-e-anagrafica-degli-impianti",
+            u"MISE - Ministero Sviluppo Economico",
+            CSV(Source(attribution = u"MISE - Ministero Sviluppo Economico",
+                    fileUrl = u"https://www.mise.gov.it/images/exportCSV/anagrafica_impianti_attivi.csv",
+                    encoding = "utf-8"),
+                    separator = u";"),
+            Load("Longitudine", "Latitudine",
+                where = lambda row: row["Bandiera"] != u"Pompe Bianche"),
+            Mapping(
+                select = Select(
+                    types = ["nodes", "ways"],
+                    tags = {"amenity": "fuel"}),
+                osmRef = "ref:MISE",
+                conflationDistance = 300,
+                generate = Generate(
+                    static1 = {"amenity": "fuel"},
+                    static2 = {"source": self.source},
+                    mapping1 = {
+                        "ref:MISE": u"idImpianto",
+                        "operator": lambda res: nomalizeString(res[u"Gestore"]),
+                        "brand": u"Bandiera"},
+                text = lambda tags, fields: {"en": u"%s, %s" % (fields["Indirizzo"], fields["Comune"])} )))
+
+    # First Char Uppercase
+    # commas (,) removal
+    # extra spaces trim
+    def nomalizeString(self, str):
+        return " ".join(map(lambda x: x.capitalize(), str.replace(",", " ").split()))
+
