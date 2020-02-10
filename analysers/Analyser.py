86fb93c28af9c5b074e6d83fbb767d6ed57b7a92
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2020-02-10 20:26:40 +0100

    Make sax test more robust on code line changes

--- analysers/Analyser.py
+++ analysers/Analyser.py
@@ -301,6 +301,9 @@ class TestAnalyser(unittest.TestCase):
                                 del c[k][t]
                         if len(c[k]) == 1:
                             c[k] = c[k][0]
+                if "source" in c:
+                    # remove line part
+                    c["source"] = c["source"].split("#")[0]
         else:
             c = a["analysers"][name_analyser]["class"]
             for k in ('classtext', 'detail', 'fix', 'trap', 'example'):
@@ -310,6 +313,9 @@ class TestAnalyser(unittest.TestCase):
                             del c[k][t]
                     if len(c[k]) == 1:
                         c[k] = c[k][0]
+            if "source" in c:
+                # remove line part
+                c["source"] = c["source"].split("#")[0]
 
         if "error" in a["analysers"][name_analyser]:
             if not isinstance(a["analysers"][name_analyser]["error"], list):
