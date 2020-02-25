95f03c3364567569687c32aa9cb21140b2925ab1
Stefal <stephane.peneau@wanadoo.fr>
2020-02-25 13:48:08 +0100

    Adding some more text

--- analysers/analyser_merge_recycling_FR_csma.py
+++ analysers/analyser_merge_recycling_FR_csma.py
@@ -58,4 +58,4 @@ class Analyser_Merge_Recycling_FR_csma(Analyser_Merge):
                         "location": lambda fields: "underground" if fields["type"] == "Colonne enterrée" else None,
                         "operator" : lambda fields : "Clisson Sèvre et Maine Agglo" if fields["detail"] != "vêtements" else "le Relais",
                         "opening_hours": lambda fields : "24/7" if "Déchèterie" not in fields["adresse"] and "Pôle environnement" not in fields["adresse"] else None,},
-                    text = lambda tags, fields : {"en": "%s" %(fields["adresse"])} )))
+                    text = lambda tags, fields : {"en": "%s - %s" %(fields["detail"], fields["adresse"])} )))
