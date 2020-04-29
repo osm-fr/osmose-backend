514c313b66b3577e54d2dbdff75ed2edca8e37bb
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2020-04-29 23:49:45 +0200

    Clean code, fix SourceVersion test

--- modules/SourceVersion.py
+++ modules/SourceVersion.py
@@ -53,7 +53,7 @@ class Test(unittest.TestCase):
 
     def test(self):
         self.assertEqual(version(1), 876922281)
-        self.assertEqual(version(PointInPolygon), 1157204887)
+        self.assertEqual(version(PointInPolygon), 125363717)
 
         try:
             version("1")
