20f650265a77b43adef52f7f9bb0a71e0af589cd
Vincent Bergeot <veille@numetlib.fr>
2020-05-08 12:16:05 +0200

    Update analysers/analyser_merge_parking_FR_BNLS.py
    Co-authored-by: Frédéric Rodrigo <fred.rodrigo@gmail.com>

--- analysers/analyser_merge_parking_FR_BNLS.py
+++ analysers/analyser_merge_parking_FR_BNLS.py
@@ -31,25 +31,25 @@ class Analyser_Merge_Parking_FR_BNLS(Analyser_Merge):
                 detail = T_(
 ''''This is a data from an opendatabase, without any verification'''),
                 fix = T_(
-'''If you're sure that it's a new data for OpenStreetMap, then you can add it with your favorite editor (picture of Id, josm link ?)'''),
+'''If you are sure that it's a new data for OpenStreetMap, then you can add it with your favourite editor (picture of Id, josm link ?).'''),
                 trap = T_(
-'''It's not because it's open that it's right,see it's because it's opendata that OSM contributors needs to check '''),
+'''It is not because it is open that it is good data, see it is because it is opendata that OSM contributors needs to check.'''),
             doc3 = dict(
                 title = T_('Integration suggestion'),
                 detail = T_(
 ''''This is a integration suggestion, mixing opendatabase and OpenStreetMap.'''),
                 fix = T_(
-'''If you're sure that it's a good integration then you can add it with your favorite editor (picture of Id, josm link ?)'''),
+'''If you are sure that it is a good integration then you can add it with your favourite editor (picture of Id, josm link ?).'''),
                 trap = T_(
-'''It's not because there's a integration suggestion that it's right, you're OSM contributors and not a machine !'''),
+'''It is not because there is an integration suggestion that it is right, you are an OSM contributor and not an integration machine.'''),
             doc4 = dict(
                 title = T_('Update'),
                 detail = T_(
-''''This is an update suggestion because there's the same ref in opendatabase and OSM.'''),
+''''This is an update suggestion because there is the same ref in opendatabase and OSM.'''),
                 fix = T_(
-'''If you're sure that it's a good update then you can add it with your favorite editor (picture of Id, josm link ?)'''),
+'''If you are sure that it is a good update then you can add the tag, or part of, it with your favourite editor (picture of Id, josm link ?)'''),
                 trap = T_(
-'''It's not because there's a update suggestion that it's right, you're OSM contributors and not a machine !''')
+'''It is not because there is a update suggestion that it is right, you are an OSM contributor and not an integration machine.''')
 
         self.missing_official = self.def_class(item = 8130, id = 1, level = 3, tags = ['merge', 'parking'],**self.merge_docs(doc1,
             title = T_f('{0} parking not integrated', 'BNLS'),
