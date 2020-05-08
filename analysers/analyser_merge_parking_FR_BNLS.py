dec8f5a8cefa3a0323d8eb6b79f36a9f7f8e98c5
Vincent Bergeot <veille@numetlib.fr>
2020-05-08 09:30:06 +0200

    Update analysers/analyser_merge_parking_FR_BNLS.py
    indentation

--- analysers/analyser_merge_parking_FR_BNLS.py
+++ analysers/analyser_merge_parking_FR_BNLS.py
@@ -26,7 +26,7 @@ from .Analyser_Merge import Analyser_Merge, Source, CSV, Load, Mapping, Select, 
 class Analyser_Merge_Parking_FR_BNLS(Analyser_Merge):
     def __init__(self, config, logger = None):        
         Analyser_Merge.__init__(self, config, logger)
-               doc = dict(
+            doc = dict(
             title = T_('Opendata'),
             detail = T_(
 ''''This is a data from an opendatabase, without any verification'''),
