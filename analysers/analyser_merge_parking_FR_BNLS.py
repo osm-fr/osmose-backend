c09a420f23ee1ac9c4c398dfb9bc6621c6eadba0
Vincent Bergeot <veille@numetlib.fr>
2020-05-11 08:50:30 +0200

    Update analysers/analyser_merge_parking_FR_BNLS.py

--- analysers/analyser_merge_parking_FR_BNLS.py
+++ analysers/analyser_merge_parking_FR_BNLS.py
@@ -71,7 +71,6 @@ on the  wiki. Add a node or add tags if already existing.'''),
             detail = T_(
 '''lorem ipsum specific explanation about update for specific opendata base'''),
             fix = T_(
-'''lorem ipsum, How to fix about update for specific opendata base'''),
         self.init(
             "https://www.data.gouv.fr/fr/datasets/base-nationale-des-lieux-de-stationnement/",
             "Base Nationale des Lieux de Stationnement",
