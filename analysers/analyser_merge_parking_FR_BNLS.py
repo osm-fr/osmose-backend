dd1cfef152f2be0e2035e8b050a29b8e20ce86d9
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2020-05-12 21:55:15 +0200

    Update analysers/analyser_merge_parking_FR_BNLS.py

--- analysers/analyser_merge_parking_FR_BNLS.py
+++ analysers/analyser_merge_parking_FR_BNLS.py
@@ -40,7 +40,7 @@ on the wiki. Add a node or add tags if already existing.''')
             title = T_f('{0} parking integration suggestion', 'BNLS'),
             detail = doc_detail,
             trap = T_(
-'''It is not street parking, it is only closed (with or without fee, for all or not...)''')
+'''It is not street parking, it is only closed (with or without fee, for all or not...)'''))
         self.update_official = self.def_class(item = 8132, id = 54, level = 3, tags = ['merge', 'parking'],
             title = T_f('{0} parking  update', 'BNLS'),
             detail = doc_detail)
