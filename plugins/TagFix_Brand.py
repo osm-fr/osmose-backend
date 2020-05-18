7b911b16bc09223cc48031bbddd0a8ff8b341d03
Noémie <noemie.lehuby@zaclys.net>
2020-05-18 17:19:04 +0200

    nsi sax plugin - handle local brands

--- plugins/TagFix_Brand.py
+++ plugins/TagFix_Brand.py
@@ -24,25 +24,24 @@ from plugins.Plugin import Plugin
 from modules.downloader import urlread
 import json
 
-#TODO : handle country codes
 
 class TagFix_Brand(Plugin):
 
     def init(self, logger):
         Plugin.init(self, logger)
         doc = dict(
-                detail = T_(
-'''This object has a very common name that is likely to be a brand.'''),
-                fix = T_(
-'''If this is indeed a brand, add `brand` and `brand:wikidata` tags.
-If not, report it to the [name-suggestion-index project](https://github.com/osmlab/name-suggestion-index/blob/master/CONTRIBUTING.md)
-which is used here to link frequent names to brands'''))
+            detail=T_(
+                '''This object has a very common name that probably corresponds to a brand name.'''),
+            fix=T_(
+                '''If this is indeed a brand, add `brand` and `brand:wikidata` tags.
+If not, see if you can improve the [name-suggestion-index project](https://github.com/osmlab/name-suggestion-index/blob/master/CONTRIBUTING.md) which is used to link frequent names to brands and their tags'''))
 
         self.errors[31301] = self.def_class(item=3130, level=2, tags=['brand', 'fix:chair'],
                                             title=T_(
                                                 "This name is very common, is it a brand ?"),
                                             resource='https://nsi.guide/', **doc)
 
+        self.country_code = self.father.config.options.get("country").split("-")[0]
         self.frequent_names_from_nsi = self._get_frequent_names()
         self.brands_from_nsi = self._get_brands()
 
@@ -58,21 +57,25 @@ which is used here to link frequent names to brands'''))
         results = json.loads(json_str)
         additional_brands = {}
         for brand_nsi_name, brand in results["brands"].items():
+            if "countryCodes" in brand and self.country_code.lower() not in brand["countryCodes"]:
+                #print("ignoring brand " + brand_nsi_name)
+                continue
+            brand_nsi_name = brand_nsi_name.split("~")[0]
             if "matchTags" in brand:
                 for additional_tag in brand["matchTags"]:
                     nsi_key = "{}|{}".format(additional_tag, brand_nsi_name.split("|")[1])
-                    additional_brands[nsi_key.lower()]=brand
+                    additional_brands[nsi_key.lower()] = brand
             if "matchNames" in brand:
                 for additional_name in brand["matchNames"]:
                     nsi_key = "{}|{}".format(brand_nsi_name.split("|")[0], additional_name)
-                    additional_brands[nsi_key.lower()]=brand
-            additional_brands[brand_nsi_name.lower()]=brand
+                    additional_brands[nsi_key.lower()] = brand
+            additional_brands[brand_nsi_name.lower()] = brand
         return additional_brands
 
     def node(self, data, tags):
         if "name" in tags and not "brand" in tags:
             for main_key in ["shop", "amenity"]:
-                if main_key in tags :
+                if main_key in tags:
                     nsi_key = "{}/{}|{}".format(main_key, tags[main_key], tags["name"]).lower()
                     if nsi_key in self.frequent_names_from_nsi:
                         if nsi_key in self.brands_from_nsi.keys():
@@ -82,10 +85,10 @@ which is used here to link frequent names to brands'''))
                                 if not tags.get(tag):
                                     tags_to_add[tag] = brands_tags[tag]
                             return {"class": 31301, "subclass": 0,
-                                "fix": {"+": tags_to_add }}
+                                    "fix": {"+": tags_to_add}}
                         else:
                             return {"class": 31301, "subclass": 1,
-                                "fix": {"+": {"brand": tags["name"]}}}
+                                    "fix": {"+": {"brand": tags["name"]}}}
 
     def way(self, data, tags, nds):
         return self.node(data, tags)
@@ -102,5 +105,5 @@ class Test(TestPluginCommon):
         a = TagFix_Brand(None)
         a.init(None)
 
-        assert a.node(None, {"name": "Kiabi", "shop":"clothes"})
-        assert not a.node(None, {"brand": "Kiabi", "shop":"clothes", "name":"Kiabi"})
+        assert a.node(None, {"name": "Kiabi", "shop": "clothes"})
+        assert not a.node(None, {"brand": "Kiabi", "shop": "clothes", "name": "Kiabi"})
