d6e5ff4e1715e2dedb08ce1b9fda58774d03a855
didier2020 <didier2020@free.fr>
2020-06-01 15:59:51 +0200

    Update analyser_merge_milestone_FR_metropole.py
    Co-authored-by: Frédéric Rodrigo <fred.rodrigo@gmail.com>

--- analyser_merge_milestone_FR_metropole.py
+++ analyser_merge_milestone_FR_metropole.py
@@ -3,7 +3,7 @@
 
 ###########################################################################
 ##                                                                       ##
-## Copyrights Frédéric Rodrigo 2014-2020                                 ##
+## Copyrights Didier2020 2020                                            ##
 ##            Didier Marchand 2020                                       ##
 ## This program is free software: you can redistribute it and/or modify  ##
 ## it under the terms of the GNU General Public License as published by  ##
