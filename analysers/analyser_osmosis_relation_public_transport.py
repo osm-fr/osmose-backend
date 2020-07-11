25ef1466923c828d2d41a57ddb2a8e980eaf9b3d
Noémie <noemie.lehuby@zaclys.net>
2020-07-11 18:57:02 +0200

    public transport - add tests about stop_position (#926)

--- analysers/analyser_osmosis_relation_public_transport.py
+++ analysers/analyser_osmosis_relation_public_transport.py
@@ -117,6 +117,7 @@ SELECT DISTINCT ON (relations.id, ways.id)
   relations.id,
   relation_members.member_type,
   ways.id AS mid,
+  relation_members.member_role as mrole,
   ST_Transform(ways.linestring, {0}) AS geom
 FROM
   relations
@@ -138,6 +139,7 @@ SELECT DISTINCT ON (relations.id, nodes.id)
   relations.id,
   relation_members.member_type,
   nodes.id AS mid,
+  relation_members.member_role as mrole,
   ST_Transform(nodes.geom, {0}) AS geom
 FROM
   relations
@@ -257,7 +259,80 @@ WHERE
     )
 """
 
+sql60 = """
+SELECT
+    nodes.id,
+    ST_AsText(nodes.geom) AS geom
+FROM
+    nodes
+    JOIN way_nodes ON
+        nodes.id = way_nodes.node_id
+    JOIN highways ON
+    way_nodes.way_id = highways.id
+WHERE
+    nodes.tags != ''::hstore AND
+    nodes.tags?'highway' AND nodes.tags->'highway' = 'bus_stop' AND
+    nodes.tags->'public_transport' != 'stop_position' AND
+    highways.highway NOT IN ('footway', 'platform')
+"""
+
+sql70 = """
+SELECT
+    nodes.id,
+    ST_AsText(nodes.geom) AS geom
+FROM
+    nodes
+    LEFT JOIN way_nodes ON
+        nodes.id = way_nodes.node_id
+WHERE nodes.tags != ''::hstore AND
+    nodes.tags?'public_transport' AND
+    nodes.tags->'public_transport' = 'stop_position' AND
+    node_id is null;
+"""
+
+sql80 = """
+SELECT
+    relations.id,
+    'N' || nodes.id,
+    ST_AsText(nodes.geom) AS geom,
+    way_nodes.way_id
+FROM
+    way_nodes
+    JOIN relation_members ON
+        relation_members.member_type = 'N' AND
+        relation_members.member_id = node_id AND
+        relation_members.member_role NOT IN ('stop', 'stop_exit_only', 'stop_entry_only')
+    JOIN relations ON
+        relation_members.relation_id = relations.id
+    JOIN highways ON
+        way_nodes.way_id = highways.id
+    JOIN nodes ON
+        nodes.id = way_nodes.node_id
+WHERE
+  relations.tags->'type' = 'route' AND
+  relations.tags->'route' IN ('bus', 'trolleybus', 'coach', 'share_taxi', 'school_bus', 'walking_bus') AND
+  highways.highway NOT IN ('footway', 'platform')
+"""
+
+sql90 = """
+SELECT
+    stop_platform.id,
+    stop_platform.member_type || stop_platform.mid,
+    ST_AsText(stop_platform.geom) AS geom
+FROM
+  stop_platform
+  JOIN relations ON
+      stop_platform.id = relations.id
+  LEFT JOIN way_nodes ON
+      mid = node_id
+WHERE
+    mrole IN ('stop', 'stop_exit_only', 'stop_entry_only') AND
+    relations.tags->'public_transport:version' = '2' AND
+    node_id IS NULL;
+"""
+
 class Analyser_Osmosis_Relation_Public_Transport(Analyser_Osmosis):
+    requires_tables_common = ['highways']
 
     def __init__(self, config, logger = None):
         Analyser_Osmosis.__init__(self, config, logger)
@@ -271,6 +346,15 @@ class Analyser_Osmosis_Relation_Public_Transport(Analyser_Osmosis):
             title = T_('Public transport relation route not in route_master relation'))
         self.classs[5] = self.def_class(item = 1260, level = 3, tags = ['public_transport'],
             title = T_('network, operator, ref, colour tag should be the same on route and route_master relations'))
+        self.classs[6] = self.def_class(item = 1260, level = 3, tags = ['public_transport'],
+            title = T_('The bus stop is part of a way, it should have public_transport=stop_position tag'))
+        self.classs[7] = self.def_class(item = 1260, level = 3, tags = ['public_transport'],
+            title = T_('The stop_position is not part of a way'))
+        self.classs[8] = self.def_class(item = 1260, level = 3, tags = ['public_transport'],
+            title = T_('The platform is part of a way, it should have the role stop'))
+        self.classs[9] = self.def_class(item = 1260, level = 3, tags = ['public_transport'],
+            title = T_('The stop is not part of a way'),
+            fix = T_('Change the role in the relation to platform or add the stop to the way and turn it to a public_transport=stop_position'))
 
         self.callback10 = lambda res: {"class":1, "data":[self.relation_full, self.positionAsText]}
         self.callback20 = lambda res: {"class":2, "data":[self.relation_full, self.any_full, self.positionAsText]}
@@ -279,6 +363,11 @@ class Analyser_Osmosis_Relation_Public_Transport(Analyser_Osmosis):
         self.callback50 = lambda res: {"class":5,
              "text": T_("%s are different", ", ".join(filter(lambda r: r, [res[3] and "network", res[4] and "operator", res[5] and "ref", res[6] and "colour"]))),
              "data": [self.relation_full, self.relation_full, self.positionAsText] }
+        self.callback60 = lambda res: {"class":6, "data":[self.node_full, self.positionAsText]}
+        self.callback70 = lambda res: {"class":7, "data":[self.node_full, self.positionAsText]}
+        self.callback80 = lambda res: {"class":8, "data":[self.relation_full, self.any_full, self.positionAsText]}
+        self.callback90 = lambda res: {"class":9, "data":[self.relation_full, self.any_full, self.positionAsText]}
+
 
     def analyser_osmosis_common(self):
         self.run(sql00)
@@ -291,3 +380,7 @@ class Analyser_Osmosis_Relation_Public_Transport(Analyser_Osmosis):
         self.run(sql30, self.callback30)
         self.run(sql40, self.callback40)
         self.run(sql50, self.callback50)
+        self.run(sql60, self.callback60)
+        self.run(sql70, self.callback70)
+        self.run(sql80, self.callback80)
+        self.run(sql90, self.callback90)
