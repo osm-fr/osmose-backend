480318577ba52c3e5ffaf0c5dde88226f8f09f99
Marc Ducobu <marc.ducobu@champs-libres.coop>
2020-09-23 22:43:16 +0200

    replacing postgis by

--- tools/docker-entrypoint.sh
+++ tools/docker-entrypoint.sh
@@ -4,13 +4,13 @@ set -x
 
 # It can take a bit to start the DB inside the container. Wait for it to be ready...
 TIMER="5"
-until pg_isready --host=postgis; do
+until pg_isready --host=$DB_HOST; do
   >&2 echo "PostgrSQL not yet available. Sleeping for $TIMER seconds..."
   sleep $TIMER
 done
 
 if  [ -v POSTGRESQL_POSTCREATION ]; then
-  psql -h postgis osmose postgres -c "$POSTGRESQL_POSTCREATION"
+  psql -h $DB_HOST osmose postgres -c "$POSTGRESQL_POSTCREATION"
 fi
 
 # workaround: when mounting docker with tmpfs on data it fails to set a proper mode on already existing paths, even it ending up as tmpfs
