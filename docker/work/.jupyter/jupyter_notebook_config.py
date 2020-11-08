2c991aed5b21bb576fc21e9620865ada0eb2abd7
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2020-11-08 10:43:26 +0000

    Remove Quit button from jupyter

--- docker/work/.jupyter/jupyter_notebook_config.py
+++ docker/work/.jupyter/jupyter_notebook_config.py
@@ -11,3 +11,5 @@ c.NotebookApp.notebook_dir = 'doc'
 c.NotebookApp.open_browser = False
 
 c.NotebookApp.password = 'sha1:28328cbe81a3:53ac8c5d33f95a7c5dbffc8334b93a3586261b49' # aeje9ahmaiNg
+
+c.NotebookApp.quit_button = False
