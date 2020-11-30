ea52aa0265390b5ee63a89d96cfba792acdd9d7c
Minh Nguyễn <mxn@1ec5.org>
2020-11-29 21:23:17 -0800

    Relaxed key characters for ref:*, hazmat:*

--- plugins/TagFix_BadKey.py
+++ plugins/TagFix_BadKey.py
@@ -60,6 +60,8 @@ separator '_' or ':'. See
                                 "voltage-high", "voltage-low",
                                 "cityracks.housenum", "cityracks.installed", "cityracks.large", "cityracks.rackid", "cityracks.small", "cityracks.street", # NYC amenity=bicycle_parking
                                 "strassen-nrw", # DE import
+                                "ref",
+                                "hazmat",
                              ) )
 
         self.exceptions_whole = set((
@@ -101,6 +103,8 @@ class Test(TestPluginCommon):
                   "ISO3166-1", "ISO3166-1:alpha2", "nhd-shp:fdate",
                   "railway:memor2+", "railway:tbl1+",
                   "strassen-nrw:abs",
+                  "ref:clochers.org",
+                  "hazmat:4.3",
                  ]:
             assert not a.node(None, {k: '1'}), ("key='{0}'".format(k))
 
