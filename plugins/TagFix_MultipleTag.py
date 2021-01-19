bec3c2b93863fff41465761a3423e461451d2aaa
Jeroen Hoek <mail@jeroenhoek.nl>
2021-01-19 21:28:49 +0100

    Treat building:part as top-level tag
    Fixes #1109.

--- plugins/TagFix_MultipleTag.py
+++ plugins/TagFix_MultipleTag.py
@@ -28,7 +28,7 @@ class TagFix_MultipleTag(Plugin):
     def init(self, logger):
         Plugin.init(self, logger)
         self.country = self.father.config.options.get("country")
-        main_tags = ('type', 'aerialway', 'aeroway', 'amenity', 'barrier', 'boundary', 'building', 'craft', 'entrance', 'emergency', 'geological', 'highway', 'historic', 'landuse', 'leisure', 'man_made', 'military', 'natural', 'office', 'place', 'power', 'public_transport', 'railway', 'route', 'shop', 'sport', 'tourism', 'waterway', 'mountain_pass', 'traffic_sign', 'golf', 'piste:type', 'junction', 'healthcare', 'health_facility:type', 'indoor', 'club', 'seamark:type', 'attraction', 'information', 'advertising', 'ford')
+        main_tags = ('type', 'aerialway', 'aeroway', 'amenity', 'barrier', 'boundary', 'building', "building:part", 'craft', 'entrance', 'emergency', 'geological', 'highway', 'historic', 'landuse', 'leisure', 'man_made', 'military', 'natural', 'office', 'place', 'power', 'public_transport', 'railway', 'route', 'shop', 'sport', 'tourism', 'waterway', 'mountain_pass', 'traffic_sign', 'golf', 'piste:type', 'junction', 'healthcare', 'health_facility:type', 'indoor', 'club', 'seamark:type', 'attraction', 'information', 'advertising', 'ford')
 
         self.errors[30320] = self.def_class(item = 3032, level = 1, tags = ['tag', 'highway', 'fix:chair'],
             title = T_('Watch multiple tags'))
@@ -265,6 +265,7 @@ class Test(TestPluginCommon):
         assert not a.node(None, {"name": "foo", "disused:highway": "bar"})
         assert not a.node(None, {"name": "foo", "abandoned:highway": "bar"})
         assert not a.node(None, {"name": "foo", "historic:railway": "station"})
+        assert not a.node(None, {"name": "foo", "building:part": "yes"})
 
         self.check_err(a.way(None, {"waterway": "stream", "level": "-1"}, None))
 
