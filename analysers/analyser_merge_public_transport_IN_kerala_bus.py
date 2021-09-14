d2db133118087b124115b84a51cbfc03f7ae8f8d
Noémie Lehuby <noemie.lehuby@zaclys.net>
2021-09-14 19:07:41 +0200

    Fix merge bus stops in Kerala, India

--- analysers/analyser_merge_public_transport_IN_kerala_bus.py
+++ analysers/analyser_merge_public_transport_IN_kerala_bus.py
@@ -21,7 +21,7 @@
 ###########################################################################
 
 from modules.OsmoseTranslation import T_
-from .Analyser_Merge import Analyser_Merge, Source, GeoJSON, Load, Mapping, Select
+from .Analyser_Merge import Analyser_Merge, Source, GeoJSON, Load, Conflate, Mapping, Select
 
 
 class Analyser_Merge_Public_Transport_IN_Kerala_Bus_Stops(Analyser_Merge):
@@ -33,16 +33,16 @@ class Analyser_Merge_Public_Transport_IN_Kerala_Bus_Stops(Analyser_Merge):
         self.init(
             u"https://opensdi.kerala.gov.in/",
             u"Kerala Bus Stops",
-            GeoJSON(Source(attribution = u"Kerala State Electronics Development Corporation Limited", millesime = "07/2021",
+            GeoJSON(Source(attribution = u"Kerala State Electronics Development Corporation Limited", millesime = "09/2021",
                     fileUrl = u"https://opensdi.kerala.gov.in/geoserver/ows?service=WFS&version=1.0.0&request=GetFeature&typename=geonode%3AKerala_Bus_Stops&outputFormat=json&srs=EPSG%3A32643&srsName=EPSG%3A4326"),
                 extractor = lambda geojson: geojson),
             Load("geom_x", "geom_y"),
-            Mapping(
+            Conflate(
                 select = Select(
                     types = ["nodes"],
                     tags = {"highway": "bus_stop"}),
                 conflationDistance = 100,
-                generate = Mapping(
+                mapping = Mapping(
                     static1 = {
                         "highway": "bus_stop",
                         "public_transport": "platform",
