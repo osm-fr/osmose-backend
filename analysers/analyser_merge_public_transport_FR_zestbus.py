dc4de594eefba7a3ca2d4005b8a3d339c438d258
Théo <github@theopeltier.me>
2022-02-03 13:16:07 +0100

    Create analyser_merge script for Zest'Bus network

---
+++ analysers/analyser_merge_public_transport_FR_zestbus.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+###########################################################################
+##                                                                       ##
+## Copyrights Théo Peltier 2022                                          ##
+##                                                                       ##
+## This program is free software: you can redistribute it and/or modify  ##
+## it under the terms of the GNU General Public License as published by  ##
+## the Free Software Foundation, either version 3 of the License, or     ##
+## (at your option) any later version.                                   ##
+##                                                                       ##
+## This program is distributed in the hope that it will be useful,       ##
+## but WITHOUT ANY WARRANTY; without even the implied warranty of        ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         ##
+## GNU General Public License for more details.                          ##
+##                                                                       ##
+## You should have received a copy of the GNU General Public License     ##
+## along with this program.  If not, see <http://www.gnu.org/licenses/>. ##
+##                                                                       ##
+###########################################################################
+
+from modules.OsmoseTranslation import T_
+from .Analyser_Merge import Analyser_Merge, GTFS, Load, Conflate, Select, Mapping, Source
+
+
+class Analyser_Merge_Public_Transport_FR_zestbus(Analyser_Merge):
+
+    def __init__(self, config, logger = None):
+        Analyser_Merge.__init__(self, config, logger)
+        place = "Zest'Bus"
+        self.def_class_missing_official(item = 8040, id = 41, level = 3, tags = ['merge', 'public transport', 'fix:survey', 'fix:picture'],
+                                        title = T_('{0} stop not integrated', place))
+        self.def_class_possible_merge(item = 8041, id = 43, level = 3, tags = ['merge', 'public transport', 'fix:chair'],
+                                      title = T_('{0} stop, integration suggestion', place))
+        self.def_class_update_official(item = 8042, id = 44, level = 3, tags = ['merge', 'public transport', 'fix:chair'],
+                                       title = T_('{0} stop update', place))
+
+        self.init(
+            "https://www.data.gouv.fr/fr/datasets/horaires-reseau-zest/",
+            "Horaires du réseau de transport urbain des communes de Menton et de la communauté d'agglomération de la Riviera française",
+            GTFS(Source(attribution = "Keolis Menton Riviera", millesime = "12/2022",
+                        fileUrl = "https://www.data.gouv.fr/fr/datasets/r/72609821-2459-47fb-a63b-3dbbc0d96c92")),
+            Load("stop_lon", "stop_lat",
+                 select = {"parent_station": {'like': 'ARCOM%'}},
+
+                 ),
+            Conflate(
+                select = Select(
+                    types = ["nodes", "ways"],
+                    tags = [{"highway": "bus_stop", "public_transport": "platform"},{"highway": "bus_stop", "public_transport": False}, {"highway":"platform"}]),
+                osmRef = "ref:FR:ZEST",
+                conflationDistance = 100,
+                mapping = Mapping(
+                    static1 = {
+                        "highway": "bus_stop",
+                        "public_transport": "platform",
+                        "bus": "yes"},
+                    static2 = {"source": self.source},
+                    mapping1 = {
+                        "ref:FR:ZEST": lambda res: res["stop_code"],
+                        "name": lambda res: self.replace(res['stop_name'].title()),
+                        "wheelchair": lambda res: 'yes' if res["wheelchair_boarding"] == '1' else 'no' if res["wheelchair_boarding"] == '0' else None,
+                    },
+                    text = lambda tags, fields: T_(f"{place} stop of {fields['stop_name']}")))
+        )
+
+
+    def replace(self, string):
+        for term, translation in self.replacement.items():
+            string = string.replace(term, translation)
+        return string
+
+    replacement = {
+        ' De ': ' de ',
+        ' Des ': ' des ',
+        ' Du ': ' du ',
+        ' La ': ' la ',
+        ' et ': ' et ',
+        'Rd ': 'RD ' # Route départementale
+    }
