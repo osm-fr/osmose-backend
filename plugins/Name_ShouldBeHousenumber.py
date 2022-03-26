b22ccdd8e35ed0c46f8c842b2c481e10a6f6cc70
arrival-spring <lnnunns+git@gmail.com>
2022-03-26 12:10:03 +0000

    Use blacklist of features

--- plugins/Name_ShouldBeHousenumber.py
+++ plugins/Name_ShouldBeHousenumber.py
@@ -30,19 +30,20 @@ class Name_ShouldBeHousenumber(Plugin):
         self.errors[804] = self.def_class(item = 5010, level = 1, tags = ['name', 'fix:chair'],
             title = T_('Name with only numbers'),
             detail = T_(
-'''This residential building is tagged with a name which contains only numbers.
+'''This building is tagged with a name which contains only numbers
+and does not appear to have a main feature key.
  For the majority of buildings this should be tagged as the housenumber.'''),
             trap = T_(
 '''While uncommon, it is possible for a name to be only numbers.
  This is particularly the case for some brands or amenities.''')
         )
         self.numerical = re.compile(r"^[0-9]+$")
-        self.residential_buildings = ["house", "residential", "apartments", "detached", "terrace", "dormitory", "semi", "semidetached_house"]
+        self.feature_keys = ["amenity", "craft", "emergency", "leisure", "military", "office", "railway", "shop", "tourism"]
 
     def node(self, data, tags):
         err = []
         if u"building" in tags:
-            if tags["building"] in self.residential_buildings:
+            if any(feature in tags for feature in self.feature_keys):
                 if u"name" in tags:
                     if self.Numerical.match(tags[u"name"]):
                         err.append({"class": 804, "text": T_("Concerns tag: `{0}`", '='.join(['name', tags['name']])) })
@@ -62,15 +63,15 @@ class Test(TestPluginCommon):
     def test(self):
         a = Name_ShouldBeHousenumber(None)
         a.init(None)
-        for t in [{u"building": u"house", u"name": "123"},
+        for t in [{u"building": u"yes", u"name": "123"},
                   {u"building": u"residential", u"name": "5"},
                   {u"building": u"detached", u"name": "5637"},
                  ]:
             self.check_err(a.node(None, t), t)
             self.check_err(a.way(None, t, None), t)
 
-        for t in [{u"building": u"house", u"name": "12 Flats"},
-                  {u"building": u"retail", u"name": "21"},
-                  {u"building": u"semi", u"name": "25a"},
+        for t in [{u"building": u"yes", u"name": "12 Flats"},
+                  {u"building": u"yes", u"name": "21", u"shop": "yes"},
+                  {u"building": u"commercial", u"name": "1", u"office": "yes"},
                  ]:
             assert not a.node(None, t), t
