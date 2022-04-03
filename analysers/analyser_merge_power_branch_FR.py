2467a6605ab86fd0e5a619c6af3fdec55a3f00c8
FLacombe <francois.lacombe@infos-reseaux.com>
2022-04-03 16:37:46 +0200

    Improve titles and copyright notice

--- analysers/analyser_merge_power_branch_FR.py
+++ analysers/analyser_merge_power_branch_FR.py
@@ -3,7 +3,7 @@
 
 ###########################################################################
 ##                                                                       ##
-## Copyrights Frédéric Rodrigo 2017                                      ##
+## Copyrights François Lacombe 2022                                      ##
 ##                                                                       ##
 ## This program is free software: you can redistribute it and/or modify  ##
 ## it under the terms of the GNU General Public License as published by  ##
@@ -28,16 +28,16 @@ class Analyser_Merge_Power_Substation_FR(Analyser_Merge):
     def __init__(self, config, logger = None):
         Analyser_Merge.__init__(self, config, logger)
         self.def_class_missing_osm(item = 7190, id = 22, level = 3, tags = ['merge', 'power', 'fix:chair'],
-            title = T_('Power transmission branch not known from operator'),
-            detail = T_('This power transmission branch from OSM is now known to operator. It may have been destroyed or moved.'))
+            title = T_('Power connection not known from operator'),
+            detail = T_('This power transmission branch from OSM is not known to operator. It may have been destroyed or moved.'))
         self.def_class_possible_merge(item = 8281, id = 23, level = 3, tags = ['merge', 'power', 'fix:chair'],
-            title = T_('Power transmission branch, integration suggestion'),
+            title = T_('Power connection, integration suggestion'),
             detail = T_('This existing power transmission branch can be integrated with official attributes.'))
         self.def_class_update_official(item = 8282, id = 24, level = 3, tags = ['merge', 'power', 'fix:chair'],
-            title = T_('Power transmission branch update'),
+            title = T_('Power connection update'),
             detail = T_('This existing power transmission branch can be updated with following values.'))
         self.def_class_missing_official(item = 8280, id = 21, level = 3, tags = ['merge', 'power', 'fix:survey', 'fix:picture', 'fix:imagery'],
-            title = T_('Power transmission branch is missing in OSM or without tag "ref:FR:RTE"'),
+            title = T_('Power connection is missing in OSM or without tag "ref:FR:RTE"'),
             detail = T_('This power transmission branch is missing in OSM or lacks an official identifier.'))
 
         self.init(
