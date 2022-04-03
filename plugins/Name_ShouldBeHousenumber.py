e949c3b587cd6a38ee4be8ef7d9dc0a4aff4f3e1
arrival-spring <77166354+arrival-spring@users.noreply.github.com>
2022-04-03 11:01:40 +0100

    Numerical name plugin (#1406)
    * Numerical name plugin
    
    * Use blacklist of features
    
    * Missing not
    
    * Fixes from review
    
    * Add fix suggestion
    
    * Change level

---
+++ plugins/Name_ShouldBeHousenumber.py
@@ -0,0 +1,81 @@
+#-*- coding: utf-8 -*-
+
+###########################################################################
+##                                                                       ##
+## Copyrights arrival-spring 2022                                        ##
+##                                                                       ##
+## This program is free software: you can redistribute it and/or modify  ##
+## it under the terms of the GNU General Public License as published by  ##
+## the Free Software Foundation, either version 3 of the License, or     ##
+## (at your option) any later version.                                   ##
+##                                                                       ##
+## This program is distributed in the hope that it will be useful,       ##
+## but WITHOUT ANY WARRANTY; without even the implied warranty of        ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         ##
+## GNU General Public License for more details.                          ##
+##                                                                       ##
+## You should have received a copy of the GNU General Public License     ##
+## along with this program.  If not, see <http://www.gnu.org/licenses/>. ##
+##                                                                       ##
+###########################################################################
+
+from modules.OsmoseTranslation import T_
+from plugins.Plugin import Plugin
+import regex as re
+
+class Name_ShouldBeHousenumber(Plugin):
+
+    def init(self, logger):
+        Plugin.init(self, logger)
+        self.errors[804] = self.def_class(item = 5010, level = 2, tags = ['name', 'fix:chair'],
+            title = T_('Name with only numbers'),
+            detail = T_(
+'''This building is tagged with a name which contains only numbers
+and does not appear to have a main feature key.
+For the majority of buildings this should be tagged with `addr:housenumber`.'''),
+            fix = T_(
+'''Change the name key to `addr:housenumber` (if it is not already present and is not different).'''),
+            trap = T_(
+'''While uncommon, it is possible for a name to be only numbers.
+ This is particularly the case for some brands or amenities.''')
+        )
+        self.Numerical = re.compile(r"^[0-9]+$")
+        self.feature_keys = ["amenity", "craft", "emergency", "leisure", "military", "office", "railway", "shop", "tourism"]
+
+    def node(self, data, tags):
+        err = []
+        if "building" in tags:
+            if not any(feature in tags for feature in self.feature_keys):
+                if "name" in tags:
+                    if self.Numerical.match(tags["name"]):
+                        err.append({"class": 804,
+                            "text": T_("Concerns tag: `{0}`", '='.join(['name', tags['name']])),
+                            "fix": [{"+": {"addr:housenumber": tags["name"]}}, {"-": ["name"]}]})
+        return err
+
+    def way(self, data, tags, nds):
+        return self.node(data, tags)
+
+    def relation(self, data, tags, members):
+        return self.node(data, tags)
+
+
+###########################################################################
+from plugins.Plugin import TestPluginCommon
+
+class Test(TestPluginCommon):
+    def test(self):
+        a = Name_ShouldBeHousenumber(None)
+        a.init(None)
+        for t in [{"building": "yes", "name": "123"},
+                  {"building": "residential", "name": "5"},
+                  {"building": "detached", "name": "5637"},
+                 ]:
+            self.check_err(a.node(None, t), t)
+            self.check_err(a.way(None, t, None), t)
+
+        for t in [{"building": "yes", "name": "12 Flats"},
+                  {"building": "yes", "name": "21", "shop": "yes"},
+                  {"building": "commercial", "name": "1", "office": "yes"},
+                 ]:
+            assert not a.node(None, t), t
