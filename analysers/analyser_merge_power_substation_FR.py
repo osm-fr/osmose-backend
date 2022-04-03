ddf5f16a3136360c16a7693e0f430e563af68d78
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2022-04-03 17:41:07 +0200

    Update analysers/analyser_merge_power_substation_FR.py

--- analysers/analyser_merge_power_substation_FR.py
+++ analysers/analyser_merge_power_substation_FR.py
@@ -30,8 +30,7 @@ class Analyser_Merge_Power_Substation_FR(Analyser_Merge):
         self.def_class_missing_osm(item = 7190, id = 2, level = 3, tags = ['merge', 'power', 'fix:chair'],
             title = T_('Power substation is not known from operator'))
         self.def_class_possible_merge(item = 8281, id = 3, level = 3, tags = ['merge', 'power', 'fix:chair'],
-            title = T_('Power substation, integration suggestion'),
-            detail = T_('This existing power substation can be integrated with official attributes.'))
+            title = T_('Power substation, integration suggestion'))
         self.def_class_update_official(item = 8282, id = 4, level = 3, tags = ['merge', 'power', 'fix:chair'],
             title = T_('Power substation update'),
             detail = T_('This existing power substation can be updated with following values.'))
