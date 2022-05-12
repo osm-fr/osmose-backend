7622f09492dfc9c7ba7e0a39e9343de0905a9ad5
emerson <git@emersonveenstra.net>
2022-05-12 15:26:35 -0400

    fixed highway=service

--- plugins/TagRemove_Incompatibles.py
+++ plugins/TagRemove_Incompatibles.py
@@ -73,7 +73,7 @@ class TagRemove_Incompatibles(Plugin):
                 ['footway', 'railway', 'disused'],
                 ['path', 'railway', 'disused'],
                 ['service', 'amenity', 'weighbridge'],
-                ['slipway', 'leisure', 'slipway'],
+                ['service', 'leisure', 'slipway'],
             ],
             'natural': [
                 ['water', 'leisure', 'marina'],
