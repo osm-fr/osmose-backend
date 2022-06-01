3d733a318a7a7b97341cb3b9a028f6efd923a42c
Famlam <fam.lam@live.nl>
2022-06-01 21:50:10 +0200

    More exceptions
    cafe -> cave (https://osmose.openstreetmap.fr/nl/issue/b5fe2db0-1cad-287e-405a-6dfb7f27af3d, https://www.openstreetmap.org/node/4121332496)
    produce -> product (https://osmose.openstreetmap.fr/nl/issue/c39054ee-1210-35d1-389c-2c35450a71a1, https://www.openstreetmap.org/way/827666026)

--- analysers/analyser_osmosis_tag_typo.py
+++ analysers/analyser_osmosis_tag_typo.py
@@ -83,6 +83,8 @@ FROM
             'massage', 'message',
             'bath',
             'port',
+            'cave',
+            'produce',
             'name_1', 'name_2', 'name_3', 'name_4', 'name_5', 'name_6', 'name_7', 'name_8', 'name_9', -- Tiger mess
 
             -- Regional hiking/cycling/etc. routes. Lesser used ones like 'rhn' for horse riding trigger false positives:
