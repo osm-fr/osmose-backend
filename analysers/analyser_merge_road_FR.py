52786a9b99ee0819d9bf203e4c09978fdd278180
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2022-09-15 20:57:46 +0000

    Use dep code in analyser_merge_road_FR

--- analysers/analyser_merge_road_FR.py
+++ analysers/analyser_merge_road_FR.py
@@ -31,12 +31,13 @@ class Analyser_Merge_Road_FR(Analyser_Merge_Network):
         self.def_class_missing_official(item = 7170, id = 13, level = 2, tags = ['merge', 'highway', 'fix:survey', 'fix:imagery'],
             title = T_('Road not integrated'))
 
+        dep_code = config.options.get('dep_code') or config.options.get('country').split('-')[1]
         self.init(
             "https://ign.fr",
             "IGN-troncon_de_route",
             GPKG(SourceIGN(attribution = "IGN",
-                    fileUrl = "http://files.opendatarchives.fr/professionnels.ign.fr/bdtopo/latest/BDTOPO_3-0_TOUSTHEMES_GPKG_LAMB93_D033_2022-06-15.7z",
-                    extract = "BDTOPO_3-0_TOUSTHEMES_GPKG_LAMB93_D033_2022-06-15/BDTOPO/1_DONNEES_LIVRAISON_2022-06-00173/BDT_3-0_GPKG_LAMB93_D033-ED2022-06-15/BDT_3-0_GPKG_LAMB93_D033-ED2022-06-15.gpkg"),
+                    fileUrl = f"http://files.opendatarchives.fr/professionnels.ign.fr/bdtopo/latest/BDTOPO_3-0_TOUSTHEMES_GPKG_LAMB93_D{dep_code}_2022-06-15.7z",
+                    extract = f"BDTOPO_3-0_TOUSTHEMES_GPKG_LAMB93_D{dep_code}_2022-06-15/BDTOPO/1_DONNEES_LIVRAISON_2022-06-00173/BDT_3-0_GPKG_LAMB93_D{dep_code}-ED2022-06-15/BDT_3-0_GPKG_LAMB93_D{dep_code}-ED2022-06-15.gpkg"),
                 layer = "troncon_de_route"),
             Load('geom',
                 table_name = 'road_fr_' + config.options['country'].replace("-", "_"),
