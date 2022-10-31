7fd473e43194be0e311a80261ce39a493732f6d7
Famlam <fam.lam@live.nl>
2022-10-31 22:28:21 +0100

    Separate whitelists for similar and equal values (#1630)

--- plugins/TagFix_DuplicateValue.py
+++ plugins/TagFix_DuplicateValue.py
@@ -37,7 +37,9 @@ similar.'''),
             fix = T_(
 '''Delete one value.'''),
             trap = T_(
-'''In some case all values maybe required.'''))
+'''In some cases all values may be required.
+
+Ensure the interpretation of the tag does not change when you delete one item.'''))
         self.errors[3060]  = self.def_class(item = 3060, level = 3, tags = ['value', 'fix:chair'],
             title = T_('Duplicated values'),
             **doc)
@@ -46,39 +48,53 @@ similar.'''),
             resource = 'https://en.wikipedia.org/wiki/Levenshtein_distance',
             **doc)
 
-        self.BlackList = set((
-            'ref', 'created_by', 'is_in',
+        self.WhitelistSimilarEqual = set(( # Keys that can have similar and even equal values
             'CLC:id', 'GNS:id', 'tmc', 'tiger:cfcc', 'statscan:rbuid', 'nysgissam:nysaddresspointid',
-            'source:geometry:date', 'source:geometry:ref', # Belgium, Flanders
+            'ref',
             'source:date',
-            'service_times', 'collection_times',
-            'phone', 'contact:phone', 'fax', 'contact:fax',
-            'url',
-            'technology', 'cables', 'position', 'passenger', 'couplings:diameters',
-            'healthcare:speciality',
-            'traffic_sign',
-            'sport',
+            'source:geometry:date', # Belgium, Flanders
+            'technology',
+            'cables',
+            'position',
+            'couplings:diameters',
             'voltage',
             'addr:flats', 'addr:housenumber', 'addr:unit', 'addr:floor', 'addr:block', 'addr:door',
         ))
-        self.BlackListRegex = set((
+        self.WhitelistSimilar = set(( # Keys that can have similar, but not equal values
+            'source:geometry:ref', # Belgium, Flanders
+            'created_by',
+            'is_in',
+            'service_times', 'collection_times',
+            'phone', 'contact:phone', 'fax', 'contact:fax',
+            'email',
+            'url',
+            'destination',
+            'passenger',
+            'healthcare:speciality',
+            'traffic_sign', 'traffic_sign:forward', 'traffic_sign:backward',
+            'sport',
+            'communication:mobile_phone',
+            'cuisine',
+            'operator',
+        ))
+        self.WhitelistSimilarEqualRegex = set(( # Regexes for keys that can have similar and even equal values
             re.compile('seamark:.+:colour'),
             re.compile('.+_ref'), re.compile('ref:.+'),
             re.compile('destination:.+'),
-            re.compile('AND_.+'), re.compile('AND:.+'),
+            re.compile('AND_.+'), re.compile('AND:.+'), # Netherlands
             re.compile('[Nn][Hh][Dd]:.+'),
             re.compile('massgis:.+'),
-            re.compile('maxspeed(:.+)?'),
-            re.compile('maxheight(:.+)?'),
-            re.compile('maxwidth(:.+)?'),
-            re.compile('maxweight(:.+)?'),
             re.compile('lacounty:.+'),
-            re.compile('.+:conditional'),
-            re.compile('railway:signal:.+'),
             re.compile('turn:lanes.*'),
+        ))
+        self.WhitelistSimilarRegex = set(( # Regexes for keys that can have similar, but not equal values
+            re.compile('.+_name'),
+            re.compile('.+:conditional'),
+            re.compile('.+:date'),
             re.compile('opening_hours(:.+)?'),
             re.compile('(.+:)?wikidata'),
-       ))
+            re.compile('railway:signal:.+'),
+        ))
 
     # http://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#Python
     def levenshtein(self, s1, s2):
@@ -99,37 +115,45 @@ similar.'''),
 
         return previous_row[-1]
 
+    def anyRegexMatch(self, set_of_regexes, key):
+        for r in set_of_regexes:
+            if r.match(key):
+                return True
+        return False
+
     def node(self, data, tags):
         err = []
         keys = tags.keys()
+
         for k in keys:
-            if k in self.BlackList:
-                continue
-
-            try:
-                for blr in self.BlackListRegex:
-                    if blr.match(k):
-                        raise Exception
-            except Exception:
-                continue
-
+            if k in self.WhitelistSimilarEqual:
+                continue # Key may have equal or similar values
             v = tags[k]
+            if not ';' in v:
+                continue
+            if self.anyRegexMatch(self.WhitelistSimilarEqualRegex, k):
+                continue # Key may have equal or similar values
             if k == 'source':
                 v = v.replace('Cadastre ; mise', 'Cadastre, mise') # France
                 v = v.replace('GSImaps/ort', 'GSImaps/std') # Japan
-            if ';' in v:
-                vs = list(filter(lambda w: len(w) > 0, map(lambda w: w.strip(), v.split(';'))))
-                if len(vs) != len(set(vs)):
-                    err.append({"class": 3060, "subclass": stablehash64(k),
-                                "text": T_("Duplicated values {key}={val}", key = k, val = tags[k]),
-                                "fix": {k: ";".join(set(vs))} })
-                else:
-                    vs_long = filter(lambda w: len(w) > 6, vs)
-                    for v1,v2 in itertools.combinations(vs_long, 2):
-                        if abs(len(v1)-len(v2)) < 4 and self.levenshtein(v1, v2) < 4:
-                            err.append({"class": 30601, "subclass": stablehash64(k),
-                                        "text": T_("Duplicated similar values {key}={val}", key = k, val = tags[k])})
-                            break
+            vs = list(filter(lambda w: len(w) > 0, map(lambda w: w.strip(), v.split(';'))))
+
+            if len(vs) != len(set(vs)):
+                err.append({"class": 3060, "subclass": stablehash64(k),
+                            "text": T_("Duplicated values {key}={val}", key = k, val = tags[k]),
+                            "fix": {k: ";".join(set(vs))} })
+            else:
+                if k in self.WhitelistSimilar:
+                    continue # Key may have similar values
+                if self.anyRegexMatch(self.WhitelistSimilarRegex, k):
+                    continue # Key may have similar values
+
+                vs_long = filter(lambda w: len(w) > 6, vs)
+                for v1,v2 in itertools.combinations(vs_long, 2):
+                    if abs(len(v1)-len(v2)) < 4 and self.levenshtein(v1, v2) < 4:
+                        err.append({"class": 30601, "subclass": stablehash64(k),
+                                    "text": T_("Similar values {v1} and {v2} in {key}={val}", v1 = v1, v2 = v2, key = k, val = tags[k])})
+                        break
 
         return err
 
@@ -151,6 +175,8 @@ class Test(TestPluginCommon):
         for t in [{"oneway":"yes;yes"},
                   {"oneway":"yes;yes;no"},
                   {"oneway":"yes;yes;yes;yes;-1;-1;no;no"},
+                  {"passenger":"national;national"},
+                  {"opening_hours": "Mo 14:00-19:00; Tu-Fr 10:00-14:00,15:00-19:00; Mo 14:00-19:00"},
                   {"source":u"cadastre-dgi-fr source : Direction Générale des Impôts - Cadastre ; mise à jour : 2013;cadastre-dgi-fr source : Direction Générale des Impôts - Cadastre ; mise à jour : 2013"},
                   {"source":u"cadastre-dgi-fr source : Direction Générale des Impôts - Cadastre ; mise à jour : 2010;cadastre-dgi-fr source : Direction Générale des Impôts - Cadastre ; mise à jour : 2013"},
                   {"source":"GSImaps/ort;GSImaps/std"},
@@ -164,6 +190,7 @@ class Test(TestPluginCommon):
                   {"ref:mhs":"IA00070520; IA00070492"},
                   {"opening_hours": "Mo 14:00-19:00; Tu-Fr 10:00-14:00,15:00-19:00; Sa 10:00-19:00"},
                   {"oneway":"yes;no"},
+                  {"passenger":"national;international;regional"},
                   {"AND_toto":"121;121;121"},
                   {"NHD:ComID":"141725410;141725411"},
                   {"massgis:OS_ID":"305-735;305-764"},
