f70eacc9944095be09d4e1375ce50b72349f86d1
Famlam <fam.lam@live.nl>
2022-11-06 12:55:28 +0100

    Add analyser and test
    - Test for inconsistencies between vehicles allowed on a highway and vehicles allowed to pass a barrier on that highway. Only for bollard and bus_trap as they have access/motor_vehicle=no by default
    
    - Also fix the detail text of class 1 (there's no test for destination)
    
    - Include a test file
    
    - Only test for the ones that are relatively common to have exceptions on a certain type of barrier

--- analysers/analyser_osmosis_highway_area_access.py
+++ analysers/analyser_osmosis_highway_area_access.py
@@ -51,20 +51,87 @@ WHERE
   )
 """
 
+sql20 = """
+CREATE TEMP TABLE {barriertype} AS
+SELECT
+  barrier.id AS id,
+  barrier.tags AS tags,
+  ways.id as wid,
+  ST_AsText(barrier.geom) AS geom
+FROM
+  nodes AS barrier
+  JOIN way_nodes ON
+    way_nodes.node_id = barrier.id
+  JOIN highways AS ways ON
+    ways.id = way_nodes.way_id AND
+    NOT ways.is_construction AND
+    NOT ways.is_area
+  LEFT JOIN highway_ends ON
+    ways.id = highway_ends.id AND
+    barrier.id = highway_ends.nid
+WHERE
+  barrier.tags != ''::hstore AND
+  barrier.tags?'barrier' AND
+  barrier.tags->'barrier' = '{barriertype}' AND
+  (NOT barrier.tags?'access' OR barrier.tags->'access' = 'no') AND -- no = default if we don't test foot/horse/bicycle/psv/bus
+  (NOT barrier.tags?'vehicle' OR barrier.tags->'vehicle' = 'no') AND -- no = default if we don't test foot/horse/bicycle/psv/bus
+  (NOT barrier.tags?'motor_vehicle' OR barrier.tags->'motor_vehicle' = 'no') AND -- no = default if we don't test foot/horse/bicycle/psv/bus/taxi
+  highway_ends.nid IS NULL -- Ignore transitions between ways that may have different tags
+"""
+
+sql21 = """
+SELECT
+    barrier.wid,
+    barrier.id,
+    barrier.geom,
+    highway.tags->'{vehicle}'
+FROM
+    {barriertype} AS barrier
+    JOIN highways AS highway ON
+      highway.id = barrier.wid AND
+      highway.tags?'{vehicle}' AND
+      NOT highway.tags->'{vehicle}' IN ('no', 'use_sidepath', 'unknown')
+WHERE
+    NOT barrier.tags?'{vehicle}'
+"""
+
 
 class Analyser_Osmosis_HighwayAreaAccess(Analyser_Osmosis):
 
+    requires_tables_common = ['highway_ends']
     requires_tables_full = ['highways']
     requires_tables_diff = ['highways', 'touched_highways', 'not_touched_highways']
 
     def __init__(self, config, logger = None):
         Analyser_Osmosis.__init__(self, config, logger)
+        if not "proj" in self.config.options:
+            return
         self.classs_change[1] = self.def_class(item = 2130, level = 3, tags = ['highway', 'routing'],
             title = T_('Inconsistent Access'),
             detail = T_(
-'''Inconsistent `motor_vehicle` values (`destination`!=)'''))
+'''Inconsistent `motor_vehicle` values.'''))
+        self.classs[2] = self.def_class(item = 2130, level = 3, tags = ['highway', 'routing'],
+            title = T_('Inconsistent Access'),
+            detail = T_(
+'''Inconsistent access values between barrier and highway.'''),
+            trap = T_(
+'''Sometimes a barrier can exist on an (otherwise uninterrupted) highway to prevent vehicles from using it for purposes other than destination traffic.'''),
+            fix = T_(
+'''Copy the appropriate access tag to the barrier node'''))
         self.callback10 = lambda res: {"class":1, "data":[self.node_full, self.way_full, self.positionAsText],
             "text": T_("Inconsistent motor_vehicle values ('{0}'!='{1}')", res[3] if res[3] else '', res[4] if res[4] else '') }
+
+    def analyser_osmosis_common(self):
+        self.run(sql20.format(barriertype='bollard'))
+        self.run(sql20.format(barriertype='bus_trap'))
+        for vehicle in ['motorcycle', 'mofa', 'moped', 'emergency', 'motorcar']:
+            self.run(sql21.format(barriertype='bollard', vehicle=vehicle), lambda res: {
+              "class":2, "data":[self.way_full, self.node_full, self.positionAsText],
+              "text": T_("Inconsistent {0} access: '{1}' on highway, not set on barrier", vehicle, res[3])})
+        for vehicle in ['motorcycle', 'mofa', 'moped', 'agricultural', 'hgv', 'emergency']:
+            self.run(sql21.format(barriertype='bus_trap', vehicle=vehicle), lambda res: {
+              "class":2, "data":[self.way_full, self.node_full, self.positionAsText],
+              "text": T_("Inconsistent {0} access: '{1}' on highway, not set on barrier", vehicle, res[3])})
 
     def analyser_osmosis_full(self):
         self.run(sql10.format("", ""), self.callback10)
@@ -72,3 +139,29 @@ class Analyser_Osmosis_HighwayAreaAccess(Analyser_Osmosis):
     def analyser_osmosis_diff(self):
         self.run(sql10.format("touched_", "not_touched_"), self.callback10)
         self.run(sql10.format("", "touched_"), self.callback10)
+
+
+
+###########################################################################
+
+from .Analyser_Osmosis import TestAnalyserOsmosis
+
+class Test(TestAnalyserOsmosis):
+    @classmethod
+    def setup_class(cls):
+        from modules import config
+        TestAnalyserOsmosis.setup_class()
+        cls.analyser_conf = cls.load_osm("tests/osmosis_highway_access_barrier.osm",
+                                         config.dir_tmp + "/tests/osmosis_highway_access_barrier.test.xml",
+                                         {"proj": 2154}) # Random proj to satisfy highway table generation
+
+    def test_classes(self):
+        with Analyser_Osmosis_HighwayAreaAccess(self.analyser_conf, self.logger) as a:
+            a.analyser()
+
+        self.root_err = self.load_errors()
+        self.check_err(cl="1", elems=[("node", "24"), ("way", "108")])
+        self.check_err(cl="1", elems=[("node", "29"), ("way", "109")])
+        self.check_err(cl="1", elems=[("node", "30"), ("way", "110")])
+        self.check_err(cl="2", elems=[("node", "20"), ("way", "107")])
+        self.check_num_err(4)
