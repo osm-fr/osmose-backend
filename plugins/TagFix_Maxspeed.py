5c888055e642dce78ea5d9364e3e503fa0f951ba
mcliquid <info@mcliquid.de>
2022-11-10 20:36:55 +0100

    Fix 1640: Updated values and rules for AT:urban (#1644)

--- plugins/TagFix_Maxspeed.py
+++ plugins/TagFix_Maxspeed.py
@@ -36,7 +36,8 @@ class TagFix_Maxspeed(Plugin):
     maxspeed_table = {
         'at:rural': ['100'],
         'at:trunk': ['100'],
-        'at:urban': ['30', '40'],
+        'at:urban40': ['40'],
+        'at:urban30': ['30'],
         'be-bru:rural': ['70'],
         'be-bru:urban': ['30'],
         'be:motorway': ['120'],
