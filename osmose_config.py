47ed2742b0d138a5f531805ad6a8b9ab26904320
LeJun <lejun@gmx.fr>
2023-01-17 06:13:11 +0000

    Ajout bicycle_parking sur Strasbourg

--- osmose_config.py
+++ osmose_config.py
@@ -277,11 +277,14 @@ france_departement = gen_country('europe', 'france', download_repo=OSMFR, langua
     'merge_road_FR',
 ], **{'addr:city-admin_level': '8,9'})
 
-france_departement("alsace/bas_rhin", 7415, "FR-67")
+france_departement("alsace/bas_rhin", 7415, "FR-67"), include=[
+    # Strasbourg
+    'merge_bicycle_parking_FR_strasbourg',
+])
 france_departement("alsace/haut_rhin", 7403, "FR-68")
 
 include_aquitaine = [
-    # Aquitiane
+    # Aquitaine
     'merge_tourism_FR_aquitaine_camp_caravan',
     'merge_tourism_FR_aquitaine_museum',
     'merge_sport_FR_aquitaine_equestrian',
