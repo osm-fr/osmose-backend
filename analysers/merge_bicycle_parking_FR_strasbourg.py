722f99acf3d1f964d197bb7d6b5eba33c80daf4b
LeJun <lejun@gmx.fr>
2023-01-23 12:22:33 +0000

    Added secondary tags
    Added secondary tagset according to frodrigo

--- analysers/merge_bicycle_parking_FR_strasbourg.py
+++ analysers/merge_bicycle_parking_FR_strasbourg.py
@@ -23,21 +23,50 @@
 from modules.OsmoseTranslation import T_
 from .Analyser_Merge import Analyser_Merge_Point, SourceOpenDataSoft, CSV, Load_XY, Conflate, Select, Mapping
 
-tag_mapping = {
-    "ref": (
-        lambda res["id_local"]
-    ),
+class Analyser_Merge_Bicycle_Parking_FR_Strasbourg(Analyser_Merge_Point):
+    def __init__(self, config, logger = None):
+        Analyser_Merge_Point.__init__(self, config, logger)
+        self.def_class_missing_official(item = 8150, id = 21, level = 3, tags = ['merge', 'public equipment', 'bicycle', 'fix:survey', 'fix:picture'],
+            title = T_('Strasbourg bicycle parking not integrated'))
+
+        self.init(
+            "https://data.strasbourg.eu/explore/dataset/stationnementcyclable_ville_et_eurometropole_de_strasbourg/information/",
+            "Stationnement cyclable au format BNSC",
+            CSV(SourceOpenDataSoft(
+                attribution="Ville et eurométropole de Strasbourg",
+                url="https://data.strasbourg.eu/explore/dataset/stationnementcyclable_ville_et_eurometropole_de_strasbourg/")),
+            Load_XY("geo_point_2d", "geo_point_2d",
+                xFunction = lambda x: Load_XY.float_comma(x.split(',')[1]),
+                yFunction = lambda y: Load_XY.float_comma(y.split(',')[0])
+            ),
+            Conflate(
+                select = Select(
+                    types = ["nodes", "ways"],
+                    tags = {"amenity": "bicycle_parking"}),
+                conflationDistance = 20,
+                mapping = Mapping(
+                    static1 = {"amenity": "bicycle_parking"},
+                    static2 = {"source": self.source},
+                    mapping1 = tag_mapping1,
+                    mapping2 = tag_mapping2)))
+tag_mapping1 = {
     "capacity": (
         lambda res: None if res["capacite"] in (None, "0") else res["capacite"]
     ),
     "capacity:cargo_bike": (
         lambda res: None if res["capacite_cargo"] in (None, "0") else res["capacite_cargo"]
-    ),
+    )
 #    "bicycle_parking": (
 #      type_accroche
 #      mobilier
 #      protection
-#    ),
+#    )
+}
+
+tag_mapping2 = {
+    "ref": (
+        lambda res["id_local"]
+    ),
 #    "access": (
 #        acces      
 #    ),
@@ -69,29 +98,3 @@ tag_mapping = {
         lambda res["commentaires"]
     )
 }
-
-class Analyser_Merge_Bicycle_Parking_FR_Strasbourg(Analyser_Merge_Point):
-    def __init__(self, config, logger = None):
-        Analyser_Merge_Point.__init__(self, config, logger)
-        self.def_class_missing_official(item = 8150, id = 21, level = 3, tags = ['merge', 'public equipment', 'bicycle', 'fix:survey', 'fix:picture'],
-            title = T_('Strasbourg bicycle parking not integrated'))
-
-        self.init(
-            "https://data.strasbourg.eu/explore/dataset/stationnementcyclable_ville_et_eurometropole_de_strasbourg/information/",
-            "Stationnement cyclable au format BNSC",
-            CSV(SourceOpenDataSoft(
-                attribution="Ville et eurométropole de Strasbourg",
-                url="https://data.strasbourg.eu/explore/dataset/stationnementcyclable_ville_et_eurometropole_de_strasbourg/")),
-            Load_XY("geo_point_2d", "geo_point_2d",
-                xFunction = lambda x: Load_XY.float_comma(x.split(',')[1]),
-                yFunction = lambda y: Load_XY.float_comma(y.split(',')[0])
-            ),
-            Conflate(
-                select = Select(
-                    types = ["nodes", "ways"],
-                    tags = {"amenity": "bicycle_parking"}),
-                conflationDistance = 20,
-                mapping = Mapping(
-                    static1 = {"amenity": "bicycle_parking"},
-                    static2 = {"source": self.source},
-                    mapping1 = tag_mapping )))
