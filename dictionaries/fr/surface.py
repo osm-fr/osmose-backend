3e648dc549809d8657a02c8eb3d27e65d3d4cd92
Jean-Baptiste Holcroft <jean-baptiste@holcroft.fr>
2023-03-08 23:59:16 +0100

    Make surface mapping independent and analyze it

---
+++ dictionaries/fr/surface.py
@@ -0,0 +1,12 @@
+SURFACE = {
+        "Sable": "sand",
+        "Gazon naturel": "grass",
+        "Bitume": "asphalt",
+        "Béton": "concrete",
+        "Gazon synthétique": "artificial_turf",
+        "Bois": "wood",
+        "Terre battue": "clay",
+        "Métal": "metal",
+        # 2967 Carrelage
+        # 4637 Parquet
+}
--- tools/manual_review_merge_pitch_FR.py
+++ tools/manual_review_merge_pitch_FR.py
@@ -15,12 +15,18 @@ import os.path
 
 import pandas as pd
 
+import dictionaries.fr.surface
+
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 log = logging.getLogger()
-ES_SOURCE = "data-es.csv"
-ES_EQUIPMENT_COL_NAME = "Type d'équipement sportif"
-MERGE_MATRIX = os.path.join("..", "merge_data", "pitch_FR.mapping.csv")
 
+ES_SOURCE = "data-es.csv"
+EQUIPMENT_COL = "Type d'équipement sportif"
+SURFACE_COL = "Nature du sol"
+MERGE_MATRIX = os.path.join("..", "merge_data", "pitch_FR.mapping.csv")
+SURFACE_MAP = dictionaries.fr.surface.SURFACE
+
+log.info("Jetzt geht's los")
 log.info("Loading source files")
 
 if os.path.exists(ES_SOURCE) is False:
@@ -28,21 +34,54 @@ if os.path.exists(ES_SOURCE) is False:
     exit()
 
 # read specific columns of csv file using Pandas
-df_es = pd.read_csv(ES_SOURCE, sep=";", usecols=[ES_EQUIPMENT_COL_NAME])
+df_es = pd.read_csv(ES_SOURCE, sep=";", usecols=[EQUIPMENT_COL, SURFACE_COL])
+df_es.fillna("", inplace=True)
 df_matrix = pd.read_csv(MERGE_MATRIX, sep=",", header=None, skip_blank_lines=True)
+df_matrix.fillna("", inplace=True)
 
 logging.info("Step 1: searching for missing merge values")
-equipments_es = df_es[ES_EQUIPMENT_COL_NAME].unique()
+equipments_es = df_es[EQUIPMENT_COL].unique()
 equipments_matrix = df_matrix[1].unique()
 
-es_equipments_count = df_es[ES_EQUIPMENT_COL_NAME].value_counts()
+es_equipments_count = df_es[EQUIPMENT_COL].value_counts()
 
-log.info(f"  Step 1.1: do we have all {ES_EQUIPMENT_COL_NAME} in {MERGE_MATRIX}?")
+log.info(f"  Step 1.1: do we have all '{EQUIPMENT_COL}' in '{MERGE_MATRIX}'?")
+coverage = 0
 for equipment in equipments_es:
     if equipments_matrix.__contains__(equipment) is False:
-        log.info(f"    '{equipment}' does not exist in {MERGE_MATRIX}, it is {es_equipments_count.loc[equipment]} equipments")
+        log.info(
+            f"    '{equipment}' does not exist in '{MERGE_MATRIX}', it is {es_equipments_count.loc[equipment]} equipments")
+    else:
+        coverage += es_equipments_count.loc[equipment]
+log.info(f"  -> current coverage is {coverage / df_es[EQUIPMENT_COL].count():.2%}, (missing: {df_es[EQUIPMENT_COL].count() - coverage})")
 
-log.info(f"  Step 1.2: do we have all values from {MERGE_MATRIX} in '{ES_EQUIPMENT_COL_NAME}' column?")
+log.info(f"  Step 1.2: do we have all values from '{MERGE_MATRIX}' in '{EQUIPMENT_COL}' column?")
 for equipment in equipments_matrix:
     if equipments_es.__contains__(equipment) is False:
-        log.info(f"    '{equipment}' does not exist in {ES_SOURCE}")
+        log.info(f"    '{equipment}' does not exist in '{ES_SOURCE}'")
+
+del equipments_es, equipments_matrix, es_equipments_count
+
+logging.info("Step 2: is our surface coverage exhaustive?")
+surface_es = df_es[SURFACE_COL].unique()
+surface_matrix = SURFACE_MAP
+es_surface_count = df_es[SURFACE_COL].value_counts()
+
+log.info(f"  Step 2.1: do we have all '{SURFACE_COL}' in '{MERGE_MATRIX}'?")
+coverage = 0
+for surface in surface_es:
+    if surface_matrix.__contains__(surface) is False:
+        log.info(f"    '{surface}' does not exist in '{MERGE_MATRIX}', it is {es_surface_count.loc[surface]} equipments")
+    else:
+        coverage += es_surface_count.loc[surface]
+log.info(f"  -> current coverage is {coverage / df_es[SURFACE_COL].count():.2%}, (missing: {df_es[SURFACE_COL].count() - coverage})")
+
+
+log.info(f"  Step 2.2: do we have all values from '{MERGE_MATRIX}' in '{SURFACE_COL}' column?")
+for surface in surface_matrix:
+    if surface_es.__contains__(surface) is False:
+        log.info(f"    '{surface}' does not exist in {ES_SOURCE}")
+
+del surface_es, surface_matrix, es_surface_count
+
+log.info("Färtig !")
