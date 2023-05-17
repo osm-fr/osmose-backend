69b8d30ca81f373dd031f9e3203adb119875ceef
FLacombe <francois.lacombe@infos-reseaux.com>
2023-05-17 22:39:30 +0200

    Fix

--- analysers/Analyser_Merge_street_lamp_FR_eclext.py
+++ analysers/Analyser_Merge_street_lamp_FR_eclext.py
@@ -35,78 +35,6 @@ class Analyser_Merge_street_lamp_FR_eclext (Analyser_Merge_Point):
         self.def_class_update_official(item = 8092, id = 14, level = 3, tags = ['merge', 'street_lamp', 'fix:chair', 'fix:survey'],
             title = T_('Street light update'))
 
-        extract_light_method = {
-            'FLUO': 'fluorescent',
-            'HAL': 'halogen',
-            'IM': 'metal-iodide',
-            'INC': 'incandescent',
-            'IND': 'induction',
-            'LED': 'LED',
-            'SBP': 'low_pressure_sodium',
-            'SHP': 'high_pressure_sodium',
-            'VM': 'mercury',
-            'XEN': 'xenon'
-        }
-
-        extract_support = {
-            'CAT': 'catenary',
-            'MUR': 'wall',
-            'SOL': 'ground'
-        }
-
-        def extract_temperature(res):
-            v = res.get('TemperatureCouleur')
-            if v:
-                try:
-                    num_value = int(float(v))
-                    if num_value > 0:
-                        return str(num_value)+" K"
-                    else:
-                        return None
-                except ValueError:
-                    return None
-
-        def extract_adaptatif(res):
-            adaptatif = res.get('eclairageAdaptatif')
-
-            if adaptatif == 'EN':
-                return 'dusk-dawn'
-            elif adaptatif == 'CO':
-                return 'motion'
-            elif adaptatif == 'AV':
-                return 'demand'
-            else:
-                return None
-
-        def clean_string_tag(name):
-            def mapper(res):
-                v = res.get(name)
-                if v:
-                    if v != "INCONNU":
-                        return v.lower()
-                    else:
-                        return None
-                else:
-                    return None
-            return mapper
-
-        def clean_float_tag(name):
-            """
-            If this can be parsed as a number, ensures it is printed as
-            an integer, otherwise return the string unchanged.
-            Returns None for 0.
-            """
-            def mapper(res):
-                v = res.get(name)
-                if v:
-                    try:
-                        num_value = float(v)
-                        if num_value:
-                            return str(num_value)
-                    except ValueError:
-                        return v
-            return mapper
-
         self.init(
             source_url,
             dataset_name,
@@ -122,16 +50,82 @@ class Analyser_Merge_street_lamp_FR_eclext (Analyser_Merge_Point):
                     static1 = {"highway": "street_lamp", "operator": "Syane"},
                     static2 = {"source": self.source},
                     mapping1 = {
-                        "light:method":lambda res: extract_light_method[res.get('typeSource')] if extract_light_method[res.get('typeSource')] else None,
-                        "light:colour":extract_temperature,
-                        "light:flux":clean_float_tag("fluxSource"),
-                        "light:height":clean_float_tag("hauteurFeu"),
-                        "light:power":clean_float_tag("puissance"),
-                        "light:lit":extract_adaptatif,
-                        "support":lambda res: extract_support[res.get('support')] if extract_support[res.get('support')] else None,
-                        "manufacturer":clean_string_tag("fabricant"),
-                        "model":clean_string_tag("model"),
+                        "light:method":lambda res: self.extract_light_method[res.get('typeSource')] if self.extract_light_method[res.get('typeSource')] else None,
+                        "light:colour":self.extract_temperature,
+                        "light:flux":self.clean_float_tag("fluxSource"),
+                        "light:height":self.clean_float_tag("hauteurFeu"),
+                        "light:power":self.clean_float_tag("puissance"),
+                        "light:lit":lambda res: self.extract_adaptatif[res.get('eclairageAdaptatif')] if self.extract_adaptatif[res.get('eclairageAdaptatif')] else None,
+                        "support":lambda res: self.extract_support[res.get('support')] if self.extract_support[res.get('support')] else None,
+                        "manufacturer":self.clean_string_tag("fabricant"),
+                        "model":self.clean_string_tag("model"),
                         "power": lambda res: "pole" if (res.get('typeSource') == 'POT') else None,
                         "start_date": lambda res: parse(res.get('datePremieInstallation')).date() if res.get('datePremieInstallation') else None},
 
                 text = lambda tags, fields: {} )))
+        
+    extract_light_method = {
+        'FLUO': 'fluorescent',
+        'HAL': 'halogen',
+        'IM': 'metal-iodide',
+        'INC': 'incandescent',
+        'IND': 'induction',
+        'LED': 'LED',
+        'SBP': 'low_pressure_sodium',
+        'SHP': 'high_pressure_sodium',
+        'VM': 'mercury',
+        'XEN': 'xenon'
+    }
+
+    extract_support = {
+        'CAT': 'catenary',
+        'MUR': 'wall',
+        'SOL': 'ground'
+    }
+
+    def extract_temperature(res):
+        v = res.get('TemperatureCouleur')
+        if v:
+            try:
+                num_value = int(float(v))
+                if num_value > 0:
+                    return str(num_value)+" K"
+                else:
+                    return None
+            except ValueError:
+                return None
+
+    extract_adaptatif = {
+        'EN': 'dusk_dawn',
+        'CO': 'motion',
+        'AV': 'demand'
+    }
+
+    def clean_string_tag(name):
+        def mapper(res):
+            v = res.get(name)
+            if v:
+                if v != "INCONNU":
+                    return v.lower()
+                else:
+                    return None
+            else:
+                return None
+        return mapper
+
+    def clean_float_tag(name):
+        """
+        If this can be parsed as a number, ensures it is printed as
+        an integer, otherwise return the string unchanged.
+        Returns None for 0.
+        """
+        def mapper(res):
+            v = res.get(name)
+            if v:
+                try:
+                    num_value = float(v)
+                    if num_value:
+                        return str(num_value)
+                except ValueError:
+                    return v
+        return mapper
