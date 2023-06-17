7637c911fc965b81cbb55e11759a2a552acb18f9
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-06-17 11:37:41 +0000

    Update datasource in analyser_merge_hydrants_FR_SDIS_21 #1908

--- analysers/analyser_merge_hydrants_FR_SDIS_21.py
+++ analysers/analyser_merge_hydrants_FR_SDIS_21.py
@@ -27,10 +27,10 @@ from .Analyser_Merge import Source
 class Analyser_Merge_Hydrants_FR_SDIS_21(_Analyser_Merge_Afigeo_Hydrants):
     def __init__(self, config, logger = None):
         _Analyser_Merge_Afigeo_Hydrants.__init__(self, config,
-            source_url='https://trouver.ternum-bfc.fr/dataset/points-deau-incendie-pei-de-cote-dor-du-sdis-21',
-            dataset_name='Points d\'eau incendie (PEI) de Côte d\'Or du SDIS 21',
-            source=Source(attribution='Service départemental d\'incendie et de secours 21',
-                millesime='2022-03',
-                fileUrl='https://trouver.ternum-bfc.fr/dataset/e2ea75c7-0c44-4894-bd70-6c59a2e665da/resource/806a9dc0-e31d-47a9-ab8d-9da7f6401085/service_proxy?SERVICE=WFS&VERSION=2.0.0&REQUEST=GetFeature&TYPENAME=pei_b80f3a5&OUTPUTFORMAT=geojson&CRSNAME=EPSG:4326'),
+            source_url='https://trouver.ternum-bfc.fr/dataset/points-deau-incendie-pei-du-sdis-cote-dor',
+            dataset_name='Points d\'eau incendie (PEI) du SDIS Côte d\'Or',
+            source=Source(attribution='Service Départemental d\'Incendie et de Secours de la Côte d\'Or (SDIS21) Service Prévision',
+                millesime='2023-07',
+                fileUrl='https://trouver.ternum-bfc.fr/dataset/230e8d9c-8a4d-446c-b910-7d1f85aff047/resource/b991c949-8068-4f9d-ba6a-7765eb2e0504/service_proxy?SERVICE=WFS&VERSION=2.0.0&REQUEST=GetFeature&TYPENAME=poteaux_incendie_a11c796&OUTPUTFORMAT=geojson&CRSNAME=EPSG:4326'),
             osmRef='ref:FR:SDIS21',
             logger=logger)
