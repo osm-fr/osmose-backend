451a6411766d22ad9eca67a51afec9514cb90168
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-07-22 08:38:08 +0000

    Update Osmosis to osmosis-0.48.3-36-gada1b6f5-SNAPSHOT using https://github.com/openstreetmap/osmosis/pull/134 #1951 Keep closed linestring closed even if the original way is closed and missing start/end node location

--- osmose_config.py
+++ osmose_config.py
@@ -50,8 +50,8 @@ class template_config:
     bin_pyosmium_up_to_date = modules.config.bin_pyosmium_up_to_date
     osmosis_pre_scripts = [
         dir_scripts + "/osmosis/pgsnapshot_schema_0.6.sql",
-#       dir_scripts + "/osmosis/osmosis-0.48.3-34-gb5383475-SNAPSHOT/script/pgsnapshot_schema_0.6_bbox.sql",
-        dir_scripts + "/osmosis/osmosis-0.48.3-34-gb5383475-SNAPSHOT/script/pgsnapshot_schema_0.6_linestring.sql",
+#       dir_scripts + "/osmosis/osmosis-0.48.3-36-gada1b6f5-SNAPSHOT/script/pgsnapshot_schema_0.6_bbox.sql",
+        dir_scripts + "/osmosis/osmosis-0.48.3-36-gada1b6f5-SNAPSHOT/script/pgsnapshot_schema_0.6_linestring.sql",
         dir_scripts + "/osmosis/CreateMetainfo.sql",
     ]
     osmosis_import_prepare_scripts = [
@@ -71,14 +71,14 @@ class template_config:
     ]
     osmosis_change_init_post_scripts = [  # Scripts to run on database initialisation
         dir_scripts + "/osmosis/pgsimple_schema_0.6_action_drop.sql",
-        dir_scripts + "/osmosis/osmosis-0.48.3-34-gb5383475-SNAPSHOT/script/pgsnapshot_schema_0.6_action.sql",
+        dir_scripts + "/osmosis/osmosis-0.48.3-36-gada1b6f5-SNAPSHOT/script/pgsnapshot_schema_0.6_action.sql",
     ]
     osmosis_change_post_scripts = [  # Scripts to run each time the database is updated
         dir_scripts + "/osmosis/CreateTouched.sql",
     ]
     osmosis_resume_init_post_scripts = [  # Scripts to run on database initialisation
         dir_scripts + "/osmosis/pgsimple_schema_0.6_action_drop.sql",
-        dir_scripts + "/osmosis/osmosis-0.48.3-34-gb5383475-SNAPSHOT/script/pgsnapshot_schema_0.6_action.sql",
+        dir_scripts + "/osmosis/osmosis-0.48.3-36-gada1b6f5-SNAPSHOT/script/pgsnapshot_schema_0.6_action.sql",
     ]
     osmosis_resume_post_scripts = [  # Scripts to run each time the database is updated
         dir_scripts + "/osmosis/ActionFromTimestamp.sql",
