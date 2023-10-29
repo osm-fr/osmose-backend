8cef05f6ebc59abb72e4fa755bcff44b8f071f89
Famlam <fam.lam@live.nl>
2023-10-29 12:53:48 +0100

    Partial rewrite of deadend oneway detection (#2018)

--- analysers/analyser_osmosis_highway_deadend.py
+++ analysers/analyser_osmosis_highway_deadend.py
@@ -23,6 +23,18 @@
 from modules.OsmoseTranslation import T_
 from .Analyser_Osmosis import Analyser_Osmosis
 
+sql10 = """
+-- From https://wiki.postgresql.org/wiki/Array_reverse
+-- Inverts the sequence of the elements in an array
+CREATE OR REPLACE FUNCTION array_reverse(anyarray) RETURNS anyarray AS $$
+SELECT ARRAY(
+    SELECT $1[i]
+    FROM generate_subscripts($1,1) AS s(i)
+    ORDER BY i DESC
+);
+$$ LANGUAGE 'sql' STRICT IMMUTABLE;
+"""
+
 sql20 = """
 SELECT
     wid,
@@ -71,45 +83,182 @@ WHERE
     bicycle_parking.id IS NULL
 """
 
+
 sql30 = """
-CREATE TEMP TABLE oneway AS
+-- Collect all oneways
+CREATE TEMP TABLE oneway_highway AS
 SELECT
   id,
-  t.nid,
+  CASE -- If oneway=-1, pretent the way is the other way around
+    WHEN tags?'oneway' AND tags->'oneway' = '-1' THEN array_reverse(nodes)
+    ELSE nodes
+  END AS nodes,
+  linestring,
+  array_length(nodes, 1) AS length,
+  generate_subscripts(nodes, 1) AS nid_index
+FROM
+  highways
+WHERE
+  (is_oneway OR is_roundabout) AND
+  NOT is_area AND
+  NOT is_construction
+"""
+
+sql31 = """
+CREATE INDEX idx_oneway_highway_linestring ON oneway_highway USING GIST(linestring) WHERE nid_index=1;
+"""
+
+sql32 = """
+-- Collect all nodes that could be possible (valid) end nodes
+CREATE TEMP TABLE allowed_end_nodes AS
+SELECT DISTINCT
+  unnest(nodes) AS id
+FROM
+  ways
+WHERE
+  ways.tags != ''::hstore AND
+  (
+    (ways.tags?'amenity' AND ways.tags->'amenity' = 'parking') OR
+    (ways.tags?'railway' AND ways.tags->'railway' = 'platform') OR
+    (ways.tags?'aeroway' AND ways.tags->'aeroway' = 'taxiway')
+  )
+UNION ALL
+SELECT
+  id
+FROM
+  nodes
+WHERE
+  tags != ''::hstore AND
+  (
+    (tags?'amenity'  AND tags->'amenity' = 'parking_entrance') OR
+    (tags?'entrance' AND tags->'entrance' IN ('garage', 'emergency'))
+  )
+UNION ALL
+SELECT DISTINCT
+  unnest(nodes) AS id
+FROM
+  highways
+WHERE
+  -- Non-oneway highways are valid input nodes for oneways
+  (NOT is_oneway AND NOT is_roundabout) OR
+  -- Force motorways as valid input nodes
+  -- Raceways are commonly isolated from the main network
+  -- Escape (emergency stop) ways are supposed to be not used / dead-ended
+  -- Footway can lead to anything (attraction entrances, ...) so exclude them even if oneway
+  highway IN ('motorway', 'raceway', 'escape', 'footway') OR
+  -- Construction roads are usually temporary, the connections are likely access=no or similar
+  is_construction
+UNION ALL
+-- Include nodes of ways that cross the extract border. See #1949
+-- Better would be to only include the nodes outside of the extract, but that's expensive
+SELECT
+  unnest(borderways.nodes)
+FROM
+  relation_members AS boundary_members
+  JOIN ways AS boundary_ways ON
+    boundary_members.member_id = boundary_ways.id
+  JOIN highways AS borderways ON
+    ST_Intersects(boundary_ways.linestring, borderways.linestring)
+WHERE
+  boundary_members.member_type = 'W' AND
+  boundary_members.relation_id IN {boundary_ids}
+"""
+
+sql33 = """
+CREATE INDEX idx_allowed_nodes_id ON allowed_end_nodes(id);
+"""
+
+sql34 = """
+-- Make table as short as possible: keep only nodes that are actually nodes of oneways
+CREATE TEMP TABLE allowed_oneway_end_nodes AS
+SELECT DISTINCT
+  allowed_end_nodes.id
+FROM
+  allowed_end_nodes
+  JOIN oneway_highway ON
+    -- Note that we cannot literally only get the first and last node, as i.e. in a looped oneway the 'oneway-entrance/exit node' might be in the middle
+    allowed_end_nodes.id = ANY(oneway_highway.nodes) AND
+    oneway_highway.nid_index = 1 -- no need to compare length(nodes) times
+"""
+
+sql35 = """
+CREATE INDEX idx_allowed_oneway_nodes_id ON allowed_oneway_end_nodes(id);
+DROP TABLE allowed_end_nodes;
+"""
+
+sql36 = """
+-- All end nodes of oneway streets
+CREATE TEMP TABLE oneway_terminal AS
+SELECT
+  t.*,
+  geom
+FROM (
+  SELECT DISTINCT ON (nid)
+    oneway.id AS wid,
+    oneway.nodes[oneway.nid_index] AS nid,
+    oneway.nid_index = oneway.length AS is_oneway_last_node
+  FROM
+    oneway_highway AS oneway
+    LEFT JOIN allowed_oneway_end_nodes ON
+      oneway.nodes[oneway.nid_index] = allowed_oneway_end_nodes.id
+  WHERE
+    oneway.nid_index IN (1, oneway.length) AND
+    allowed_oneway_end_nodes IS NULL AND
+    array_length(array_positions(oneway.nodes, oneway.nodes[oneway.nid_index]), 1) = 1 -- exclude end nodes folding back into the same way, e.g. in P or O-shaped ways
+  ORDER BY
+    nid,
+    oneway.id
+  ) AS t
+  JOIN nodes ON
+    id = nid
+"""
+
+sql37 = """
+-- Find all end nodes that are not connected to a way that allows to enter/leave the oneway street
+CREATE TEMP TABLE results_straightforward AS
+SELECT
+  wid,
+  nid,
+  geom
+FROM
+  oneway_terminal
+  LEFT JOIN oneway_highway AS other_highway ON
+    oneway_terminal.nid = ANY(other_highway.nodes) AND
+    oneway_terminal.geom && other_highway.linestring AND -- much faster than comparison with nid
+    other_highway.nid_index = 1 AND -- no need to compare length(nodes) times
+    oneway_terminal.wid != other_highway.id AND
+    (
+      array_length(array_positions(other_highway.nodes, oneway_terminal.nid), 1) != 1 OR -- end node of loop/self-intersecting way that also occurs elsewhere
+      (
+        (oneway_terminal.is_oneway_last_node AND oneway_terminal.nid != other_highway.nodes[other_highway.length]) OR
+        (NOT oneway_terminal.is_oneway_last_node AND oneway_terminal.nid != other_highway.nodes[1])
+      )
+    )
+WHERE
+  other_highway IS NULL
+"""
+
+sql38 = """
+-- All nodes of oneways that have a connection (or are end nodes)
+CREATE TEMP TABLE oneway_connection_nodes AS
+SELECT
+  wid,
+  nid,
   nid_index
 FROM (
   SELECT
-    id,
-    CASE oneway_1
-      WHEN false THEN nodes[nid_index]
-      WHEN true THEN nodes[length - nid_index + 1]
-    END AS nid,
+    id AS wid,
     nid_index,
-    length
-  FROM (
-    SELECT
-      id,
-      nodes,
-      tags?'oneway' AND tags->'oneway' = '-1' AS oneway_1,
-      generate_subscripts(nodes, 1) AS nid_index,
-      array_length(nodes, 1) AS length
-    FROM
-      highways
-    WHERE
-      NOT highways.is_construction AND
-      highway != 'motorway' AND -- Ignore motorway even with oneway tag
-      highway != 'raceway' AND -- Usually not part of the regular road network
-      (
-        is_oneway OR
-        is_roundabout
-      )
+    length,
+    nodes[nid_index] AS nid
+  FROM
+    oneway_highway
   ) AS t
-) AS t
   JOIN way_nodes ON
     way_nodes.node_id = t.nid
 GROUP BY
-  id,
-  t.nid,
+  wid,
+  nid,
   nid_index,
   length
 HAVING
@@ -117,75 +266,39 @@ HAVING
   nid_index IN (1, length)
 """
 
-sql31 = """
-CREATE INDEX idx_oneway_id ON oneway(id)
+sql39 = """
+CREATE INDEX idx_oneway_connection_nodes_wid ON oneway_connection_nodes(wid);
+CREATE INDEX idx_oneway_connection_nodes_nid ON oneway_connection_nodes(nid);
+DROP TABLE oneway_terminal;
 """
 
-sql32 = """
-CREATE INDEX idx_oneway_nid ON oneway(nid)
-"""
-
-sql33 = """
-CREATE TEMP TABLE input_nodes AS (
-SELECT DISTINCT
-  oneway.nid
-FROM
-  oneway
-  JOIN way_nodes ON
-    way_nodes.way_id != oneway.id AND
-    way_nodes.node_id = oneway.nid
-  JOIN ways ON
-    ways.id = way_nodes.way_id
-WHERE
-  ways.tags != ''::hstore AND
-  (
-    ways.tags?'highway' AND
-    (
-        ways.tags->'highway' = 'motorway' OR -- Force motorway as input nodes
-        ways.tags->'highway' = 'raceway' OR -- Commonly isolated chunks
-        (
-          (NOT ways.tags?'oneway' OR ways.tags->'oneway' NOT IN ('yes', 'true', '1', '-1')) AND
-          (NOT ways.tags?'junction' OR ways.tags->'junction' != 'roundabout')
-        )
-    )
-  ) OR (
-    ways.tags?'amenity' AND ways.tags->'amenity' = 'parking'
-  )
-) UNION ALL (
+sql40 = """
+CREATE TEMP TABLE input_nodes AS
 SELECT
-  oneway.nid
+  id
 FROM
-  oneway
-  JOIN nodes ON
-    nodes.id = oneway.nid
-WHERE
-  nodes.tags != ''::hstore AND
-  (
-    (
-      nodes.tags?'amenity' AND
-      nodes.tags->'amenity' = 'parking_entrance' -- entrance and/or exit
-    ) OR
-    (
-      nodes.tags?'entrance' AND
-      nodes.tags->'entrance' = 'garage'
-    )
-  )
-)
+  allowed_oneway_end_nodes
+UNION ALL
+-- Avoid results via recursive checking that originate from a 'straightforward' deadend
+SELECT
+  nid AS id
+FROM
+  results_straightforward
 """
 
-sql34 = """
+sql41 = """
 CREATE TEMP TABLE r AS
 WITH RECURSIVE t AS (
-  SELECT nid FROM input_nodes
+  SELECT id FROM input_nodes
 UNION
   SELECT
     oneway_next.nid AS nid
   FROM
     t
-    JOIN oneway AS oneway_input ON
-      oneway_input.nid = t.nid
-    JOIN oneway AS oneway_next ON
-      oneway_next.id = oneway_input.id AND
+    JOIN oneway_connection_nodes AS oneway_input ON
+      oneway_input.nid = t.id
+    JOIN oneway_connection_nodes AS oneway_next ON
+      oneway_next.wid = oneway_input.wid AND
       oneway_next.nid_index > oneway_input.nid_index
 )
 SELECT
@@ -194,21 +307,49 @@ FROM
   t
 """
 
-sql35 = """
-SELECT
-  DISTINCT ON (oneway.id)
-  oneway.id,
-  oneway.nid,
-  (SELECT ST_AsText(geom) FROM nodes WHERE id = oneway.nid)
+sql42 = """
+CREATE TEMP TABLE results_recursive AS
+SELECT DISTINCT ON (oneway_connection_nodes.wid)
+  oneway_connection_nodes.wid,
+  oneway_connection_nodes.nid
 FROM
-  oneway
+  oneway_connection_nodes
   LEFT JOIN r ON
-    r.nid = oneway.nid
+    r.id = oneway_connection_nodes.nid
 WHERE
-  r.nid IS NULL
+  r.id IS NULL
+ORDER BY
+  oneway_connection_nodes.wid
 """
 
-sql40 = """
+sql43 = """
+SELECT DISTINCT ON(nid)
+  wid,
+  nid,
+  ST_AsText(geom)
+FROM (
+  SELECT
+    wid,
+    nid,
+    geom
+  FROM
+    results_straightforward
+  UNION ALL
+  SELECT
+    wid,
+    nid,
+    geom
+  FROM
+    results_recursive
+    JOIN nodes ON
+      nodes.id = results_recursive.nid
+) AS t
+ORDER BY
+  nid,
+  wid
+"""
+
+sql50 = """
 SELECT
   drivethroughs.id,
   nid,
@@ -238,6 +379,7 @@ WHERE
   drivethroughs.nodes[1] != drivethroughs.nodes[array_length(drivethroughs.nodes,1)] AND
   other_highways.id IS NULL
 """
+
 
 class Analyser_Osmosis_Highway_DeadEnd(Analyser_Osmosis):
 
@@ -280,13 +422,28 @@ Ensure that `service=drive-through` is the correct tag.''')),
         self.callback20 = lambda res: {"class":1 if res[3] == 'cycleway' else 2, "data":[self.way_full, self.node_full, self.positionAsText]}
 
     def analyser_osmosis_common(self):
+        boundary_relation = self.config.polygon_id # Either a number, None or (number, number, ...)
+        if isinstance(boundary_relation, int):
+          boundary_relation = "({0})".format(boundary_relation)
+        elif not boundary_relation:
+          boundary_relation = "(0)"
+
+        self.run(sql10)
         self.run(sql30)
         self.run(sql31)
-        self.run(sql32)
+        self.run(sql32.format(boundary_ids=boundary_relation))
         self.run(sql33)
         self.run(sql34)
-        self.run(sql35, lambda res: {"class":3, "data":[self.way_full, self.node, self.positionAsText]})
-        self.run(sql40, lambda res: {"class":5, "data":[self.way_full, self.node, self.positionAsText]})
+        self.run(sql35)
+        self.run(sql36)
+        self.run(sql37)
+        self.run(sql38)
+        self.run(sql39)
+        self.run(sql40)
+        self.run(sql41)
+        self.run(sql42)
+        self.run(sql43, lambda res: {"class":3, "data":[self.way_full, self.node_full, self.positionAsText]})
+        self.run(sql50, lambda res: {"class":5, "data":[self.way_full, self.node, self.positionAsText]})
 
     def analyser_osmosis_full(self):
         self.run(sql20.format(''), self.callback20)
@@ -316,17 +473,28 @@ class Test(TestAnalyserOsmosis):
         self.check_err(cl="2", elems=[("node", "59"), ("way", "1026")])
         self.check_err(cl="2", elems=[("node", "55"), ("way", "1024")])
 
-        self.check_err(cl="3", elems=[("node", "2"), ("way", "1000")]) # May become node = 1
-        self.check_err(cl="3", elems=[("node", "5"), ("way", "1001")]) # May become node = 3
-        self.check_err(cl="3", elems=[("node", "3"), ("way", "1002")]) # May become node = 4
+        # Detection of straightforward oneway deadends
+        self.check_err(cl="3", elems=[("node", "1"), ("way", "1000")])
+        self.check_err(cl="3", elems=[("node", "2"), ("way", "1000")])
         self.check_err(cl="3", elems=[("node", "9"), ("way", "1003")])
         self.check_err(cl="3", elems=[("node", "14"), ("way", "1005")])
+        self.check_err(cl="3", elems=[("node", "15"), ("way", "1006")]) # way 1006 or 1007 are both fine
         self.check_err(cl="3", elems=[("node", "65"), ("way", "1028")])
-        self.check_err(cl="3", elems=[("node", "15"), ("way", "1006")])
-        self.check_err(cl="3", elems=[("node", "15"), ("way", "1007")])
-        self.check_err(cl="3", elems=[("node", "21"), ("way", "1009")]) # May become node = 23
-        self.check_err(cl="3", elems=[("node", "23"), ("way", "1034")]) # May become node = 21
+        self.check_err(cl="3", elems=[("node", "82"), ("way", "1036")]) # way 1036 or 1037 are both fine
+        self.check_err(cl="3", elems=[("node", "84"), ("way", "1038")]) # way 1038 or 1040 are both fine
+        self.check_err(cl="3", elems=[("node", "90"), ("way", "1043")])
+        self.check_err(cl="3", elems=[("node", "92"), ("way", "1045")])
+        self.check_err(cl="3", elems=[("node", "97"), ("way", "1049")])
+        self.check_err(cl="3", elems=[("node", "98"), ("way", "1048")])
+        self.check_err(cl="3", elems=[("node", "101"), ("way", "1051")])
+        self.check_err(cl="3", elems=[("node", "102"), ("way", "1052")])
+        # Detections of deadend circular oneway highways. Note that some test cases have multiple valid 'solutions'
+        # for dead-end oneway islands upon traversing, so results may change upon code update and still be valid
+        self.check_err(cl="3", elems=[("node", "4"), ("way", "1001")]) # way 1001 or 1002 are both fine
+        self.check_err(cl="3", elems=[("node", "21"), ("way", "1009")]) # way 1009 or 1034 are both fine
+        self.check_err(cl="3", elems=[("node", "108"), ("way", "1058")]) # way 1058 or 1059 are both fine
+        self.check_err(cl="3", elems=[("node", "109"), ("way", "1060")]) # way 1058 or 1060 are both fine
 
         self.check_err(cl="5", elems=[("node", "73"), ("way", "1031")])
 
-        self.check_num_err(13)
+        self.check_num_err(21)
