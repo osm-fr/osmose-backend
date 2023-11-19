8f9cf785f570a1f34e4fd47935b5631e1713be42
Famlam <fam.lam@live.nl>
2023-11-19 22:42:40 +0100

    Regenerate with Antlr

--- mapcss/generated/MapCSSListener.py
+++ mapcss/generated/MapCSSListener.py
@@ -242,6 +242,51 @@ class MapCSSListener(ParseTreeListener):
         pass
 
 
+    # Enter a parse tree produced by MapCSSParser#supports_block.
+    def enterSupports_block(self, ctx:MapCSSParser.Supports_blockContext):
+        pass
+
+    # Exit a parse tree produced by MapCSSParser#supports_block.
+    def exitSupports_block(self, ctx:MapCSSParser.Supports_blockContext):
+        pass
+
+
+    # Enter a parse tree produced by MapCSSParser#supports_rule.
+    def enterSupports_rule(self, ctx:MapCSSParser.Supports_ruleContext):
+        pass
+
+    # Exit a parse tree produced by MapCSSParser#supports_rule.
+    def exitSupports_rule(self, ctx:MapCSSParser.Supports_ruleContext):
+        pass
+
+
+    # Enter a parse tree produced by MapCSSParser#supports_condition.
+    def enterSupports_condition(self, ctx:MapCSSParser.Supports_conditionContext):
+        pass
+
+    # Exit a parse tree produced by MapCSSParser#supports_condition.
+    def exitSupports_condition(self, ctx:MapCSSParser.Supports_conditionContext):
+        pass
+
+
+    # Enter a parse tree produced by MapCSSParser#supports_in_parens.
+    def enterSupports_in_parens(self, ctx:MapCSSParser.Supports_in_parensContext):
+        pass
+
+    # Exit a parse tree produced by MapCSSParser#supports_in_parens.
+    def exitSupports_in_parens(self, ctx:MapCSSParser.Supports_in_parensContext):
+        pass
+
+
+    # Enter a parse tree produced by MapCSSParser#supports_declaration.
+    def enterSupports_declaration(self, ctx:MapCSSParser.Supports_declarationContext):
+        pass
+
+    # Exit a parse tree produced by MapCSSParser#supports_declaration.
+    def exitSupports_declaration(self, ctx:MapCSSParser.Supports_declarationContext):
+        pass
+
+
     # Enter a parse tree produced by MapCSSParser#booleanOperator.
     def enterBooleanOperator(self, ctx:MapCSSParser.BooleanOperatorContext):
         pass
