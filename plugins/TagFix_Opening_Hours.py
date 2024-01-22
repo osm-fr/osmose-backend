a589083000dde264d7b7c78ad7844b60f36a6c32
Famlam <fam.lam@live.nl>
2024-01-23 00:08:51 +0100

    Enable opening_hours check for subkeys
    See #2135
    Enable the plugin for selected subkeys of opening_hours

--- plugins/TagFix_Opening_Hours.py
+++ plugins/TagFix_Opening_Hours.py
@@ -21,6 +21,7 @@
 
 from modules.OsmoseTranslation import T_
 from plugins.Plugin import Plugin
+from modules.Stablehash import stablehash64
 
 try:
     from PyKOpeningHours.PyKOpeningHours import OpeningHours, Error
@@ -38,6 +39,8 @@ class TagFix_Opening_Hours(Plugin):
         self.errors[32501] = self.def_class(item = 3250, level = 3, tags = ['value', 'fix:chair'],
             title = T_('Invalid Opening Hours'))
 
+        self.OHkeys = ['opening_hours', 'opening_hours:kitchen', 'opening_hours:drive_through']
+
     # Function is also called from ConditionalRestrictions
     def sanitize_openinghours(self, openinghours_value):
         if not module_PyKOpeningHours:
@@ -54,14 +57,18 @@ class TagFix_Opening_Hours(Plugin):
         return {"isValid": True}
 
     def node(self, data, tags):
-        if 'opening_hours' not in tags:
-            return
-        sanitized = self.sanitize_openinghours(tags['opening_hours'])
-        if not sanitized['isValid']:
-            if "fix" in sanitized:
-                return {"class": 32501, "subclass": 0, 'fix': {'opening_hours': sanitized['fix']}}
-            else:
-                return {"class": 32501, "subclass": 1, 'text': T_("The `opening_hours` value is invalid and could not be parsed")}
+        err = []
+        for tag in self.OHkeys:
+            if not tag in tags:
+                continue
+            sanitized = self.sanitize_openinghours(tags[tag])
+            if not sanitized['isValid']:
+                if "fix" in sanitized:
+                    err.append({"class": 32501, "subclass": 0+stablehash64(tag), 'fix': {tag: sanitized['fix']}})
+                else:
+                    err.append({"class": 32501, "subclass": 1+stablehash64(tag), 'text': T_("The `{0}` value is invalid and could not be parsed", tag)})
+        if err != []:
+            return err
 
     def way(self, data, tags, nds):
         return self.node(data, tags)
@@ -78,22 +85,23 @@ class Test(TestPluginCommon):
         a = TagFix_Opening_Hours(None)
         a.init(None)
 
-        # These should return a modification suggestion
-        self.check_err(a.node(None, {'opening_hours': 'mo-fr 10h - 19h00'}), expected={"class": 32501, "subclass": 0, 'fix': {'opening_hours': 'Mo-Fr 10:00-19:00'}})
-        self.check_err(a.node(None, {'opening_hours': 'Monday to Friday 8:00AM to 4:30PM'}), expected={"class": 32501, "subclass": 0, 'fix': {'opening_hours': 'Mo-Fr 08:00-16:30'}})
+        for k in ['opening_hours', 'opening_hours:kitchen']:
+            # These should return a modification suggestion
+            self.check_err(a.node(None, {k: 'mo-fr 10h - 19h00'}), expected={"class": 32501, 'fix': {k: 'Mo-Fr 10:00-19:00'}})
+            self.check_err(a.node(None, {k: 'Monday to Friday 8:00AM to 4:30PM'}), expected={"class": 32501, 'fix': {k: 'Mo-Fr 08:00-16:30'}})
 
-        # These return a parse error
-        self.check_err(a.node(None, {'opening_hours': 'mo-fr 10h - 19h00"'}), expected={"class": 32501, "subclass": 1})
-        self.check_err(a.node(None, {'opening_hours': '09:00-21:00 TEL/072(360)3200'}), expected={"class": 32501, "subclass": 1})
+            # These return a parse error
+            self.check_err(a.node(None, {k: 'mo-fr 10h - 19h00"'}), expected={"class": 32501})
+            self.check_err(a.node(None, {k: '09:00-21:00 TEL/072(360)3200'}), expected={"class": 32501})
 
-        # These are OK, no suggestion
-        assert not a.node(None, {'opening_hours': 'Mo-Fr 10:00-19:00'})
-        assert not a.node(None, {'opening_hours': 'Mo-Tu,Th-Fr 09:30-12:00; We 15:00-17:00; Dec 24,31 off; Sa,Su off; PH off'})
-        assert not a.node(None, {'opening_hours': 'Mo off, Tu-Th 09:00-18:00; Fr 09:00-19:00; Sa 08:00-18:00; Su off'})
+            # These are OK, no suggestion
+            assert not a.node(None, {k: 'Mo-Fr 10:00-19:00'})
+            assert not a.node(None, {k: 'Mo-Tu,Th-Fr 09:30-12:00; We 15:00-17:00; Dec 24,31 off; Sa,Su off; PH off'})
+            assert not a.node(None, {k: 'Mo off, Tu-Th 09:00-18:00; Fr 09:00-19:00; Sa 08:00-18:00; Su off'})
 
-        # Check 00:00 and 24:00 are both OK
-        assert not a.node(None, {'opening_hours': 'Mo-Su 09:00-00:00'})
-        assert not a.node(None, {'opening_hours': 'Mo-Su 09:00-24:00'})
+            # Check 00:00 and 24:00 are both OK
+            assert not a.node(None, {k: 'Mo-Su 09:00-00:00'})
+            assert not a.node(None, {k: 'Mo-Su 09:00-24:00'})
 
-        # Shold be OK https://github.com/osm-fr/osmose-backend/issues/1568
+        # Should be OK https://github.com/osm-fr/osmose-backend/issues/1568
         assert not a.node(None, {'opening_hours': 'Tu-Fr 11:00-19:00; Sa 10:00-22:15; 14:00-15:00 closed'})
