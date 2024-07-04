19e819f2a6dcf889556a717ed2dcab854a5c7b9b
ComradeRamen <150104144+ComradeRamen@users.noreply.github.com>
2024-07-04 01:53:46 -0700

    add noexit=yes as valid oneway endpoint

--- analysers/analyser_osmosis_highway_deadend.py
+++ analysers/analyser_osmosis_highway_deadend.py
@@ -155,7 +155,8 @@ WHERE
   (
     (tags?'amenity'  AND tags->'amenity' IN ('parking_entrance', 'parking', 'ferry_terminal')) OR
     (tags?'entrance' AND tags->'entrance' IN ('garage', 'emergency', 'yes')) OR
-    (tags?'aerialway' AND tags->'aerialway' = 'station')
+    (tags?'aerialway' AND tags->'aerialway' = 'station') OR
+    (tags?'noexit' AND tags->'noexit' = 'yes')
   )
 UNION ALL
 SELECT DISTINCT
