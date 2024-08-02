4d225c715f8f3011e23f27e72449cfa50c213b9a
didier2020 <didier2020@free.fr>
2024-08-02 10:03:16 +0200

    Update osmose_config.py
    only one analyser for metropole and com

--- osmose_config.py
+++ osmose_config.py
@@ -285,7 +285,7 @@ france_departement = gen_country('europe', 'france', download_repo=OSMFR, langua
     'osmosis_fantoir',
     'osmosis_highway_motorway',
     'osmosis_highway_zone',
-    'merge_milestone_FR_metropole',
+    'merge_milestone_FR',
     'merge_shop_FR',
     'merge_cemetery_FR',
     'merge_man_made_FR',
@@ -561,10 +561,10 @@ france_departement_dom = gen_country(None, country_base='france', download_repo=
 
 france_departement_dom(["central-america", "guadeloupe"], 1401835, "FR-GP", dep_code=971, proj=32620, phone_code="590")
 france_departement_dom(["south-america", "guyane"], 1260551, "FR-GF", dep_code=973, language='fr_GF', proj=2972, phone_code="594", include=[
-    'merge_milestone_FR_guyane',])
+    'merge_milestone_FR',])
 france_departement_dom(["central-america", "martinique"], 1891495, "FR-MQ", dep_code=972, proj=32620, phone_code="596")
 france_departement_dom(["africa", "mayotte"], 1259885, "FR-YT", dep_code=976, proj=32738, phone_code="262", include=[
-    'merge_milestone_FR_mayotte',])
+    'merge_milestone_FR',])
 france_departement_dom(["africa", "reunion"], 1785276, "FR-RE", dep_code=974, proj=2975, phone_code="262")
 
 france_com = gen_country(None, country_base='france', download_repo=OSMFR, language='fr', municipality_ref='ref:INSEE',
@@ -604,7 +604,7 @@ france_com(["north-america", "saint_pierre_et_miquelon"], 3406826, "FR-PM", dep_
     'merge_natural_FR',
     'merge_reservoir_FR',
     'merge_water_FR',
-    'merge_milestone_FR_saint_pierre_et_miquelon',
+    'merge_milestone_FR',
     'merge_road_FR',
 ])
 france_com(["oceania", "wallis_et_futuna"], 290162, "FR-WF", proj=32701, phone_code="681", country="wallisetfutuna")
