3018e04ef0f972b589a83443eb768fb3fa99a7f7
ComradeRamen <150104144+ComradeRamen@users.noreply.github.com>
2024-10-23 15:01:10 -0700

    remove motorway and trunk exclusions from almost junction check.

--- analysers/analyser_osmosis_highway_almost_junction.py
+++ analysers/analyser_osmosis_highway_almost_junction.py
@@ -45,7 +45,7 @@ FROM (
     FROM
       highways
     WHERE
-      highway NOT IN ('motorway', 'motorway_link', 'trunk', 'trunk_link', 'service', 'footway', 'path', 'platform', 'steps') AND
+      highway NOT IN ('service', 'footway', 'path', 'platform', 'steps') AND
       NOT is_construction AND
       NOT is_polygon AND
       ST_Length(linestring_proj) > 10
