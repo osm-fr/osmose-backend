775d7c39705900b52c8a838ce95404dd3eaab586
FLacombe <francois.lacombe@infos-reseaux.com>
2024-11-18 23:00:32 +0100

    Add merge analyser for France cher power poles

--- analysers/analyser_merge_power_pole_FR_spec_sde18.py
+++ analysers/analyser_merge_power_pole_FR_spec_sde18.py
@@ -21,41 +21,43 @@
 ###########################################################################
 
 from modules.OsmoseTranslation import T_
-from .Analyser_Merge import Analyser_Merge_Point, GDAL, LoadGeomCentroid, Conflate, Select, Mapping
+from .Analyser_Merge import Analyser_Merge_Point, SourceDataGouv, GDAL, LoadGeomCentroid, Conflate, Select, Mapping
 
 
-class Analyser_Merge_power_pole_FR_gracethd3 (Analyser_Merge_Point):
-    def __init__(self, config, source_url, dataset_name, source, srid, conflationDistance, classs, extract_operator = None, logger = None):
+class Analyser_Merge_power_pole_FR_spec_sde18 (Analyser_Merge_Point):
+    def __init__(self, config, logger = None):
         Analyser_Merge_Point.__init__(self, config, logger)
-        self.def_class_missing_official(item = 8290, id = classs + 1, level = 3, tags = ['merge', 'power', 'fix:chair', 'fix:survey'],
+        self.def_class_missing_official(item = 8290, id = 1001, level = 3, tags = ['merge', 'power', 'fix:chair', 'fix:survey'],
             title = T_('Power pole not integrated'))
-        self.def_class_possible_merge(item = 8291, id = classs + 3, level = 3, tags = ['merge', 'power', 'fix:chair', 'fix:survey'],
+        self.def_class_possible_merge(item = 8291, id = 1003, level = 3, tags = ['merge', 'power', 'fix:chair', 'fix:survey'],
             title = T_('Power pole integration suggestion'))
-        self.def_class_update_official(item = 8290, id = classs + 4, level = 3, tags = ['merge', 'power', 'fix:chair', 'fix:survey'],
+        self.def_class_update_official(item = 8290, id = 1004, level = 3, tags = ['merge', 'power', 'fix:chair', 'fix:survey'],
             title = T_('Power pole update'))
 
         self.init(
-            source_url,
-            dataset_name,
-            GDAL(source, srid = srid, zip="*.shp"),
-            LoadGeomCentroid(),
+            "https://www.data.gouv.fr/fr/datasets/supports-eclairage-public-du-sde18/",
+            "Supports Eclairage Public du SDE18",
+            GDAL(SourceDataGouv(
+                    attribution="SDE 18",
+                    dataset="673b1255be2baa1d2a71c950",
+                    resource="a6296c3c-8f51-485c-a6c7-c0cead86d474"),
+                srid = 2154,
+                extract="*.shp"),
+            LoadGeomCentroid(select = {"_type": ["poteau", "Poteau", "POTEAU"]} ),
             Conflate(
                 select = Select(
                     types = ['nodes'],
                     tags = {'power': 'pole'}),
-                conflationDistance = conflationDistance,
+                conflationDistance = 5,
                 mapping = Mapping(
                     static1 = {'power': 'pole'},
-                    static2 = {'source': self.source},
+                    static2 = {'source': self.source, 'highway': 'street_lamp', 'operator':'Enedis', 'operator:wikidata':'Q3587594'},
                     mapping1 = {
-                        'material': lambda res: self.extract_material.get(res['pt_nature']),
-                        'operator': lambda res: extract_operator.get(res['pt_gest'], extract_operator.get(res['pt_prop'])),
-                        'height': lambda res: res['pt_a_haut'] if res['pt_a_haut'] and float(res['pt_a_haut']) > 6.0 else None},
+                        'material': lambda res: self.extract_material.get(res['_matiere']),
+                        'height': lambda res: res['hauteur'] if res['hauteur'] and res['hauteur'].isnumeric() and float(res['hauteur']) > 6.0 else None},
                 text = lambda tags, fields: {} )))
 
     extract_material = {
-        'PBOI': 'wood',
-        'PBET': 'concrete',
-        'PCMP': 'epoxy',
-        'PMET': 'steel'
+        'Bois': 'wood',
+        'Béton': 'concrete'
     }
