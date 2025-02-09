42e463fb8926476dc77f2e4675f392ef82c19f04
FLacombe <francois.lacombe@infos-reseaux.com>
2025-02-09 13:42:06 +0100

    Fix lint

--- analysers/analyser_merge_power_pole_FR_spec_sdey.py
+++ analysers/analyser_merge_power_pole_FR_spec_sdey.py
@@ -28,11 +28,11 @@ class Analyser_Merge_power_pole_FR_spec_sdey (Analyser_Merge_Point):
     def __init__(self, config, logger = None):
         Analyser_Merge_Point.__init__(self, config, logger)
         classs = 1060
-        self.def_class_missing_official(item = 8290, id = classs +1, level = 3, tags = ['merge', 'power', 'fix:chair', 'fix:survey'],
+        self.def_class_missing_official(item = 8290, id = classs + 1, level = 3, tags = ['merge', 'power', 'fix:chair', 'fix:survey'],
             title = T_('Power pole not integrated'))
-        self.def_class_possible_merge(item = 8291, id = classs +3, level = 3, tags = ['merge', 'power', 'fix:chair', 'fix:survey'],
+        self.def_class_possible_merge(item = 8291, id = classs + 3, level = 3, tags = ['merge', 'power', 'fix:chair', 'fix:survey'],
             title = T_('Power pole integration suggestion'))
-        self.def_class_update_official(item = 8290, id = classs +4, level = 3, tags = ['merge', 'power', 'fix:chair', 'fix:survey'],
+        self.def_class_update_official(item = 8290, id = classs + 4, level = 3, tags = ['merge', 'power', 'fix:chair', 'fix:survey'],
             title = T_('Power pole update'))
 
         self.init(
