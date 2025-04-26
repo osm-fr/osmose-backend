ffac283faf6cd93fe76a9b173ad8a20b44b0f4be
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2025-04-26 15:59:20 +0200

    Update analysers/analyser_osmosis_powerline.py

--- analysers/analyser_osmosis_powerline.py
+++ analysers/analyser_osmosis_powerline.py
@@ -30,7 +30,7 @@ sql01 = """
 CREATE TEMP TABLE power_lines_nodes AS
 SELECT
     w.id as wid,
-    unnest('{NULL}'||w.nodes[1:array_length(w.nodes,1)-1]) AS nid_prec,
+    unnest('{NULL}' || w.nodes[1:array_length(w.nodes, 1) - 1]) AS nid_prec,
     unnest(w.nodes) AS nid,
     unnest(w.nodes[2:array_length(w.nodes,1)]) AS nid_next,
     w.tags->'cables' as cables,
