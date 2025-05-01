f6eb613d84e6353d85d6e7565800a79efe672492
Wolfgang <wolfbert@users.noreply.github.com>
2025-05-01 21:46:27 +0200

    Removed unused maxspeed types for Austria
    at:urban30 and at:urban40 have been superseded by at:city_limit30 and at:city_limit40 (which will be checked in a separate plugin).

--- plugins/TagFix_Maxspeed.py
+++ plugins/TagFix_Maxspeed.py
@@ -38,8 +38,6 @@ class TagFix_Maxspeed(Plugin):
     maxspeed_table = {
         'at:rural': ['100'],
         'at:trunk': ['100'],
-        'at:urban40': ['40'],
-        'at:urban30': ['30'],
         'be-bru:rural': ['70'],
         'be-bru:urban': ['30'],
         'be:motorway': ['120'],
